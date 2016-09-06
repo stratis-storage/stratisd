2faafd57157dcb8507da569b6e4a41c13ce52dee
mulhern <amulhern@redhat.com>
2016-09-06 09:04:58 -0400

    Omit force flag for destroy pool.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- stratisd/stratis_dbus.c
+++ stratisd/stratis_dbus.c
@@ -554,16 +554,8 @@ static int destroy_pool(sd_bus_message *m, void *userdata, sd_bus_error *error) 
 	spool_t *spool = NULL;
 	char dbus_name[MAX_STRATIS_NAME_LEN] = "";
 	char *name = NULL;
-	int force_flag = FALSE;
 
 	rc = sd_bus_message_read(m, "s", &name);
-
-	if (rc < 0) {
-		rc = STRATIS_BAD_PARAM;
-		goto out;
-	}
-
-	rc = sd_bus_message_read(m, "i", &force_flag);
 
 	if (rc < 0) {
 		rc = STRATIS_BAD_PARAM;
@@ -1336,7 +1328,7 @@ static const sd_bus_vtable stratis_manager_vtable[] = {
 	SD_BUS_PROPERTY("LogLevel", "s", property_get_log_level,  0, SD_BUS_VTABLE_PROPERTY_CONST),
 	SD_BUS_METHOD("ListPools", NULL, "asis", list_pools, SD_BUS_VTABLE_UNPRIVILEGED),
 	SD_BUS_METHOD("CreatePool", "sasi", "sis", create_pool, SD_BUS_VTABLE_UNPRIVILEGED),
-	SD_BUS_METHOD("DestroyPool", "si", "sis", destroy_pool, SD_BUS_VTABLE_UNPRIVILEGED),
+	SD_BUS_METHOD("DestroyPool", "s", "sis", destroy_pool, SD_BUS_VTABLE_UNPRIVILEGED),
 	SD_BUS_METHOD("GetPoolObjectPath", "s", "sis", get_pool_object_path, SD_BUS_VTABLE_UNPRIVILEGED),
 	SD_BUS_METHOD("GetVolumeObjectPath", "ss", "sis", get_volume_object_path, SD_BUS_VTABLE_UNPRIVILEGED),
 	SD_BUS_METHOD("GetDevObjectPath", "s", "sis", get_dev_object_path, SD_BUS_VTABLE_UNPRIVILEGED),

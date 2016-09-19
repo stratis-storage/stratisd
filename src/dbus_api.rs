7ace60505c6a3e70d8a799b65f57137186dfb33e
Todd Gill <tgill@redhat.com>
2016-09-19 10:59:00 -0400

    split methods out so they are not constructed inline

--- src/dbus_api.rs
+++ src/dbus_api.rs
@@ -12,6 +12,8 @@ use dbus::{Connection, NameFlag};
 use dbus::tree::{Factory, Tree, Property, MethodFn, MethodErr, EmitsChangedSignal, Interface};
 use dbus::MessageItem;
 use dbus;
+use dbus::Message;
+use dbus::tree::MethodResult; 
 
 use blockdev::{BlockMember, BlockDevs};
 
@@ -41,8 +43,195 @@ impl<'a> DbusContext<'a> {
     }
 }
 
+fn listpools(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
 
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
 
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn createpool(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn destroypool(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getpoolobjectpath(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getvolumeobjectpath(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getdevobjectpath(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getcacheobjectpath(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn geterrorcodes(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getraidlevels(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
+
+fn getdevtypes(m: &Message) -> MethodResult {
+    let mut items = m.get_items();
+    if items.len() < 1 {
+        return Err(MethodErr::no_arg());
+    }
+
+    let name = try!(items.pop()
+        .ok_or_else(MethodErr::no_arg)
+        .and_then(|i| {
+            i.inner::<&str>()
+                .map_err(|_| MethodErr::invalid_arg(&i))
+                .map(|i| i.to_owned())
+        }));
+
+    println!("method called");
+
+    Ok(vec![m.method_return()])
+}
 pub fn get_base_tree<'a>(c: &'a Connection) -> StratisResult<Tree<MethodFn<'a>>> {
     c.register_name(STRATIS_BASE_SERVICE, NameFlag::ReplaceExisting as u32).unwrap();
 
@@ -50,212 +239,56 @@ pub fn get_base_tree<'a>(c: &'a Connection) -> StratisResult<Tree<MethodFn<'a>>>
 
     let base_tree = f.tree();
 
-    let listpools_method = f.method(LIST_POOLS, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let createpool_method = f.method(CREATE_POOL, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let destroypool_method = f.method(DESTROY_POOL, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getpoolobjectpath_method = f.method(GET_POOL_OBJECT_PATH, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getvolumeobjectpath_method = f.method(GET_VOLUME_OBJECT_PATH, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getdevobjectpath_method = f.method(GET_DEV_OBJECT_PATH, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getcacheobjectpath_method = f.method(GET_CACHE_OBJECT_PATH, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let geterrorcodes_method = f.method(GET_ERROR_CODES, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getraidlevels_method = f.method(GET_RAID_LEVELS, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
-    let getdevtypes_method = f.method(GET_DEV_TYPES, move |m, _, _| {
-            let mut items = m.get_items();
-            if items.len() < 1 {
-                return Err(MethodErr::no_arg());
-            }
-
-            let name = try!(items.pop()
-                .ok_or_else(MethodErr::no_arg)
-                .and_then(|i| {
-                    i.inner::<&str>()
-                        .map_err(|_| MethodErr::invalid_arg(&i))
-                        .map(|i| i.to_owned())
-                }));
-
-            println!("method called");
-
-            Ok(vec![m.method_return()])
-        })
-        .in_arg(("name", "s"));
+    let listpools_method = f.method(LIST_POOLS, move |m, _, _| { listpools(m) })
+         .in_arg(("poolname", "s"))
+         .in_arg(("devlist", "as"))
+         .in_arg(("raidtype", "i"))
+         .out_arg(("object_name", "s"))
+         .out_arg(("return_code", "i"))
+         .out_arg(("return_string", "s"));
+        
+    let createpool_method = f.method(CREATE_POOL, move |m, _, _| { createpool(m) })
+         .in_arg(("name", "s"));
+          
+    let destroypool_method = f.method(DESTROY_POOL, move |m, _, _| { destroypool(m) })
+         .in_arg(("name", "s"));
+         
+    let getpoolobjectpath_method = f.method(GET_POOL_OBJECT_PATH, move |m, _, _| { getpoolobjectpath(m) })
+         .in_arg(("name", "s"));
+         
+    let getvolumeobjectpath_method = f.method(GET_VOLUME_OBJECT_PATH, move |m, _, _| { getvolumeobjectpath(m) })
+         .in_arg(("name", "s"));
+    
+    let getdevobjectpath_method = f.method(GET_DEV_OBJECT_PATH, move |m, _, _| { getdevobjectpath(m) })
+         .in_arg(("name", "s"));
+    
+    let getcacheobjectpath_method = f.method(GET_CACHE_OBJECT_PATH, move |m, _, _| { getcacheobjectpath(m) })
+         .in_arg(("name", "s"));
+    
+    let geterrorcodes_method = f.method(GET_ERROR_CODES, move |m, _, _| { geterrorcodes(m) })
+         .in_arg(("name", "s"));
+    
+    let getraidlevels_method = f.method(GET_RAID_LEVELS, move |m, _, _| { getraidlevels(m) })
+         .in_arg(("name", "s"));
+    
+    let getdevtypes_method = f.method(GET_DEV_TYPES, move |m, _, _| { getdevtypes(m) })
+         .in_arg(("name", "s"));
 
 
 
     let obj_path = f.object_path(STRATIS_BASE_PATH)
         .introspectable()
         .add(f.interface(STRATIS_BASE_SERVICE)
-        	.add_m(listpools_method)
-			.add_m(createpool_method)
-			.add_m(destroypool_method)
-			.add_m(getpoolobjectpath_method)
-			.add_m(getvolumeobjectpath_method)
-			.add_m(getdevobjectpath_method)
-			.add_m(getcacheobjectpath_method)
-			.add_m(geterrorcodes_method)
-			.add_m(getraidlevels_method)
-			.add_m(getdevtypes_method));
+            .add_m(listpools_method)
+            .add_m(createpool_method)
+            .add_m(destroypool_method)
+            .add_m(getpoolobjectpath_method)
+            .add_m(getvolumeobjectpath_method)
+            .add_m(getdevobjectpath_method)
+            .add_m(getcacheobjectpath_method)
+            .add_m(geterrorcodes_method)
+            .add_m(getraidlevels_method)
+            .add_m(getdevtypes_method));
 
 
     let base_tree = base_tree.add(obj_path);

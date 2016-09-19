e5cbef035f0548b20ccd5e6244432b8df72d03ff
Andy Grover <agrover@redhat.com>
2016-09-19 12:05:18 -0700

    Fix spacing in consts.rs
    Remove double spaces before some consts
    
    Remove tabs in enums
    
    Newline at end of file.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- src/consts.rs
+++ src/consts.rs
@@ -4,17 +4,17 @@ pub const SECTOR_SIZE: u64 = 512;
 
 
 
-pub const  STRATIS_VERSION: &'static str = "1";
-pub const  MANAGER_NAME:  &'static str = "/Manager";
-pub const  STRATIS_BASE_PATH:  &'static str = "/org/storage/stratis1";
-pub const  STRATIS_BASE_SERVICE:  &'static str = "org.storage.stratis1";
-pub const  STRATIS_BASE_MANAGER:  &'static str = "/org/storage/stratis1/Manager";
-pub const  STRATIS_MANAGER_INTERFACE: &'static str = "org.storage.stratis1.Manager";
-pub const  STRATIS_POOL_BASE_INTERFACE: &'static str = "org.storage.stratis1.pool";
-pub const  STRATIS_VOLUME_BASE_INTERFACE: &'static str = "org.storage.stratis1.volume";
-pub const  STRATIS_DEV_BASE_INTERFACE: &'static str = "org.storage.stratis1.dev";
-pub const  STRATIS_CACHE_BASE_INTERFACE: &'static str = "org.storage.stratis1.cache";
-pub const  STRATIS_POOL_BASE_PATH: &'static str = "/org/storage/stratis/pool";
+pub const STRATIS_VERSION: &'static str = "1";
+pub const MANAGER_NAME:  &'static str = "/Manager";
+pub const STRATIS_BASE_PATH:  &'static str = "/org/storage/stratis1";
+pub const STRATIS_BASE_SERVICE:  &'static str = "org.storage.stratis1";
+pub const STRATIS_BASE_MANAGER:  &'static str = "/org/storage/stratis1/Manager";
+pub const STRATIS_MANAGER_INTERFACE: &'static str = "org.storage.stratis1.Manager";
+pub const STRATIS_POOL_BASE_INTERFACE: &'static str = "org.storage.stratis1.pool";
+pub const STRATIS_VOLUME_BASE_INTERFACE: &'static str = "org.storage.stratis1.volume";
+pub const STRATIS_DEV_BASE_INTERFACE: &'static str = "org.storage.stratis1.dev";
+pub const STRATIS_CACHE_BASE_INTERFACE: &'static str = "org.storage.stratis1.cache";
+pub const STRATIS_POOL_BASE_PATH: &'static str = "/org/storage/stratis/pool";
 
 
 pub const LIST_POOLS: &'static str = "ListPools";
@@ -31,32 +31,32 @@ pub const GET_DEV_TYPES: &'static str = "GetDevTypes";
 
 pub enum StratisErrorEnum {
     STRATIS_OK,
-	STRATIS_ERROR,
-	STRATIS_NULL,
-	STRATIS_MALLOC,
-	STRATIS_NOTFOUND,
-	STRATIS_POOL_NOTFOUND,
-	STRATIS_VOLUME_NOTFOUND,
-	STRATIS_DEV_NOTFOUND,
-	STRATIS_CACHE_NOTFOUND,
-	STRATIS_BAD_PARAM,
-	STRATIS_ALREADY_EXISTS,
-	STRATIS_NULL_NAME,
-	STRATIS_NO_POOLS,
-	STRATIS_LIST_FAILURE,
-	STRATIS_ERROR_MAX,
+    STRATIS_ERROR,
+    STRATIS_NULL,
+    STRATIS_MALLOC,
+    STRATIS_NOTFOUND,
+    STRATIS_POOL_NOTFOUND,
+    STRATIS_VOLUME_NOTFOUND,
+    STRATIS_DEV_NOTFOUND,
+    STRATIS_CACHE_NOTFOUND,
+    STRATIS_BAD_PARAM,
+    STRATIS_ALREADY_EXISTS,
+    STRATIS_NULL_NAME,
+    STRATIS_NO_POOLS,
+    STRATIS_LIST_FAILURE,
+    STRATIS_ERROR_MAX,
 }
 
 
 pub enum StratisRaidType {
-	STRATIS_RAID_TYPE_UNKNOWN,
-	/** Single */
-	STRATIS_RAID_TYPE_SINGLE,
-	/** Mirror between two disks. For 4 disks or more, they are RAID10.*/
-	STRATIS_RAID_TYPE_RAID1,
-	/** Block-level striping with distributed parity */
-	STRATIS_RAID_TYPE_RAID5,
-	/** Block-level striping with two distributed parities, aka, RAID-DP */
-	STRATIS_RAID_TYPE_RAID6,
-	STRATIS_RAID_TYPE_MAX,
+    STRATIS_RAID_TYPE_UNKNOWN,
+    /** Single */
+    STRATIS_RAID_TYPE_SINGLE,
+    /** Mirror between two disks. For 4 disks or more, they are RAID10.*/
+    STRATIS_RAID_TYPE_RAID1,
+    /** Block-level striping with distributed parity */
+    STRATIS_RAID_TYPE_RAID5,
+    /** Block-level striping with two distributed parities, aka, RAID-DP */
+    STRATIS_RAID_TYPE_RAID6,
+    STRATIS_RAID_TYPE_MAX,
 }

d0b900b9583ab59d59f04732777b1e9fcc16f42d
mulhern <amulhern@redhat.com>
2016-10-05 15:53:04 -0400

    Alphabetize "use" directives.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/sim_engine.rs
+++ src/sim_engine.rs
@@ -2,12 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use blockdev::BlockDevs;
+
+use engine::Engine;
+
+use pool::StratisPool;
+
 use std::path::Path;
 use std::collections::BTreeMap;
+
 use types::StratisResult;
-use engine::Engine;
-use pool::StratisPool;
-use blockdev::BlockDevs;
+
 
 
 pub struct SimEngine {

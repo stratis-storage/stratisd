5c2cdbf0c8e12734db29f186c25a85f257821973
mulhern <amulhern@redhat.com>
2016-10-27 10:09:10 -0400

    Add the connection facilities.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ src/stratisd_client_dbus/_connection.py
@@ -0,0 +1,48 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Miscellaneous helpful methods.
+"""
+
+import dbus
+
+
+class Bus(object):
+    """
+    Our bus.
+    """
+    # pylint: disable=too-few-public-methods
+
+    _BUS = None
+
+    @staticmethod
+    def get_bus():
+        """
+        Get our bus.
+        """
+        if Bus._BUS is None:
+            Bus._BUS = dbus.SessionBus()
+
+        return Bus._BUS
+
+def get_object(object_path):
+    """
+    Get an object from an object path.
+
+    :param str object_path: an object path with a valid format
+    :returns: the proxy object corresponding to the object path
+    :rtype: ProxyObject
+    """
+    return Bus.get_bus().get_object("org.storage.stratis1", object_path)

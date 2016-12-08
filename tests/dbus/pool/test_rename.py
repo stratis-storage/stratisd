ecd60be8c062eccbbb04dc8bbba2de5007f96f99
mulhern <amulhern@redhat.com>
2016-12-08 10:26:56 -0500

    Add a few tests for Pool's Rename() method.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ tests/dbus/pool/test_rename.py
@@ -0,0 +1,121 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test renaming a pool.
+"""
+
+import time
+import unittest
+
+from stratisd_client_dbus import Manager
+from stratisd_client_dbus import Pool
+from stratisd_client_dbus import StratisdErrorsGen
+from stratisd_client_dbus import get_object
+
+from stratisd_client_dbus._constants import TOP_OBJECT
+
+from stratisd_client_dbus._implementation import ManagerSpec
+from stratisd_client_dbus._implementation import PoolSpec
+
+from .._misc import checked_call
+from .._misc import _device_list
+from .._misc import Service
+
+_MN = ManagerSpec.MethodNames
+_PN = PoolSpec.MethodNames
+
+_DEVICE_STRATEGY = _device_list(0)
+
+
+class RenameTestCase(unittest.TestCase):
+    """
+    Set up a pool with a name.
+    """
+
+    _POOLNAME = 'deadpool'
+
+    def setUp(self):
+        """
+        Start the stratisd daemon with the simulator.
+        """
+        self._service = Service()
+        self._service.setUp()
+        time.sleep(1)
+        self._proxy = get_object(TOP_OBJECT)
+        self._errors = StratisdErrorsGen.get_object()
+        (self._pool_object_path, _, _) = Manager.CreatePool(
+           self._proxy,
+           name=self._POOLNAME,
+           redundancy=0,
+           force=False,
+           devices=_DEVICE_STRATEGY.example()
+        )
+        self._pool_object = get_object(self._pool_object_path)
+        Manager.ConfigureSimulator(self._proxy, denominator=8)
+
+    def tearDown(self):
+        """
+        Stop the stratisd simulator and daemon.
+        """
+        self._service.tearDown()
+
+    def testNullMapping(self):
+        """
+        Test rename to same name.
+        """
+        (result, rc, _) = checked_call(
+           Pool.Rename(self._pool_object, new_name=self._POOLNAME),
+           PoolSpec.OUTPUT_SIGS[_PN.Rename]
+        )
+
+        self.assertEqual(rc, self._errors.OK)
+        self.assertFalse(result)
+
+        (result, rc, _) = checked_call(
+           Manager.GetPoolObjectPath(self._proxy, name=self._POOLNAME),
+           ManagerSpec.OUTPUT_SIGS[_MN.GetPoolObjectPath],
+        )
+
+        self.assertEqual(rc, self._errors.OK)
+        self.assertEqual(result, self._pool_object_path)
+
+    def testNewName(self):
+        """
+        Test rename to new name.
+        """
+        new_name = "new"
+
+        (result, rc, _) = checked_call(
+           Pool.Rename(self._pool_object, new_name=new_name),
+           PoolSpec.OUTPUT_SIGS[_PN.Rename]
+        )
+
+        self.assertTrue(result)
+        self.assertEqual(rc, self._errors.OK)
+
+        (result, rc, _) = checked_call(
+           Manager.GetPoolObjectPath(self._proxy, name=self._POOLNAME),
+           ManagerSpec.OUTPUT_SIGS[_MN.GetPoolObjectPath],
+        )
+
+        self.assertEqual(rc, self._errors.POOL_NOTFOUND)
+
+        (result, rc, _) = checked_call(
+           Manager.GetPoolObjectPath(self._proxy, name=new_name),
+           ManagerSpec.OUTPUT_SIGS[_MN.GetPoolObjectPath],
+        )
+
+        self.assertEqual(rc, self._errors.OK)
+        self.assertEqual(result, self._pool_object_path)

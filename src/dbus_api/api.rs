07820c0f44cf64dfcf396d31535e0f85cea1b78e
mulhern <amulhern@redhat.com>
2017-01-26 11:17:38 -0500

    Split dbus_api into types and api modules.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus_api/api.rs
+++ src/dbus_api/api.rs
@@ -2,15 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use bidir_map::BidirMap;
-
 use std::borrow::Cow;
-use std::cell::Cell;
-use std::cell::RefCell;
 use std::convert::From;
 use std::fmt::Display;
 use std::path::Path;
-use std::rc::Rc;
 use std::vec::Vec;
 use std::error::Error;
 
@@ -27,14 +22,12 @@ use dbus::arg::IterAppend;
 use dbus::tree::Access;
 use dbus::tree::EmitsChangedSignal;
 use dbus::tree::Factory;
-use dbus::tree::DataType;
 use dbus::tree::MethodErr;
 use dbus::tree::MTFn;
 use dbus::tree::MethodResult;
 use dbus::tree::MethodInfo;
 use dbus::tree::PropInfo;
 use dbus::tree::Tree;
-use dbus::tree::ObjectPath;
 use dbus::ConnectionItem;
 
 use super::super::types::Bytes;
@@ -48,113 +41,10 @@ use engine::RenameAction;
 
 use types::StratisResult;
 
+use super::types::{DeferredAction, DbusContext, DbusErrorEnum, TData};
+
 const STRATIS_BASE_PATH: &'static str = "/org/storage/stratis1";
 const STRATIS_BASE_SERVICE: &'static str = "org.storage.stratis1";
-
-custom_derive! {
-    #[derive(Copy, Clone, EnumDisplay,
-             IterVariants(StratisDBusErrorVariants),
-             IterVariantNames(StratisDBusErrorVariantNames))]
-    #[allow(non_camel_case_types)]
-    pub enum DbusErrorEnum {
-        OK,
-        ERROR,
-
-        ALREADY_EXISTS,
-        BAD_PARAM,
-        BUSY,
-        CACHE_NOTFOUND,
-        DEV_NOTFOUND,
-        FILESYSTEM_NOTFOUND,
-        IO_ERROR,
-        LIST_FAILURE,
-        INTERNAL_ERROR,
-        NIX_ERROR,
-        NO_POOLS,
-        NOTFOUND,
-        NULL,
-        NULL_NAME,
-        POOL_NOTFOUND,
-    }
-}
-
-/// Get the u16 value of this ErrorEnum constructor.
-impl From<DbusErrorEnum> for u16 {
-    fn from(e: DbusErrorEnum) -> u16 {
-        e as u16
-    }
-}
-
-impl DbusErrorEnum {
-    pub fn get_error_string(&self) -> &str {
-        match *self {
-            DbusErrorEnum::OK => "Ok",
-            DbusErrorEnum::ERROR => "A general error happened",
-            DbusErrorEnum::NULL => "Null parameter was supplied",
-            DbusErrorEnum::NOTFOUND => "Not found",
-            DbusErrorEnum::POOL_NOTFOUND => "Pool not found",
-            DbusErrorEnum::FILESYSTEM_NOTFOUND => "Filesystem not found",
-            DbusErrorEnum::CACHE_NOTFOUND => "Cache not found",
-            DbusErrorEnum::BAD_PARAM => "Bad parameter",
-            DbusErrorEnum::DEV_NOTFOUND => "Dev not found",
-            DbusErrorEnum::ALREADY_EXISTS => "Already exists",
-            DbusErrorEnum::NULL_NAME => "Null name supplied",
-            DbusErrorEnum::NO_POOLS => "No pools",
-            DbusErrorEnum::LIST_FAILURE => "List operation failure",
-            DbusErrorEnum::INTERNAL_ERROR => "Internal error",
-            DbusErrorEnum::IO_ERROR => "IO error during operation",
-            DbusErrorEnum::NIX_ERROR => "System error during operation",
-            DbusErrorEnum::BUSY => "Operation can not be performed at this time",
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum DeferredAction {
-    Add(ObjectPath<MTFn<TData>, TData>),
-    Remove(String),
-}
-
-#[derive(Debug, Clone)]
-pub struct DbusContext {
-    pub next_index: Rc<Cell<u64>>,
-    pub pools: Rc<RefCell<BidirMap<String, String>>>,
-    pub engine: Rc<RefCell<Box<Engine>>>,
-    pub action_list: Rc<RefCell<Vec<DeferredAction>>>,
-    pub filesystems: Rc<RefCell<BidirMap<String, (String, String)>>>,
-}
-
-impl DbusContext {
-    pub fn new(engine: Box<Engine>) -> DbusContext {
-        DbusContext {
-            action_list: Rc::new(RefCell::new(Vec::new())),
-            engine: Rc::new(RefCell::new(engine)),
-            filesystems: Rc::new(RefCell::new(BidirMap::new())),
-            next_index: Rc::new(Cell::new(0)),
-            pools: Rc::new(RefCell::new(BidirMap::new())),
-        }
-    }
-
-    /// Generates a new id for object paths.
-    /// It is assumed that, while Stratisd is running, it will never generate
-    /// more than 2^64 object paths. If it turns out that this is a bad
-    /// assumption, the solution is to use unbounded integers.
-    pub fn get_next_id(&self) -> u64 {
-        self.next_index.set(self.next_index.get() + 1);
-        self.next_index.get()
-    }
-}
-
-#[derive(Default, Debug)]
-pub struct TData;
-impl DataType for TData {
-    type ObjectPath = ();
-    type Property = ();
-    type Interface = ();
-    type Method = ();
-    type Signal = ();
-    type Tree = DbusContext;
-}
 
 /// Convert a tuple as option to an Option type
 fn tuple_to_option<T>(value: (bool, T)) -> Option<T> {

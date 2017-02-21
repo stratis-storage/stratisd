d4ec66ff6f701929dbcbe37c9eeb6536b406d357
mulhern <amulhern@redhat.com>
2017-02-21 09:51:11 -0500

    Use an if-let construction in a macro
    It is less cumbersome and requires less indentation.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus_api/macros.rs
+++ src/dbus_api/macros.rs
@@ -6,16 +6,16 @@
 /// Macro for early return with Ok dbus message on failure to get pool.
 macro_rules! get_pool {
     ( $engine:ident; $uuid:ident; $default:expr; $message:expr ) => {
-        match $engine.get_pool(&$uuid) {
-            Some(pool) => pool,
-            None => {
-                let (rc, rs) = code_to_message_items(DbusErrorEnum::POOL_NOTFOUND,
-                                                     format!("no pool for uuid {}", $uuid));
-                return Ok(vec![$message.append3($default, rc, rs)]);
-            }
+        if let Some(pool) = $engine.get_pool(&$uuid) {
+            pool
+        } else {
+            let (rc, rs) = code_to_message_items(DbusErrorEnum::POOL_NOTFOUND,
+                                                 format!("no pool for uuid {}", $uuid));
+            return Ok(vec![$message.append3($default, rc, rs)]);
         }
     }
 }
+
 /// Macros for early return with an Ok dbus message on a dbus error
 macro_rules! dbus_try {
     ( $val:expr; $default:expr; $message:expr ) => {

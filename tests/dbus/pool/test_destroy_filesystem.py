341f2cbdafd7f335076ac5e8ac844b77cc410f2a
mulhern <amulhern@redhat.com>
2017-02-22 15:10:15 -0500

    Update DestroyFilesystem tests.
    The parameter is now a list of object paths to be destroyed.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/dbus/pool/test_destroy_filesystem.py
+++ tests/dbus/pool/test_destroy_filesystem.py
@@ -78,7 +78,7 @@ class DestroyFSTestCase(unittest.TestCase):
         number of volumes.
         """
         (result, rc, _) = checked_call(
-           Pool.DestroyFilesystems(self._pool_object, names=[]),
+           Pool.DestroyFilesystems(self._pool_object, filesystems=[]),
            PoolSpec.OUTPUT_SIGS[_PN.DestroyFilesystems]
         )
 
@@ -90,15 +90,15 @@ class DestroyFSTestCase(unittest.TestCase):
 
     def testDestroyOne(self):
         """
-        Test calling with a non-existant volume name. This should succeed,
-        because at the end the volume is not there.
+        Test calling with a non-existant object path. This should succeed,
+        because at the end the filesystem is not there.
         """
         (result, rc, _) = checked_call(
-           Pool.DestroyFilesystems(self._pool_object, names=['name']),
+           Pool.DestroyFilesystems(self._pool_object, filesystems=['/']),
            PoolSpec.OUTPUT_SIGS[_PN.DestroyFilesystems]
         )
-        self.assertEqual(len(result), 0)
         self.assertEqual(rc, self._errors.OK)
+        self.assertEqual(len(result), 0)
 
         result = [x for x in get_managed_objects(self._proxy).filesystems()]
         self.assertEqual(len(result), 0)
@@ -122,15 +122,15 @@ class DestroyFSTestCase1(unittest.TestCase):
         self._proxy = get_object(TOP_OBJECT)
         self._errors = StratisdErrorsGen.get_object()
         self._devs = _DEVICE_STRATEGY.example()
-        ((poolpath, _), _, _) = Manager.CreatePool(
+        ((self._poolpath, _), _, _) = Manager.CreatePool(
            self._proxy,
            name=self._POOLNAME,
            redundancy=0,
            force=False,
            devices=self._devs
         )
-        self._pool_object = get_object(poolpath)
-        Pool.CreateFilesystems(
+        self._pool_object = get_object(self._poolpath)
+        (self._filesystems, _, _) = Pool.CreateFilesystems(
            self._pool_object,
            specs=[(self._VOLNAME, '', None)]
         )
@@ -144,17 +144,20 @@ class DestroyFSTestCase1(unittest.TestCase):
 
     def testDestroyOne(self):
         """
-        Test calling by specifying the volume name. Assume that destruction
+        Test calling by specifying the object path. Assume that destruction
         should always succeed.
         """
+        fs_object_path = self._filesystems[0][0]
         (result, rc, _) = checked_call(
-           Pool.DestroyFilesystems(self._pool_object, names=[self._VOLNAME]),
+           Pool.DestroyFilesystems(
+              self._pool_object,
+              filesystems=[fs_object_path]
+           ),
            PoolSpec.OUTPUT_SIGS[_PN.DestroyFilesystems]
         )
 
         self.assertEqual(len(result), 1)
         self.assertEqual(rc, self._errors.OK)
-        self.assertEqual(result[0], self._VOLNAME)
 
         result = [x for x in get_managed_objects(self._proxy).filesystems()]
         self.assertEqual(len(result), 0)
@@ -165,16 +168,17 @@ class DestroyFSTestCase1(unittest.TestCase):
         non-existing. Should succeed, but only the existing name should be
         returned.
         """
+        fs_object_path = self._filesystems[0][0]
         (result, rc, _) = checked_call(
            Pool.DestroyFilesystems(
-              self._pool_object, names=[self._VOLNAME, "bogus"]
+              self._pool_object,
+              filesystems=[fs_object_path, "/"]
            ),
            PoolSpec.OUTPUT_SIGS[_PN.DestroyFilesystems]
         )
 
         self.assertEqual(len(result), 1)
         self.assertEqual(rc, self._errors.OK)
-        self.assertEqual(result[0], self._VOLNAME)
 
         result = [x for x in get_managed_objects(self._proxy).filesystems()]
         self.assertEqual(len(result), 0)

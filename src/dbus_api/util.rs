fa88d8d443f4447ec618714ea4e71d4ae1e4b26d
mulhern <amulhern@redhat.com>
2017-03-22 16:51:50 -0400

    Use generic get_parent() method for get_filesystem_pool
    This will be more useful when object paths for devices are added,
    as they will also have a parent.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus_api/util.rs
+++ src/dbus_api/util.rs
@@ -90,3 +90,15 @@ pub fn get_uuid(i: &mut IterAppend, p: &PropInfo<MTFn<TData>, TData>) -> Result<
     i.append(MessageItem::Str(format!("{}", data.uuid.simple())));
     Ok(())
 }
+
+
+/// Get the parent object path for an object path.
+pub fn get_parent(i: &mut IterAppend, p: &PropInfo<MTFn<TData>, TData>) -> Result<(), MethodErr> {
+    let object_path = p.path.get_name();
+    let path = p.tree.get(&object_path).expect("implicit argument must be in tree");
+    let data = try!(ref_ok_or(path.get_data(),
+                              MethodErr::failed(&format!("no data for object path {}",
+                                                         &object_path))));
+    i.append(MessageItem::ObjectPath(data.parent.clone()));
+    Ok(())
+}

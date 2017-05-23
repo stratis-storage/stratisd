fbe45a564657e482167d001e08ef1a76879d7296
mulhern <amulhern@redhat.com>
2017-05-23 14:20:51 -0400

    Return the segments that back the mdv device
    These are needed in order to construct metadata.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/mdv.rs
+++ src/engine/strat_engine/mdv.rs
@@ -12,7 +12,7 @@ use std::io::prelude::*;
 use std::os::unix::io::AsRawFd;
 use std::path::PathBuf;
 
-use devicemapper::{LinearDev, DM};
+use devicemapper::{LinearDev, DM, Segment};
 use serde_json;
 use nix::unistd::fsync;
 
@@ -149,6 +149,11 @@ impl MetadataVol {
         }
 
         Ok(filesystems)
+    }
+
+    /// Return the segments used.
+    pub fn segments(&self) -> &[Segment] {
+        self.dev.segments()
     }
 
     /// Tear down a Metadata Volume.

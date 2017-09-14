314966ab3e253c00d2596512ede758cfd19a9d8f
mulhern <amulhern@redhat.com>
2017-09-14 11:03:38 -0400

    Get rid of newly introduced clippy clone_on_ref_ptr error
    This is really not the same clone as Clone implementations, and it _is_
    helpful to visually distinguish.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/sim_engine/engine.rs
+++ src/engine/sim_engine/engine.rs
@@ -47,7 +47,7 @@ impl Engine for SimEngine {
             .map(|x| *x)
             .collect::<Vec<&Path>>();
 
-        let pool = SimPool::new(self.rdm.clone(), name, &devices, redundancy);
+        let pool = SimPool::new(Rc::clone(&self.rdm), name, &devices, redundancy);
 
         if self.rdm.borrow_mut().throw_die() {
             return Err(EngineError::Engine(ErrorEnum::Error, "X".into()));

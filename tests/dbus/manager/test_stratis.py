01cd11125f1179f6d7d36e5c2fe140b555db2797
mulhern <amulhern@redhat.com>
2017-11-06 16:36:22 -0500

    Add some tests for exceptions raised by dbus-python-client-gen
    The idea is to differentiate among the various types of exceptions that
    might be raised by various invocation mistakes.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/dbus/manager/test_stratis.py
+++ tests/dbus/manager/test_stratis.py
@@ -19,7 +19,10 @@ Test 'stratisd'.
 import time
 import unittest
 
+from dbus_python_client_gen import DPClientInvalidArgError
+
 from stratisd_client_dbus import Manager
+from stratisd_client_dbus import ObjectManager
 from stratisd_client_dbus import get_object
 
 from stratisd_client_dbus._constants import TOP_OBJECT
@@ -57,3 +60,52 @@ class StratisTestCase(unittest.TestCase):
         version = Manager.Properties.Version.Get(get_object(TOP_OBJECT))
         (major, _, _) = version.split(".")
         self.assertEqual(major, "0")
+
+class StratisTestCase2(unittest.TestCase):
+    """
+    Test exceptions raised by various errors.
+    """
+
+    def setUp(self):
+        """
+        Start the stratisd daemon with the simulator.
+        """
+        self._service = Service()
+        self._service.setUp()
+        time.sleep(1)
+        self._proxy = get_object(TOP_OBJECT)
+        Manager.Methods.ConfigureSimulator(self._proxy, {'denominator': 8})
+
+    def tearDown(self):
+        """
+        Stop the stratisd simulator and daemon.
+        """
+        self._service.tearDown()
+
+    def testArguments(self):
+        """
+        Incorrect arguments should cause a type error.
+        """
+        with self.assertRaises(TypeError):
+            Manager.Properties.Version.Get(get_object(TOP_OBJECT), {})
+
+    def testFunctionName(self):
+        """
+        We know that it is impossible to set the Stratis version, so Set
+        method should not exist, and this should result in an Attribute error.
+        """
+        with self.assertRaises(AttributeError):
+            Manager.Properties.Version.Set(get_object(TOP_OBJECT), {})
+
+    def testFunctionArgs(self):
+        """
+        If the arguments to the D-Bus method are incorrect, the exception is
+        a DPClientInvalidArgError.
+
+        Incorrectness can be caused by incorrect keyword args, but also
+        by incorrect type of argument.
+        """
+        with self.assertRaises(DPClientInvalidArgError):
+            ObjectManager.Methods.GetManagedObjects(self._proxy, {'bogus': 2})
+        with self.assertRaises(DPClientInvalidArgError):
+            Manager.Methods.DestroyPool(self._proxy, {'pool': 2})

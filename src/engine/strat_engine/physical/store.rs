20cd13f9efc27d39e665539354805aec902c8f40
mulhern <amulhern@redhat.com>
2018-01-22 19:07:14 -0500

    Add comments to all the methods that are considered complete
    Even with the additional DM layer, these methods should not need to be
    changed.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/physical/store.rs
+++ src/engine/strat_engine/physical/store.rs
@@ -51,10 +51,16 @@ impl Store {
         self.block_mgr.alloc_space(sizes)
     }
 
+    /// Return a reference to the blockdevs that form the base of the physical
+    /// layer.
     pub fn blockdevs(&self) -> Vec<(DevUuid, &BlockDev)> {
         self.block_mgr.blockdevs()
     }
 
+    /// The current capacity of all the blockdevs that make up the physical
+    /// layer. This should be greater than the size of the dm device, which
+    /// does not include the sectors of the blockdevs that contain Stratis
+    /// metadata.
     pub fn current_capacity(&self) -> Sectors {
         self.block_mgr.current_capacity()
     }
@@ -63,22 +69,35 @@ impl Store {
         self.block_mgr.destroy_all()
     }
 
+    /// Lookup an immutable blockdev by its Stratis UUID.
+    // Used for getting properties of a blockdev via the D-Bus.
     pub fn get_blockdev_by_uuid(&self, uuid: DevUuid) -> Option<&BlockDev> {
         self.block_mgr.get_blockdev_by_uuid(uuid)
     }
 
+    /// Lookup a mutable blockdev by its Stratis UUID.
+    // Used for setting properties of a blockdev via the D-Bus.
     pub fn get_mut_blockdev_by_uuid(&mut self, uuid: DevUuid) -> Option<&mut BlockDev> {
         self.block_mgr.get_mut_blockdev_by_uuid(uuid)
     }
 
+    /// the number of sectors in the physical layer given up to Stratis
+    /// metadata. current_capacity() - metadata_size() >= the size of the
+    /// DM device.
     pub fn metadata_size(&self) -> Sectors {
         self.block_mgr.metadata_size()
     }
 
+    /// Write the given data directly to the blockdevs that make up the
+    /// physical layer. This action bypasses the DM device entirely,
+    /// in order to allow control over which blockdevs the metadata is written
+    /// to.
     pub fn save_state(&mut self, metadata: &[u8]) -> EngineResult<()> {
         self.block_mgr.save_state(metadata)
     }
 
+    /// Map a Stratis blockdev uuid to a corresponding devnode.
+    // Used during setup to construct devicemapper tables from Stratis metadata.
     pub fn uuid_to_devno(&self) -> Box<Fn(DevUuid) -> Option<Device>> {
         self.block_mgr.uuid_to_devno()
     }

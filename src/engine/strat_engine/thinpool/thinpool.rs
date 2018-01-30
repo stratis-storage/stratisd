fa31a3c34ad1a71d0abe34d9f74a2c2d4cff0ea0
mulhern <amulhern@redhat.com>
2018-01-30 10:40:18 -0500

    Make all module-level comments non-document comments
    This seems like the best way to avoid the clippy
    empty_line_after_outer_attr error occasioned by some of these comments.
    We do not have all that many such comments anyway.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/thinpool/thinpool.rs
+++ src/engine/strat_engine/thinpool/thinpool.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-/// Code to handle management of a pool's thinpool device.
+// Code to handle management of a pool's thinpool device.
 
 use std::borrow::BorrowMut;
 use std::cmp;

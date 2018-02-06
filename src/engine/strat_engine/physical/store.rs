cdbb1746828315176d13ad11d0ef0ebb52fae2b4
mulhern <amulhern@redhat.com>
2018-02-06 16:54:50 -0500

    Make a struct to hold combination of BlockDevMgr and LinearDev
    For better encapsulation.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/physical/store.rs
+++ src/engine/strat_engine/physical/store.rs
@@ -9,16 +9,109 @@ use std::path::Path;
 
 use chrono::{DateTime, Utc};
 
-use devicemapper::{Device, Sectors};
+use devicemapper::{Device, DM, DmDevice, LinearDev, Sectors};
 
 use super::super::super::engine::BlockDev;
 use super::super::super::errors::EngineResult;
 use super::super::super::types::{DevUuid, PoolUuid};
 
+use super::super::dmnames::{PhysicalRole, format_physical_name};
 use super::super::serde_structs::{Recordable, BlockDevSave};
 
 use super::blockdev::StratBlockDev;
-use super::blockdevmgr::{BlkDevSegment, BlockDevMgr};
+use super::blockdevmgr::{BlkDevSegment, BlockDevMgr, get_coalesced_segments, map_to_dm};
+
+/// Handles the lowest level, base layer of this tier.
+/// The dm_device organizes all block devs into a single linear allocation pool.
+#[derive(Debug)]
+struct DataLayer {
+    /// Manages the individual block devices
+    block_mgr: BlockDevMgr,
+    /// The device mapper device which aggregates all block_mgr's devices
+    dm_device: LinearDev,
+    /// The list of segments granted by block_mgr and used by dm_device
+    segments: Vec<BlkDevSegment>,
+    /// The position from which requested space is allocated
+    next: Sectors,
+}
+
+#[allow(dead_code)]
+impl DataLayer {
+    /// Setup a new DataLayer struct from the block_mgr.
+    /// Note that this is a metadata changing event.
+    pub fn setup(dm: &DM, mut block_mgr: BlockDevMgr) -> EngineResult<DataLayer> {
+        let avail_space = block_mgr.avail_space();
+        let segments = block_mgr
+            .alloc_space(&[avail_space])
+            .expect("asked for exactly the space available, must get")
+            .iter()
+            .flat_map(|s| s.iter())
+            .cloned()
+            .collect::<Vec<_>>();
+        let ld = LinearDev::setup(dm,
+                                  &format_physical_name(block_mgr.pool_uuid(),
+                                                        PhysicalRole::Origin),
+                                  None,
+                                  map_to_dm(&segments))?;
+        Ok(DataLayer {
+               block_mgr,
+               dm_device: ld,
+               segments,
+               next: Sectors(0),
+           })
+    }
+
+    /// Add the given paths to self. Return UUIDs of the new blockdevs
+    /// corresponding to the specified paths.
+    /// Note that this is a metadata changing event.
+    pub fn add(&mut self, dm: &DM, paths: &[&Path], force: bool) -> EngineResult<Vec<DevUuid>> {
+        let uuids = self.block_mgr.add(paths, force)?;
+
+        let avail_space = self.block_mgr.avail_space();
+        let segments = self.block_mgr
+            .alloc_space(&[avail_space])
+            .expect("asked for exactly the space available, must get")
+            .iter()
+            .flat_map(|s| s.iter())
+            .cloned()
+            .collect::<Vec<_>>();
+        let coalesced = get_coalesced_segments(&self.segments, &segments);
+
+        self.dm_device.set_table(dm, map_to_dm(&coalesced))?;
+
+        self.segments = coalesced;
+
+        Ok(uuids)
+    }
+
+    /// All the sectors available to this device
+    // Note that this should always be equivalent to the sum of the length
+    // fields of the segments in self.segments.
+    pub fn capacity(&self) -> Sectors {
+        self.dm_device.size()
+    }
+
+    /// Number of sectors unused
+    pub fn available(&self) -> Sectors {
+        self.capacity() - self.next
+    }
+
+    /// Allocate requested chunks from device.
+    /// Returns None if it is not possible to satisfy the request.
+    // Simply serves up the space in the order in which it was requested.
+    pub fn alloc_space(&mut self, sizes: &[Sectors]) -> Option<Vec<Vec<(Sectors, Sectors)>>> {
+        if self.available() < sizes.iter().cloned().sum() {
+            return None;
+        }
+
+        let mut chunks = Vec::new();
+        for size in sizes {
+            chunks.push(vec![(self.next, *size)]);
+            self.next += *size;
+        }
+        Some(chunks)
+    }
+}
 
 #[derive(Debug)]
 pub struct Store {

fd29df250e00ef354457957f33436bfe907072b4
mulhern <amulhern@redhat.com>
2018-02-08 10:05:45 -0500

    Fix up setup.py
    Remove unnecessary or inappropriate lines and fix up obsoleted ones.
    Do not get rid of the file, because tox makes use of it.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/client-dbus/setup.py
+++ tests/client-dbus/setup.py
@@ -14,31 +14,15 @@ with open(local_file("src/stratisd_client_dbus/_version.py")) as o:
 
 setuptools.setup(
     name='stratisd-client-dbus',
-    version=__version__,
     author='Anne Mulhern',
     author_email='amulhern@redhat.com',
-    description='transforms values into properly wrapped dbus-python objects',
+    description='testing library for stratisd',
     long_description=open(README, encoding='utf-8').read(),
     platforms=['Linux'],
-    license='Apache 2.0',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
     install_requires = [
        'dbus-client-gen>=0.2',
        'dbus-python-client-gen>=0.6',
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
-    url="https://github.com/stratis-storage/stratisd-client-dbus"
     )

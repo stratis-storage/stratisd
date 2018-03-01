3492833f36a5cb4689b60cb823f13ac83cf78e6b
Todd Gill <tgill@redhat.com>
2018-03-01 16:59:05 -0500

    Add @unittest.skip to the tests broken by only allowing single filesystem
    When we add support for multiple filesystems back, we should remove these
    tags.
    
    Signed-off-by: Todd Gill <tgill@redhat.com>

--- tests/client-dbus/tests/dbus/pool/test_create_filesystem.py
+++ tests/client-dbus/tests/dbus/pool/test_create_filesystem.py
@@ -87,6 +87,7 @@ class CreateFSTestCase(unittest.TestCase):
            filesystems(ObjectManager.Methods.GetManagedObjects(self._proxy, {}))
         self.assertEqual(len([x for x in result]), 0)
 
+    @unittest.skip("skip until creating multiple filesystems is supported")
     def testDuplicateSpecs(self):
         """
         Test calling with duplicate specification for same filesystem name.
@@ -188,6 +189,7 @@ class CreateFSTestCase1(unittest.TestCase):
            filesystems(ObjectManager.Methods.GetManagedObjects(self._proxy, {}))
         self.assertEqual(len([x for x in result]), 2)
 
+    @unittest.skip("skip until creating multiple filesystems is supported")
     def testCreateWithConflict(self):
         """
         Test calling by specifying several volumes. Because there is already

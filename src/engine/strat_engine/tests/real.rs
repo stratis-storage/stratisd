f2dd70c0aba64984791a8a9c59e85affef1cf754
mulhern <amulhern@redhat.com>
2018-05-21 10:48:58 -0400

    Add a min size and a max size to DeviceLimits constructors
    Adapt all tests to use these extended constructors.
    
    Export blkdev_size from backstore module for use in tests.
    
    Change RealTestDev so that its value is either a PahtBuf or a LinearDev.
    Update RealTestDev::as_path() accordingly.
    
    Teardown devices explicitly after test execution instead of using the Drop
    trait.
    
    Change get_device_counts to get_device_runs(). Instead of a list of counts,
    the method now returns a list of device specifications. It takes instead of
    the number of available devices, a table of the device paths and their sizes.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/tests/real.rs
+++ src/engine/strat_engine/tests/real.rs
@@ -2,92 +2,181 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+extern crate either;
+
 use std::fs::OpenOptions;
 use std::path::{Path, PathBuf};
 use std::{cmp, panic};
 
+use self::either::Either;
 use serde_json::{from_reader, Value};
+use uuid::Uuid;
 
-use devicemapper::{Bytes, Sectors, IEC};
+use devicemapper::{devnode_to_devno, Bytes, Device, DmDevice, DmName, LinearDev,
+                   LinearDevTargetParams, LinearTargetParams, Sectors, TargetLine, IEC};
+
+use super::super::backstore::blkdev_size;
+use super::super::device::wipe_sectors;
+use super::super::dm::get_dm;
 
 use super::logger::init_logger;
 use super::util::clean_up;
 
-use super::super::device::wipe_sectors;
-
 pub struct RealTestDev {
-    path: PathBuf,
+    dev: Either<PathBuf, LinearDev>,
 }
 
 impl RealTestDev {
     /// Construct a new test device for the given path.
     /// Wipe initial MiB to clear metadata.
-    pub fn new(path: &Path) -> RealTestDev {
-        wipe_sectors(path, Sectors(0), Bytes(IEC::Mi).sectors()).unwrap();
-        RealTestDev {
-            path: PathBuf::from(path),
-        }
+    pub fn new(dev: Either<PathBuf, LinearDev>) -> RealTestDev {
+        let test_dev = RealTestDev { dev };
+        wipe_sectors(test_dev.as_path(), Sectors(0), Bytes(IEC::Mi).sectors()).unwrap();
+        test_dev
     }
 
     /// Get the device node of the device.
-    fn as_path(&self) -> &Path {
-        &self.path
+    fn as_path(&self) -> PathBuf {
+        self.dev.as_ref().either(|p| p.clone(), |l| l.devnode())
     }
-}
 
-impl Drop for RealTestDev {
-    fn drop(&mut self) {
-        wipe_sectors(&self.path, Sectors(0), Bytes(IEC::Mi).sectors()).unwrap();
+    /// Teardown a real test dev
+    fn teardown(self) -> () {
+        wipe_sectors(&self.as_path(), Sectors(0), Bytes(IEC::Mi).sectors()).unwrap();
+        if let Some(ld) = self.dev.right() {
+            ld.teardown(get_dm()).unwrap();
+        }
     }
 }
 
 /// Shorthand for specifying devices to use for tests
+/// For the minimum size, if None is given, the default 1 GiB is chosen.
 pub enum DeviceLimits {
     /// Use exactly the number of devices specified
-    Exactly(usize),
+    /// The second argument is the minimum size for all devices.
+    /// The third argument is the maximum size for all devices.
+    Exactly(usize, Option<Sectors>, Option<Sectors>),
     /// Use at least the number of devices specified, but if there are more
     /// devices available, also use the maximum number of devices.
-    AtLeast(usize),
+    /// The second argument is the minimum size for all devices.
+    /// The third argument is the maximum size for all devices.
+    AtLeast(usize, Option<Sectors>, Option<Sectors>),
     /// Use exactly the number of devices specified in the first argument,
     /// and the minimum of the number of devices specified and the number
     /// of devices available in the second argument.
-    Range(usize, usize),
+    /// The third argument is the minimum size for all devices.
+    /// The fourth argument is the maximum size for all devices.
+    Range(usize, usize, Option<Sectors>, Option<Sectors>),
 }
 
-/// Get a list of counts of devices to use for tests.
-/// None of the counts can be greater than avail.
-fn get_device_counts(limits: DeviceLimits, avail: usize) -> Vec<usize> {
-    // Convert enum to [lower, Option<upper>) values
-    let (lower, maybe_upper) = match limits {
-        DeviceLimits::Exactly(num) => (num, Some(num + 1)),
-        DeviceLimits::AtLeast(num) => (num, None),
-        DeviceLimits::Range(lower, upper) => {
+/// Get a list of lists of devices to use for tests.
+/// May return an empty list if the request is not satisfiable.
+fn get_device_runs<'a>(
+    limits: DeviceLimits,
+    dev_sizes: &[(&'a Path, Sectors)],
+) -> Vec<Vec<(&'a Path, Option<(Sectors, Sectors)>)>> {
+    let (lower, maybe_upper, min_size, max_size) = match limits {
+        DeviceLimits::Exactly(num, min_size, max_size) => (num, Some(num), min_size, max_size),
+        DeviceLimits::AtLeast(num, min_size, max_size) => (num, None, min_size, max_size),
+        DeviceLimits::Range(lower, upper, min_size, max_size) => {
             assert!(lower < upper);
-            (lower, Some(upper + 1))
+            (lower, Some(upper), min_size, max_size)
         }
     };
 
-    let mut counts = vec![];
+    let min_size = min_size.unwrap_or(Bytes(IEC::Gi).sectors());
 
-    // Check these values against available blockdevs
-    if lower > avail {
-        return counts;
+    assert!(max_size.is_none() || Some(min_size) <= max_size);
+
+    // Retain only devices that are larger than min_size
+    let (matches, _): (Vec<(&Path, Sectors)>, Vec<(&Path, Sectors)>) =
+        dev_sizes.iter().partition(|&(_, s)| *s >= min_size);
+
+    // If there is not a sufficient number of devices large enough to match
+    // the lower bound, return an empty vec. TODO: It would be possible to try
+    // harder, by merging several devices into a single linear device. If this
+    // turns out to be necessary, this is the correct place to do it.
+    if lower > matches.len() {
+        return vec![];
     }
 
-    counts.push(lower);
+    // Retain only devices that are less than max_size
+    let (mut matches, mut too_large): (Vec<(&Path, Sectors)>, Vec<(&Path, Sectors)>) =
+        if max_size.is_none() {
+            (matches, vec![])
+        } else {
+            let max_size = max_size.expect("!max_size.is_none()");
+            matches.iter().partition(|&(_, s)| *s <= max_size)
+        };
+
+    let avail = matches.len();
+    let needed = maybe_upper.unwrap_or(lower);
+    let must_generate = if avail > needed { 0 } else { needed - avail };
+    let avail_specs = {
+        let mut avail_specs = vec![];
+        while avail_specs.len() < must_generate && (!too_large.is_empty() || !matches.is_empty()) {
+            let (path, size) = if too_large.is_empty() {
+                matches.pop().expect("!matches.is_empty()")
+            } else {
+                too_large.pop().expect("!too_large.is_empty()")
+            };
+            let mut new_pairs = vec![];
+            for i in 0..size / min_size {
+                new_pairs.push((path, Some((i * min_size, min_size))))
+            }
+            avail_specs.extend(new_pairs);
+        }
+        avail_specs.extend(matches.iter().map(|&(p, _)| (p, None)));
+        avail_specs
+    };
+
+    let avail = avail_specs.len();
+
+    // If there is not a sufficient number of devices small enough to match
+    // the lower bound, return an empty vec.
+    if lower > avail {
+        return vec![];
+    }
+
+    let mut device_lists = vec![];
+
+    device_lists.push(avail_specs.iter().take(lower).cloned().collect::<Vec<_>>());
 
     if lower != avail {
         match maybe_upper {
-            None => counts.push(avail),
+            None => device_lists.push(avail_specs.iter().take(avail).cloned().collect::<Vec<_>>()),
             Some(upper) => {
-                if lower + 1 < upper {
-                    counts.push(cmp::min(upper - 1, avail))
+                if lower < upper {
+                    device_lists.push(
+                        avail_specs
+                            .iter()
+                            .take(cmp::min(upper, avail))
+                            .cloned()
+                            .collect::<Vec<_>>(),
+                    )
                 }
             }
         }
     }
 
-    counts
+    device_lists
+}
+
+/// Make a new LinearDev according to specs
+fn make_linear_test_dev(devnode: &Path, start: Sectors, length: Sectors) -> LinearDev {
+    let params = LinearTargetParams::new(
+        Device::from(devnode_to_devno(devnode).unwrap().unwrap()),
+        start,
+    );
+    let table = vec![
+        TargetLine::new(Sectors(0), length, LinearDevTargetParams::Linear(params)),
+    ];
+    LinearDev::setup(
+        get_dm(),
+        DmName::new(&format!("stratis_test_{}", Uuid::new_v4())).expect("valid format"),
+        None,
+        table.clone(),
+    ).unwrap()
 }
 
 /// Run test on real devices, using given constraints. Constraints may result
@@ -111,26 +200,47 @@ where
         .map(|x| Path::new(x.as_str().unwrap()))
         .collect();
 
-    let counts = get_device_counts(limits, devpaths.len());
+    let dev_sizes: Vec<(&Path, Sectors)> = devpaths
+        .iter()
+        .map(|p| {
+            (
+                *p,
+                blkdev_size(&OpenOptions::new().read(true).open(p).unwrap())
+                    .unwrap()
+                    .sectors(),
+            )
+        })
+        .collect();
 
-    assert!(!counts.is_empty());
+    let runs = get_device_runs(limits, &dev_sizes);
+
+    assert!(!runs.is_empty());
 
     init_logger();
 
-    let runs = counts
-        .iter()
-        .map(|num| devpaths.iter().take(*num).collect::<Vec<_>>());
-
     for run_paths in runs {
-        let devices: Vec<_> = run_paths.iter().map(|x| RealTestDev::new(x)).collect();
-
         clean_up().unwrap();
 
-        let result =
-            panic::catch_unwind(|| test(&devices.iter().map(|x| x.as_path()).collect::<Vec<_>>()));
+        let mut devices: Vec<_> = run_paths
+            .iter()
+            .map(|&(path, spec)| match spec {
+                Some((start, length)) => {
+                    RealTestDev::new(Either::Right(make_linear_test_dev(path, start, length)))
+                }
+                None => RealTestDev::new(Either::Left(path.to_path_buf())),
+            })
+            .collect();
+
+        let paths: Vec<PathBuf> = devices.iter().map(|x| x.as_path()).collect();
+        let paths: Vec<&Path> = paths.iter().map(|x| x.as_path()).collect();
+        let result = panic::catch_unwind(|| test(&paths));
         let tear_down = clean_up();
 
         result.unwrap();
         tear_down.unwrap();
+
+        for dev in devices.drain(..) {
+            dev.teardown();
+        }
     }
 }

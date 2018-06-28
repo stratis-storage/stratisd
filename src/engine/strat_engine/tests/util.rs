e7d9f832ea347146506e8d818832cf1b9577f205
mulhern <amulhern@redhat.com>
2018-06-28 10:17:34 -0400

    Allow renamed and removed lints
    To avoid errors from error_chain macros.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/tests/util.rs
+++ src/engine/strat_engine/tests/util.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#![allow(renamed_and_removed_lints)]
+
 use std::path::PathBuf;
 
 use mnt::get_submounts;

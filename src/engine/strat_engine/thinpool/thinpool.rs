48beee04b89dbaf744e1cd3671c524b9c6892623
Andy Grover <agrover@redhat.com>
2018-07-20 10:36:07 -0700

    Add FreeSpaceState, and use data_lowater to event on state transitions
    Add a state machine around space available: either Good, Warn, or Crit.
    Use this state machine to throttle writes when a pool moves to Warn, and
    suspend filesystems when moving to Crit.
    
    Use the data lowater event for space state changes, in addition to its
    previous purpose (to extend the data device). Alas this doesn't work for
    *improving* space states. (That work TBD but we may just need to poll.)
    
    Track next_event_point since lowater may not be equal to DATA_LOWATER
    any more.
    
    Change extension policy to 1GiB max. Keeping a tighter rein on data dev
    usage makes other things easier.
    
    Add a bunch of logging statements now that we don't need to worry about
    logging when we shouldn't.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- src/engine/strat_engine/thinpool/thinpool.rs
+++ src/engine/strat_engine/thinpool/thinpool.rs
@@ -11,9 +11,9 @@ use uuid::Uuid;
 
 use devicemapper as dm;
 use devicemapper::{
-    device_exists, DataBlocks, Device, DmDevice, DmName, DmNameBuf, FlakeyTargetParams, LinearDev,
-    LinearDevTargetParams, LinearTargetParams, MetaBlocks, Sectors, TargetLine, ThinDev, ThinDevId,
-    ThinPoolDev, ThinPoolStatusSummary, IEC,
+    device_exists, Bytes, DataBlocks, Device, DmDevice, DmName, DmNameBuf, FlakeyTargetParams,
+    LinearDev, LinearDevTargetParams, LinearTargetParams, MetaBlocks, Sectors, TargetLine, ThinDev,
+    ThinDevId, ThinPoolDev, ThinPoolStatusSummary, IEC,
 };
 
 use stratis::{ErrorEnum, StratisError, StratisResult};
@@ -31,6 +31,7 @@ use super::super::dmnames::{
     format_flex_ids, format_thin_ids, format_thinpool_ids, FlexRole, ThinPoolRole, ThinRole,
 };
 use super::super::serde_structs::{FlexDevsSave, Recordable, ThinPoolDevSave};
+use super::super::set_write_throttling;
 
 use super::filesystem::{FilesystemStatus, StratFilesystem};
 use super::mdv::MetadataVol;
@@ -45,6 +46,20 @@ const DEFAULT_THIN_DEV_SIZE: Sectors = Sectors(2 * IEC::Gi); // 1 TiB
 const INITIAL_META_SIZE: MetaBlocks = MetaBlocks(4 * IEC::Ki);
 pub const INITIAL_DATA_SIZE: DataBlocks = DataBlocks(768);
 const INITIAL_MDV_SIZE: Sectors = Sectors(32 * IEC::Ki); // 16 MiB
+
+const SPACE_WARN_PCT: u8 = 90;
+const SPACE_CRIT_PCT: u8 = 95;
+
+// This is kind of low, maybe.
+const SPACE_WARN_THROTTLE_BLOCKS_PER_SEC: DataBlocks = DataBlocks(10);
+
+fn sectors_to_datablocks(sectors: Sectors) -> DataBlocks {
+    DataBlocks(*sectors / *DATA_BLOCK_SIZE)
+}
+
+fn datablocks_to_sectors(data_blocks: DataBlocks) -> Sectors {
+    Sectors(*data_blocks * *DATA_BLOCK_SIZE)
+}
 
 /// Transform a list of segments belonging to a single device into a
 /// list of target lines for a linear device.
@@ -148,6 +163,32 @@ enum PoolState {
     Bad,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum FreeSpaceState {
+    Good,
+    Warn,
+    Crit,
+}
+
+/// Return a value from 0 to 100 that is the percentage that "used" makes up
+/// in "total".
+fn used_pct(used: u64, total: u64) -> u8 {
+    assert!(total >= used);
+    let mut val = (used * 100) / total;
+    if (used * 100) % total != 0 {
+        val += 1; // round up
+    }
+    assert!(val <= 100);
+    val as u8
+}
+
+// Calculate what to set lowater to, to achieve "event when XX% of the data
+// device is used"
+fn calc_data_lowater(percent_used: u8, total: DataBlocks) -> DataBlocks {
+    assert!(percent_used <= 100);
+    DataBlocks(*total - ((*total * percent_used as u64) / 100))
+}
+
 /// A ThinPool struct contains the thinpool itself, the spare
 /// segments for its metadata device, and the filesystems and filesystem
 /// metadata associated with it.
@@ -166,6 +207,8 @@ pub struct ThinPool {
     /// The device will change if the backstore adds or removes a cache.
     backstore_device: Device,
     pool_state: PoolState,
+    free_space_state: FreeSpaceState,
+    next_event_point: DataBlocks,
 }
 
 impl ThinPool {
@@ -231,6 +274,7 @@ impl ThinPool {
         let mdv = MetadataVol::initialize(pool_uuid, mdv_dev)?;
 
         let (dm_name, dm_uuid) = format_thinpool_ids(pool_uuid, ThinPoolRole::Pool);
+        let next_event_point = DATA_LOWATER;
         let thinpool_dev = ThinPoolDev::new(
             get_dm(),
             &dm_name,
@@ -238,7 +282,7 @@ impl ThinPool {
             meta_dev,
             data_dev,
             data_block_size,
-            DATA_LOWATER,
+            next_event_point,
         )?;
         Ok(ThinPool {
             thin_pool: thinpool_dev,
@@ -251,6 +295,8 @@ impl ThinPool {
             mdv,
             backstore_device,
             pool_state: PoolState::Good,
+            free_space_state: FreeSpaceState::Good,
+            next_event_point,
         })
     }
 
@@ -291,6 +337,7 @@ impl ThinPool {
             segs_to_table(backstore_device, &data_segments),
         )?;
 
+        let next_event_point = DATA_LOWATER;
         let thinpool_dev = ThinPoolDev::setup(
             get_dm(),
             &thinpool_name,
@@ -298,7 +345,7 @@ impl ThinPool {
             meta_dev,
             data_dev,
             thin_pool_save.data_block_size,
-            DATA_LOWATER,
+            next_event_point,
         )?;
 
         let (dm_name, dm_uuid) = format_flex_ids(pool_uuid, FlexRole::MetadataVolume);
@@ -354,6 +401,8 @@ impl ThinPool {
             mdv,
             backstore_device,
             pool_state: PoolState::Good,
+            free_space_state: FreeSpaceState::Good,
+            next_event_point,
         })
     }
 
@@ -406,28 +455,55 @@ impl ThinPool {
                     }
                 }
 
-                if usage.used_data > cmp::max(usage.total_data, DATA_LOWATER) - DATA_LOWATER {
+                if usage.used_data >= self.next_event_point {
                     // Request expansion of physical space allocated to the pool
-                    // TODO: we request that the space be doubled or use the remaining space by
-                    // requesting the minimum total_data vs. available space.
-                    // A more sophisticated approach might be in order.
-                    match self.extend_thinpool(
-                        DataBlocks(cmp::min(
-                            *usage.total_data,
-                            backstore.available() / DATA_BLOCK_SIZE,
-                        )),
-                        backstore,
-                    ) {
-                        #![allow(single_match)]
-                        Ok(_) => {
-                            info!("Extended data pool");
-                            should_save = true
-                        }
-                        Err(_) => {
-                            error!("Thinpool data extend failed! -> BAD");
-                            self.pool_state = PoolState::Bad;
+                    // Ask for 1 GiB, or whatever's left.
+                    let extend_size = sectors_to_datablocks(cmp::min(
+                        backstore.available(),
+                        Bytes(IEC::Gi).sectors(),
+                    ));
+                    if extend_size == DataBlocks(0) {
+                        info!("data device fully extended, cannot extend further");
+                    } else {
+                        info!(
+                            "extending data device by {} ({})",
+                            extend_size,
+                            datablocks_to_sectors(extend_size).bytes()
+                        );
+                        match self.extend_thinpool(extend_size, backstore) {
+                            #![allow(single_match)]
+                            Ok(_) => {
+                                info!("Extended data pool by {}", extend_size);
+                                should_save = true
+                            }
+                            Err(_) => {
+                                error!("Thinpool data extend failed! -> BAD");
+                                self.pool_state = PoolState::Bad;
+                            }
                         }
                     }
+
+                    info!(
+                        "used data {} total data {} extend size {} available {}",
+                        usage.used_data,
+                        usage.total_data,
+                        extend_size,
+                        sectors_to_datablocks(backstore.available())
+                    );
+
+                    // Update pool space state
+                    self.free_space_state = self.free_space_check(
+                        usage.used_data,
+                        usage.total_data - usage.used_data
+                            + extend_size
+                            + sectors_to_datablocks(backstore.available()),
+                    )?;
+
+                    // Trigger next event depending on pool space state
+                    self.next_event_point = self.set_next_event_point(
+                        usage.total_data + extend_size,
+                        sectors_to_datablocks(backstore.available()),
+                    )?;
                 }
             }
             dm::ThinPoolStatus::Fail => {
@@ -451,6 +527,157 @@ impl ThinPool {
         }
 
         Ok(should_save)
+    }
+
+    /// Possibly transition to a new FreeSpaceState based on usage, and invoke
+    /// policies (throttling, suspension) accordingly.
+    fn free_space_check(
+        &mut self,
+        used: DataBlocks,
+        available: DataBlocks,
+    ) -> StratisResult<FreeSpaceState> {
+        let overall_used_pct = used_pct(*used, *used + *available);
+        info!("Data tier percent used: {}", overall_used_pct);
+
+        let prev_state = self.free_space_state;
+
+        let new_state = match (prev_state, overall_used_pct) {
+            (FreeSpaceState::Good, 0...SPACE_WARN_PCT) => FreeSpaceState::Good,
+            (FreeSpaceState::Good, SPACE_WARN_PCT...SPACE_CRIT_PCT) => {
+                // TODO: other steps to regain space: schedule fstrims?
+
+                // good->warn. Throttle.
+                set_write_throttling(
+                    self.thin_pool.data_dev().device(),
+                    Some(*datablocks_to_sectors(SPACE_WARN_THROTTLE_BLOCKS_PER_SEC).bytes()),
+                )?;
+                info!(
+                    "throttling to {} bytes/sec",
+                    *datablocks_to_sectors(SPACE_WARN_THROTTLE_BLOCKS_PER_SEC).bytes()
+                );
+
+                FreeSpaceState::Warn
+            }
+            (FreeSpaceState::Good, _) => {
+                // good->warn->crit. Both throttle and suspend.
+                set_write_throttling(
+                    self.thin_pool.data_dev().device(),
+                    Some(*datablocks_to_sectors(SPACE_WARN_THROTTLE_BLOCKS_PER_SEC).bytes()),
+                )?;
+                info!(
+                    "throttling to {} bytes/sec",
+                    *datablocks_to_sectors(SPACE_WARN_THROTTLE_BLOCKS_PER_SEC).bytes()
+                );
+
+                for (_, _, fs) in &mut self.filesystems {
+                    fs.suspend(true)?;
+                }
+
+                FreeSpaceState::Crit
+            }
+            (FreeSpaceState::Warn, 0...SPACE_WARN_PCT) => {
+                // warn->good.
+                set_write_throttling(self.thin_pool.data_dev().device(), None)?;
+                info!("throttling disabled");
+                FreeSpaceState::Good
+            }
+            (FreeSpaceState::Warn, SPACE_WARN_PCT...SPACE_CRIT_PCT) => FreeSpaceState::Warn,
+            (FreeSpaceState::Warn, _) => {
+                // warn->crit. Suspend.
+                for (_, _, fs) in &mut self.filesystems {
+                    fs.suspend(true)?;
+                }
+
+                FreeSpaceState::Crit
+            }
+            (FreeSpaceState::Crit, 0...SPACE_WARN_PCT) => {
+                // crit->warn->good. Unthrottle and unsuspend.
+
+                for (_, _, fs) in &mut self.filesystems {
+                    fs.resume()?;
+                }
+                set_write_throttling(self.thin_pool.data_dev().device(), None)?;
+                info!("throttling disabled");
+
+                FreeSpaceState::Good
+            }
+            (FreeSpaceState::Crit, SPACE_WARN_PCT...SPACE_CRIT_PCT) => {
+                // crit->warn. Unsuspend.
+                for (_, _, fs) in &mut self.filesystems {
+                    fs.resume()?;
+                }
+
+                FreeSpaceState::Warn
+            }
+            (FreeSpaceState::Crit, _) => FreeSpaceState::Crit,
+        };
+
+        if prev_state != new_state {
+            info!(
+                "Prev space state: {:?} New space state: {:?}",
+                prev_state, new_state
+            );
+
+            // TODO: Dbus signal
+        }
+
+        Ok(new_state)
+    }
+
+    /// Set next event point, based on pool space state and if we also want
+    /// events to extend the pool, or not.
+    /// Returns the usage.used value that will trigger this event point.
+    fn set_next_event_point(
+        &mut self,
+        data_dev_size: DataBlocks,
+        available: DataBlocks,
+    ) -> StratisResult<DataBlocks> {
+        let total = data_dev_size + available;
+        info!(
+            "total {} data_dev_size {} available {}",
+            total, data_dev_size, available
+        );
+
+        // warn/crit points as if pool was fully extended
+        let warn_lowat_for_total = calc_data_lowater(SPACE_WARN_PCT, total);
+        let crit_lowat_for_total = calc_data_lowater(SPACE_CRIT_PCT, total);
+        info!(
+            "warn_lowat_for_total {} crit_lowat_for_total {}",
+            warn_lowat_for_total, crit_lowat_for_total
+        );
+
+        // Make it be the higher of point for next extension event and point
+        // for FreeSpaceState change
+        let lowater = match self.free_space_state {
+            FreeSpaceState::Good => {
+                // DATA_LOWATER relative to end of data_dev_size, so add
+                // 'available' for correct comparison
+                if DATA_LOWATER + available > warn_lowat_for_total {
+                    DATA_LOWATER
+                } else {
+                    warn_lowat_for_total - available
+                }
+            }
+            _ => {
+                // We're throttled, so can set a tighter event and still get
+                // called before out of space
+                if SPACE_WARN_THROTTLE_BLOCKS_PER_SEC + available > crit_lowat_for_total {
+                    SPACE_WARN_THROTTLE_BLOCKS_PER_SEC
+                } else {
+                    crit_lowat_for_total - available
+                }
+            }
+        };
+
+        let next_event_point = data_dev_size - lowater;
+        info!(
+            "setting lowater to {}, next event point to {}",
+            lowater, next_event_point
+        );
+
+        self.thin_pool.set_low_water_mark(get_dm(), lowater)?;
+        self.resume()?;
+        Ok(next_event_point)
     }
 
     /// Tear down the components managed here: filesystems, the MDV,

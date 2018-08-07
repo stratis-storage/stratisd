395c4e13e58a742d9db29eebc89bd02623a90f1a
mulhern <amulhern@redhat.com>
2018-08-07 10:14:24 -0400

    Add a test for new backstore method alloc_single_segment_max
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/backstore/backstore.rs
+++ src/engine/strat_engine/backstore/backstore.rs
@@ -576,6 +576,67 @@ mod tests {
         );
     }
 
+    /// Create a backstore.
+    /// Request a amount that can not be allocated because the modulus is
+    /// bigger than the reqested amount.
+    /// Request an impossibly large amount.
+    /// Verify that the backstore is now all used up.
+    fn test_alloc_single_segment(paths: &[&Path]) -> () {
+        assert!(paths.len() > 0);
+
+        let mut backstore =
+            Backstore::initialize(Uuid::new_v4(), paths, MIN_MDA_SECTORS, false).unwrap();
+
+        assert!(
+            backstore
+                .alloc_single_segment_max(Sectors(IEC::Ki), Sectors(IEC::Mi))
+                .is_none()
+        );
+
+        let request = Sectors(IEC::Ei);
+        let modulus = Sectors(IEC::Ki);
+        let old_next = backstore.next;
+        let (start, length) = backstore
+            .alloc_single_segment_max(request, modulus)
+            .unwrap();
+        assert!(length < request);
+        // FIXME: change to Sector operation once implemented in devicemapper
+        assert_eq!(*length % *modulus, 0);
+        assert_eq!(backstore.next, old_next + length);
+        assert_eq!(start, old_next);
+
+        assert!(
+            backstore
+                .alloc_single_segment_max(request, Sectors(IEC::Ki))
+                .is_none()
+        );
+        backstore.destroy().unwrap();
+    }
+
+    #[test]
+    pub fn loop_test_alloc_single_segment() {
+        loopbacked::test_with_spec(
+            loopbacked::DeviceLimits::Range(1, 3, None),
+            test_alloc_single_segment,
+        );
+    }
+
+    #[test]
+    pub fn real_test_alloc_single_segment() {
+        real::test_with_spec(
+            real::DeviceLimits::AtLeast(1, None, None),
+            test_alloc_single_segment,
+        );
+    }
+
+    #[test]
+    pub fn travis_test_alloc_single_segment() {
+        loopbacked::test_with_spec(
+            loopbacked::DeviceLimits::Range(1, 3, None),
+            test_alloc_single_segment,
+        );
+    }
+
     /// Create a backstore with a cache.
     /// Setup the same backstore, should succeed.
     /// Verify that blockdev metadatas are the same for the backstores.

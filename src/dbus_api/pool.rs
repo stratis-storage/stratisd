ed57a3817ec6d6936152f0b786fd953dcbf93c2f
John Baublitz <jbaublitz@redhat.com>
2019-07-30 11:16:58 -0400

    Changes to DBus and engine APIs for idempotence
    Make internal changes to the engine to better support
    idempotent operations using the DBus API so that the return
    value of DBus methods provides information on what was changed
    and omits resources that were not changed.
    
    Co-Authored-By: mulhern <amulhern@redhat.com>

--- src/dbus_api/pool.rs
+++ src/dbus_api/pool.rs
@@ -13,8 +13,6 @@ use dbus::{
     Message,
 };
 
-use uuid::Uuid;
-
 use devicemapper::Sectors;
 
 use crate::{
@@ -28,7 +26,10 @@ use crate::{
             msg_string_ok,
         },
     },
-    engine::{BlockDevTier, MaybeDbusPath, Name, Pool, RenameAction},
+    engine::{
+        BlockDevTier, CreateAction, EngineAction, FilesystemUuid, MaybeDbusPath, Name, Pool,
+        PoolUuid, RenameAction,
+    },
 };
 
 fn create_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
@@ -40,7 +41,7 @@ fn create_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
 
     let object_path = m.path.get_name();
     let return_message = message.method_return();
-    let default_return: Vec<(dbus::Path, &str)> = Vec::new();
+    let default_return: (bool, Vec<(dbus::Path, &str)>) = (false, Vec::new());
 
     if filesystems.count() > 1 {
         let error_message = "only 1 filesystem per request allowed";
@@ -65,15 +66,23 @@ fn create_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
             .collect::<Vec<(&str, Option<Sectors>)>>(),
     );
 
-    let msg = match result {
-        Ok(ref infos) => {
-            let return_value = infos
+    let infos = match result {
+        Ok(created_set) => created_set.changed(),
+        Err(err) => {
+            let (rc, rs) = engine_to_dbus_err_tuple(&err);
+            return Ok(vec![return_message.append3(default_return, rc, rs)]);
+        }
+    };
+
+    let return_value = match infos {
+        Some(ref newly_created_filesystems) => {
+            let v = newly_created_filesystems
                 .iter()
                 .map(|&(name, uuid)| {
+                    // FIXME: To avoid this expect, modify create_filesystem
+                    // so that it returns a mutable reference to the
+                    // filesystem created.
                     (
-                        // FIXME: To avoid this expect, modify create_filesystem
-                        // so that it returns a mutable reference to the
-                        // filesystem created.
                         create_dbus_filesystem(
                             dbus_context,
                             object_path.clone(),
@@ -86,15 +95,16 @@ fn create_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
                     )
                 })
                 .collect::<Vec<_>>();
-
-            return_message.append3(return_value, msg_code_ok(), msg_string_ok())
+            (true, v)
         }
-        Err(err) => {
-            let (rc, rs) = engine_to_dbus_err_tuple(&err);
-            return_message.append3(default_return, rc, rs)
-        }
+        None => default_return,
     };
-    Ok(vec![msg])
+
+    Ok(vec![return_message.append3(
+        return_value,
+        msg_code_ok(),
+        msg_string_ok(),
+    )])
 }
 
 fn destroy_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
@@ -106,7 +116,7 @@ fn destroy_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let dbus_context = m.tree.get_data();
     let object_path = m.path.get_name();
     let return_message = message.method_return();
-    let default_return: Vec<&str> = Vec::new();
+    let default_return: (bool, Vec<String>) = (false, Vec::new());
 
     let pool_path = m
         .tree
@@ -117,29 +127,39 @@ fn destroy_filesystems(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let mut engine = dbus_context.engine.borrow_mut();
     let (pool_name, pool) = get_mut_pool!(engine; pool_uuid; default_return; return_message);
 
-    let mut filesystem_map: HashMap<Uuid, dbus::Path<'static>> = HashMap::new();
-    for op in filesystems {
-        if let Some(filesystem_path) = m.tree.get(&op) {
-            let filesystem_uuid = get_data!(filesystem_path; default_return; return_message).uuid;
-            filesystem_map.insert(filesystem_uuid, op);
-        }
-    }
+    let filesystem_map: HashMap<FilesystemUuid, dbus::Path<'static>> = filesystems
+        .filter_map(|path| {
+            m.tree.get(&path).and_then(|op| {
+                op.get_data()
+                    .as_ref()
+                    .map(|d| (d.uuid, op.get_name().clone()))
+            })
+        })
+        .collect();
 
     let result = pool.destroy_filesystems(
         &pool_name,
-        &filesystem_map.keys().cloned().collect::<Vec<Uuid>>(),
+        &filesystem_map.keys().cloned().collect::<Vec<_>>(),
     );
     let msg = match result {
-        Ok(ref uuids) => {
-            for uuid in uuids {
-                let op = filesystem_map
-                    .get(uuid)
-                    .expect("'uuids' is a subset of filesystem_map.keys()");
-                dbus_context.actions.borrow_mut().push_remove(op, m.tree);
-            }
-
-            let return_value: Vec<String> = uuids.iter().map(|n| uuid_to_string!(n)).collect();
-            return_message.append3(return_value, msg_code_ok(), msg_string_ok())
+        Ok(uuids) => {
+            // Only get changed values here as non-existant filesystems will have been filtered out
+            // before calling destroy_filesystems
+            let uuid_vec: Vec<String> = if let Some(ref changed_uuids) = uuids.changed() {
+                for uuid in changed_uuids {
+                    let op = filesystem_map
+                        .get(uuid)
+                        .expect("'uuids' is a subset of filesystem_map.keys()");
+                    dbus_context.actions.borrow_mut().push_remove(op, m.tree);
+                }
+                changed_uuids
+                    .iter()
+                    .map(|uuid| uuid_to_string!(uuid))
+                    .collect()
+            } else {
+                Vec::new()
+            };
+            return_message.append3((true, uuid_vec), msg_code_ok(), msg_string_ok())
         }
         Err(err) => {
             let (rc, rs) = engine_to_dbus_err_tuple(&err);
@@ -159,7 +179,7 @@ fn snapshot_filesystem(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let dbus_context = m.tree.get_data();
     let object_path = m.path.get_name();
     let return_message = message.method_return();
-    let default_return = dbus::Path::default();
+    let default_return = (false, dbus::Path::default());
 
     let pool_path = m
         .tree
@@ -180,10 +200,13 @@ fn snapshot_filesystem(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let (pool_name, pool) = get_mut_pool!(engine; pool_uuid; default_return; return_message);
 
     let msg = match pool.snapshot_filesystem(pool_uuid, &pool_name, fs_uuid, snapshot_name) {
-        Ok((uuid, fs)) => {
+        Ok(CreateAction::Created((uuid, fs))) => {
             let fs_object_path: dbus::Path =
                 create_dbus_filesystem(dbus_context, object_path.clone(), uuid, fs);
-            return_message.append3(fs_object_path, msg_code_ok(), msg_string_ok())
+            return_message.append3((true, fs_object_path), msg_code_ok(), msg_string_ok())
+        }
+        Ok(CreateAction::Identity) => {
+            return_message.append3(default_return, msg_code_ok(), msg_string_ok())
         }
         Err(err) => {
             let (rc, rs) = engine_to_dbus_err_tuple(&err);
@@ -203,7 +226,7 @@ fn add_blockdevs(m: &MethodInfo<MTFn<TData>, TData>, tier: BlockDevTier) -> Meth
     let dbus_context = m.tree.get_data();
     let object_path = m.path.get_name();
     let return_message = message.method_return();
-    let default_return: Vec<dbus::Path> = Vec::new();
+    let default_return: (bool, Vec<dbus::Path>) = (false, Vec::new());
 
     let pool_path = m
         .tree
@@ -217,8 +240,8 @@ fn add_blockdevs(m: &MethodInfo<MTFn<TData>, TData>, tier: BlockDevTier) -> Meth
     let blockdevs = devs.map(|x| Path::new(x)).collect::<Vec<&Path>>();
 
     let result = pool.add_blockdevs(pool_uuid, &*pool_name, &blockdevs, tier);
-    let msg = match result {
-        Ok(uuids) => {
+    let msg = match result.map(|bds| bds.changed()) {
+        Ok(Some(uuids)) => {
             let return_value = uuids
                 .iter()
                 .map(|uuid| {
@@ -236,8 +259,9 @@ fn add_blockdevs(m: &MethodInfo<MTFn<TData>, TData>, tier: BlockDevTier) -> Meth
                 })
                 .collect::<Vec<_>>();
 
-            return_message.append3(return_value, msg_code_ok(), msg_string_ok())
+            return_message.append3((true, return_value), msg_code_ok(), msg_string_ok())
         }
+        Ok(None) => return_message.append3(default_return, msg_code_ok(), msg_string_ok()),
         Err(err) => {
             let (rc, rs) = engine_to_dbus_err_tuple(&err);
             return_message.append3(default_return, rc, rs)
@@ -264,7 +288,7 @@ fn rename_pool(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let dbus_context = m.tree.get_data();
     let object_path = m.path.get_name();
     let return_message = message.method_return();
-    let default_return = false;
+    let default_return = (false, uuid_to_string!(PoolUuid::nil()));
 
     let pool_path = m
         .tree
@@ -278,12 +302,18 @@ fn rename_pool(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
         .rename_pool(pool_uuid, new_name)
     {
         Ok(RenameAction::NoSource) => {
-            let error_message = format!("engine doesn't know about pool {}", &pool_uuid);
+            let error_message = format!("engine doesn't know about pool {}", pool_uuid);
             let (rc, rs) = (DbusErrorEnum::INTERNAL_ERROR as u16, error_message);
             return_message.append3(default_return, rc, rs)
         }
-        Ok(RenameAction::Identity) => return_message.append3(false, msg_code_ok(), msg_string_ok()),
-        Ok(RenameAction::Renamed) => return_message.append3(true, msg_code_ok(), msg_string_ok()),
+        Ok(RenameAction::Identity) => {
+            return_message.append3(default_return, msg_code_ok(), msg_string_ok())
+        }
+        Ok(RenameAction::Renamed(uuid)) => return_message.append3(
+            (true, uuid_to_string!(uuid)),
+            msg_code_ok(),
+            msg_string_ok(),
+        ),
         Err(err) => {
             let (rc, rs) = engine_to_dbus_err_tuple(&err);
             return_message.append3(default_return, rc, rs)
@@ -301,7 +331,7 @@ fn get_pool_property<F, R>(
     getter: F,
 ) -> Result<(), MethodErr>
 where
-    F: Fn((Name, Uuid, &dyn Pool)) -> Result<R, MethodErr>,
+    F: Fn((Name, PoolUuid, &dyn Pool)) -> Result<R, MethodErr>,
     R: dbus::arg::Append,
 {
     let dbus_context = p.tree.get_data();
@@ -334,7 +364,7 @@ fn get_pool_total_physical_used(
     i: &mut IterAppend,
     p: &PropInfo<MTFn<TData>, TData>,
 ) -> Result<(), MethodErr> {
-    fn get_used((_, uuid, pool): (Name, Uuid, &dyn Pool)) -> Result<String, MethodErr> {
+    fn get_used((_, uuid, pool): (Name, PoolUuid, &dyn Pool)) -> Result<String, MethodErr> {
         let err_func = |_| {
             MethodErr::failed(&format!(
                 "no total physical size computed for pool with uuid {}",
@@ -377,7 +407,7 @@ fn get_space_state(i: &mut IterAppend, p: &PropInfo<MTFn<TData>, TData>) -> Resu
 pub fn create_dbus_pool<'a>(
     dbus_context: &DbusContext,
     parent: dbus::Path<'static>,
-    uuid: Uuid,
+    uuid: PoolUuid,
     pool: &mut dyn Pool,
 ) -> dbus::Path<'a> {
     let f = Factory::new_fn();
@@ -385,35 +415,55 @@ pub fn create_dbus_pool<'a>(
     let create_filesystems_method = f
         .method("CreateFilesystems", (), create_filesystems)
         .in_arg(("specs", "as"))
-        .out_arg(("filesystems", "a(os)"))
+        // b: true if filesystems were created
+        // a(os): Array of tuples with object paths and names
+        //
+        // Rust representation: (bool, Vec<(dbus::Path, String)>)
+        .out_arg(("results", "(ba(os))"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 
     let destroy_filesystems_method = f
         .method("DestroyFilesystems", (), destroy_filesystems)
         .in_arg(("filesystems", "ao"))
-        .out_arg(("results", "as"))
+        // b: true if filesystems were destroyed
+        // as: Array of UUIDs of destroyed filesystems
+        //
+        // Rust representation: (bool, Vec<String>)
+        .out_arg(("results", "(bas)"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 
     let add_blockdevs_method = f
         .method("AddDataDevs", (), add_datadevs)
         .in_arg(("devices", "as"))
-        .out_arg(("results", "ao"))
+        // b: Indicates if any data devices were added
+        // ao: Array of object paths of created data devices
+        //
+        // Rust representation: (bool, Vec<dbus::path>)
+        .out_arg(("results", "(bao)"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 
     let add_cachedevs_method = f
         .method("AddCacheDevs", (), add_cachedevs)
         .in_arg(("devices", "as"))
-        .out_arg(("results", "ao"))
+        // b: Indicates if any cache devices were added
+        // ao: Array of object paths of created cache devices
+        //
+        // Rust representation: (bool, Vec<dbus::path>)
+        .out_arg(("results", "(bao)"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 
     let rename_method = f
         .method("SetName", (), rename_pool)
         .in_arg(("name", "s"))
-        .out_arg(("action", "b"))
+        // b: false if no pool was renamed
+        // s: UUID of renamed pool
+        //
+        // Rust representation: (bool, String)
+        .out_arg(("result", "(bs)"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 
@@ -421,7 +471,11 @@ pub fn create_dbus_pool<'a>(
         .method("SnapshotFilesystem", (), snapshot_filesystem)
         .in_arg(("origin", "o"))
         .in_arg(("snapshot_name", "s"))
-        .out_arg(("result", "o"))
+        // b: false if no new snapshot was created
+        // s: Object path of new snapshot
+        //
+        // Rust representation: (bool, String)
+        .out_arg(("result", "(bo)"))
         .out_arg(("return_code", "q"))
         .out_arg(("return_string", "s"));
 

60abf362c7caf628b90b448cd5dcbde663366c0c
John Baublitz <jbaublitz@redhat.com>
2019-11-14 15:49:02 -0500

    D-Bus interface changes for encryption implementation
    This commit makes use of the crypt.rs module implementation of encryption
    by exposing the encryption implementation through the pool interface. The
    key_desc parameter has been added on the D-Bus which, when provided,
    causes create_pool to encrypt all block devices with the specified key
    in the kernel keyring using the key description passed using the D-Bus
    call.
    
    Co-Authored-By: mulhern <amulhern@redhat.com>

--- src/dbus_api/pool/shared.rs
+++ src/dbus_api/pool/shared.rs
@@ -2,15 +2,30 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::path::Path;
+
 use dbus::{
-    arg::IterAppend,
-    tree::{MTFn, MethodErr, MethodInfo, PropInfo, Tree},
+    arg::{Array, IterAppend},
+    tree::{MTFn, MethodErr, MethodInfo, MethodResult, PropInfo, Tree},
+    Message,
 };
 
 use crate::{
-    dbus_api::types::TData,
-    engine::{Name, Pool, PoolUuid},
+    dbus_api::{
+        blockdev::create_dbus_blockdev,
+        types::TData,
+        util::{
+            engine_to_dbus_err_tuple, get_next_arg, msg_code_ok, msg_string_ok, option_to_tuple,
+        },
+    },
+    engine::{BlockDevTier, EngineAction, Name, Pool, PoolUuid},
 };
+
+pub enum BlockDevOp {
+    InitCache,
+    AddCache,
+    AddData,
+}
 
 pub fn pool_operation<F, R>(
     tree: &Tree<MTFn<TData>, TData>,
@@ -41,6 +56,17 @@ where
     closure((pool_name, pool_uuid, pool))
 }
 
+pub fn get_pool_encryption_key_desc(
+    m: &MethodInfo<MTFn<TData>, TData>,
+) -> Result<(bool, String), String> {
+    pool_operation(m.tree, m.path.get_name(), |(_, _, pool)| {
+        Ok(option_to_tuple(
+            pool.key_desc().map(|key| key.to_string()),
+            String::new(),
+        ))
+    })
+}
+
 pub fn get_pool_has_cache(m: &MethodInfo<MTFn<TData>, TData>) -> Result<bool, String> {
     pool_operation(m.tree, m.path.get_name(), |(_, _, pool)| {
         Ok(pool.has_cache())
@@ -62,6 +88,80 @@ pub fn get_pool_total_used(m: &MethodInfo<MTFn<TData>, TData>) -> Result<String,
             .map_err(|e| e.to_string())
             .map(|size| (u128::from(*size) * devicemapper::SECTOR_SIZE as u128).to_string())
     })
+}
+
+/// A method shared by all pool interfaces and by all blockdev-adding
+/// operations, including cache initialization, which is considered a
+/// blockdev-adding operation because when a cache is initialized, the
+/// data tier must already contain some block devices. The op parameter
+/// determines which method belonging to the engine's Pool interface must
+/// be invoked.
+// FIXME: We have tried two approaches for dispatching to the proper methods
+// in the Pool trait. The first had a lot of code duplication, which was
+// a drawback. This approach has significantly less code duplication, but
+// requires discovering the state of the cache, whether initialized or not,
+// in order to figure out which method to dispatch to. We would like to find
+// a third approach, which doesn't have the flaws of the two we've tried.
+pub fn add_blockdevs(m: &MethodInfo<MTFn<TData>, TData>, op: BlockDevOp) -> MethodResult {
+    let message: &Message = m.msg;
+    let mut iter = message.iter_init();
+
+    let devs: Array<&str, _> = get_next_arg(&mut iter, 0)?;
+
+    let dbus_context = m.tree.get_data();
+    let object_path = m.path.get_name();
+    let return_message = message.method_return();
+    let default_return: (bool, Vec<dbus::Path>) = (false, Vec::new());
+
+    let pool_path = m
+        .tree
+        .get(object_path)
+        .expect("implicit argument must be in tree");
+    let pool_uuid = get_data!(pool_path; default_return; return_message).uuid;
+
+    let mut engine = dbus_context.engine.borrow_mut();
+    let (pool_name, pool) = get_mut_pool!(engine; pool_uuid; default_return; return_message);
+
+    let blockdevs = devs.map(|x| Path::new(x)).collect::<Vec<&Path>>();
+
+    let result = match op {
+        BlockDevOp::InitCache => pool.init_cache(pool_uuid, &*pool_name, &blockdevs),
+        BlockDevOp::AddCache => {
+            pool.add_blockdevs(pool_uuid, &*pool_name, &blockdevs, BlockDevTier::Cache)
+        }
+        BlockDevOp::AddData => {
+            pool.add_blockdevs(pool_uuid, &*pool_name, &blockdevs, BlockDevTier::Data)
+        }
+    };
+    let msg = match result.map(|bds| bds.changed()) {
+        Ok(Some(uuids)) => {
+            let return_value = uuids
+                .iter()
+                .map(|uuid| {
+                    // FIXME: To avoid this expect, modify add_blockdevs
+                    // so that it returns a mutable reference to each
+                    // blockdev created.
+                    create_dbus_blockdev(
+                        dbus_context,
+                        object_path.clone(),
+                        *uuid,
+                        pool.get_mut_blockdev(*uuid)
+                            .expect("just inserted by add_blockdevs")
+                            .1,
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            return_message.append3((true, return_value), msg_code_ok(), msg_string_ok())
+        }
+        Ok(None) => return_message.append3(default_return, msg_code_ok(), msg_string_ok()),
+        Err(err) => {
+            let (rc, rs) = engine_to_dbus_err_tuple(&err);
+            return_message.append3(default_return, rc, rs)
+        }
+    };
+
+    Ok(vec![msg])
 }
 
 /// Get a pool property and place it on the D-Bus. The property is

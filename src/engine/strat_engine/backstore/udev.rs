2a3dca95963a8d6f21fb3ba96fdb244f614eaa26
mulhern <amulhern@redhat.com>
2019-11-26 09:42:35 -0500

    Get the ownership designation and the hardware id at the same time
    This way, all devices belonging to the udev database are iterated through
    just once for each block device passed to udev_initialize, instead of twice.
    
    Also, the missing udev entry possibility is handled in the one place that
    the udev devices are iterated over, by returning an error if it can't be
    found, as before. Previously, with the lookup of hw_id, it ignored a missing
    udev entry, which made sense, because it had sort of proved that one
    existed, by finding it at an earlier stage in the method. But, that now
    leaves the only possibility of failure as a failure to decode the hw id
    properly, so I'll just log that as a warn. This takes care of the TODO
    and is slightly better than what the previous code would do: in the unlikely
    event of an error decoding the ID_WWN it would return a lossy string.
    
    Allow type_complexity on dev_info, it is an internal method.
    
    Get rid of some obsoleted methods.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/backstore/udev.rs
+++ src/engine/strat_engine/backstore/udev.rs
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 //! udev-related methods
-use std::{collections::HashMap, ffi::OsStr, fs, path::Path};
+use std::ffi::OsStr;
 
 use libudev;
 
@@ -30,7 +30,7 @@ pub fn stratis_enumerator(context: &libudev::Context) -> libudev::Result<libudev
 /// Returns None if no udev property found for the given property name.
 /// Returns an error if the value of the property can not be converted to
 /// a string using the standard conversion for this OS.
-fn get_udev_property<T: AsRef<OsStr>>(
+pub fn get_udev_property<T: AsRef<OsStr>>(
     device: &libudev::Device,
     property_name: T,
 ) -> Option<StratisResult<String>>
@@ -116,42 +116,4 @@ pub fn decide_ownership(device: &libudev::Device) -> StratisResult<UdevOwnership
     } else {
         UdevOwnership::Theirs
     })
-}
-
-/// Takes a libudev device entry and returns the properties as a HashMap.
-fn device_as_map(device: &libudev::Device) -> HashMap<String, String> {
-    let rc: HashMap<_, _> = device
-        .properties()
-        .map(|i| {
-            (
-                String::from(i.name().to_string_lossy()),
-                String::from(i.value().to_string_lossy()),
-            )
-        })
-        .collect();
-    rc
-}
-
-/// Common function used to retrieve the udev db entry for a block device as a HashMap when found
-pub fn get_udev_block_device(
-    dev_node_search: &Path,
-) -> StratisResult<Option<HashMap<String, String>>> {
-    let context = libudev::Context::new()?;
-    let mut enumerator = block_enumerator(&context)?;
-
-    // Get canonical form to ensure we do correct lookup in udev db
-    let canonical = fs::canonicalize(dev_node_search)?;
-
-    let result = enumerator
-        .scan_devices()?
-        .filter(|dev| dev.is_initialized())
-        .find(|x| x.devnode().map_or(false, |d| canonical == d))
-        .map(|dev| device_as_map(&dev));
-    Ok(result)
-}
-
-/// Lookup the WWN from the udev db using the device node eg. /dev/sda
-pub fn hw_lookup(dev_node_search: &Path) -> StratisResult<Option<String>> {
-    let dev = get_udev_block_device(dev_node_search)?;
-    Ok(dev.and_then(|dev| dev.get("ID_WWN").cloned()))
 }

b68bef65e5f0fe5cef17cfe279aa0b70fece1eb4
Bryan Gurney <bgurney@redhat.com>
2020-03-30 13:16:22 -0400

    Wrap lines of pool_op_logging calls
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/dbus_api/api/manager_2_0/methods.rs
+++ src/dbus_api/api/manager_2_0/methods.rs
@@ -37,7 +37,11 @@ pub fn create_pool(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
     let dbus_context = m.tree.get_data();
     let mut engine = dbus_context.engine.borrow_mut();
 
-    let result = pool_op_logging!("preparing to create pool with name {}", name; engine.create_pool(name, &blockdevs, tuple_to_option(redundancy)); "pool with name {}", name);
+    let result = pool_op_logging!(
+        "preparing to create pool with name {}", name;
+        engine.create_pool(name, &blockdevs, tuple_to_option(redundancy));
+        "pool with name {}", name
+    );
 
     let return_message = message.method_return();
 
@@ -103,7 +107,10 @@ pub fn destroy_pool(m: &MethodInfo<MTFn<TData>, TData>) -> MethodResult {
 
     let mut engine = dbus_context.engine.borrow_mut();
 
-    let result = pool_op_logging!("preparing to destroy pool with UUID {}", pool_uuid.to_simple_ref(); engine.destroy_pool(pool_uuid));
+    let result = pool_op_logging!(
+        "preparing to destroy pool with UUID {}", pool_uuid.to_simple_ref();
+        engine.destroy_pool(pool_uuid)
+    );
 
     let msg = match result {
         Ok(DeleteAction::Deleted(uuid)) => {

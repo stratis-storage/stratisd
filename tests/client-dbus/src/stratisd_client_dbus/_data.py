7859b0ebbc010faa5e0a623310f2fbcaed525434
John Baublitz <jbaublitz@redhat.com>
2020-04-29 13:23:38 -0400

    Add DeleteKey signature for testing

--- tests/client-dbus/src/stratisd_client_dbus/_data.py
+++ tests/client-dbus/src/stratisd_client_dbus/_data.py
@@ -95,6 +95,12 @@ SPECS = {
 <arg name="return_code" type="q" direction="out"/>
 <arg name="return_string" type="s" direction="out"/>
 </method>
+<method name="DeleteKey">
+<arg name="key_desc" type="s" direction="in"/>
+<arg name="result" type="b" direction="out"/>
+<arg name="return_code" type="q" direction="out"/>
+<arg name="return_string" type="s" direction="out"/>
+</method>
 <method name="CreatePool">
 <arg name="name" type="s" direction="in"/>
 <arg name="redundancy" type="(bq)" direction="in"/>

9f712de5fe95e1fc580425997d147b38e4827141
John Baublitz <jbaublitz@redhat.com>
2020-05-08 12:30:30 -0400

    Fix devlinks module to work with prototype

--- src/engine/strat_engine/devlinks.rs
+++ src/engine/strat_engine/devlinks.rs
@@ -2,26 +2,83 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#[cfg(test)]
+use std::{collections::HashSet, io::ErrorKind};
 use std::{
-    collections::HashSet,
-    fs,
-    io::ErrorKind,
-    os::unix::fs::symlink,
-    path::{Path, PathBuf},
+    fs::{self, OpenOptions},
+    io::Write,
+    path::PathBuf,
     str,
 };
 
-use crate::{
-    engine::{
-        engine::{Pool, DEV_PATH},
-        strat_engine::pool::StratPool,
-        types::{Name, PoolUuid},
-    },
-    stratis::StratisResult,
+#[cfg(test)]
+use crate::engine::{
+    strat_engine::pool::StratPool,
+    types::{Name, PoolUuid},
 };
+use crate::{
+    engine::engine::DEV_PATH,
+    stratis::{ErrorEnum, StratisError, StratisResult},
+};
+
+const UEVENT_PATH: &str = "/sys/class/block";
+const UEVENT_CHANGE_EVENT: &[u8] = b"change";
+
+pub fn pool_renamed(pool_name: &str) -> StratisResult<()> {
+    let pool_dir: PathBuf = [DEV_PATH, pool_name].iter().collect();
+
+    for file_result in fs::read_dir(&pool_dir)? {
+        let file = file_result?;
+        let file_path = file.path();
+        let file_name = file_path
+            .file_name()
+            .and_then(|s| s.to_str())
+            .ok_or_else(|| {
+                StratisError::Engine(
+                    ErrorEnum::Invalid,
+                    format!(
+                        "Failure while attempting to generate uevents for all \
+                        of the devices contained in directory {} to make /dev/stratis \
+                        symlinks consistent with internal pool state.",
+                        pool_dir.display(),
+                    ),
+                )
+            })?;
+        fs_renamed(pool_name, file_name)?
+    }
+
+    Ok(())
+}
+
+pub fn fs_renamed(pool_name: &str, fs_name: &str) -> StratisResult<()> {
+    let path: PathBuf = [DEV_PATH, pool_name, fs_name].iter().collect();
+
+    let dm_path = path.canonicalize()?;
+    let file_name = dm_path
+        .file_name()
+        .and_then(|s| s.to_str())
+        .ok_or_else(|| {
+            StratisError::Engine(
+                ErrorEnum::NotFound,
+                format!(
+                    "Failed to generate a change uevent for {} to \
+                make /dev/stratis symlinks consistent with internal pool state",
+                    dm_path.display()
+                ),
+            )
+        })?;
+
+    let uevent_path: PathBuf = [UEVENT_PATH, file_name, "uevent"].iter().collect();
+
+    let mut uevent_file = OpenOptions::new().write(true).open(&uevent_path)?;
+    uevent_file.write_all(UEVENT_CHANGE_EVENT)?;
+
+    Ok(())
+}
 
 /// Set up the root Stratis directory, where dev links as well as temporary
 /// MDV mounts will be created. This must occur before any pools are setup.
+#[cfg(test)]
 pub fn setup_dev_path() -> StratisResult<()> {
     if let Err(err) = fs::create_dir(DEV_PATH) {
         if err.kind() != ErrorKind::AlreadyExists {
@@ -32,44 +89,11 @@ pub fn setup_dev_path() -> StratisResult<()> {
     Ok(())
 }
 
-/// Setup the pool directory and the symlinks in /stratis for the specified pool and filesystems
-/// it contains.
-// Don't just remove and recreate everything in case there are processes
-// (e.g. user shells) with the current working directory within the tree.
-pub fn setup_pool_devlinks(pool_name: &str, pool: &StratPool) {
-    if let Err(err) = || -> StratisResult<()> {
-        let pool_path = pool_directory(pool_name);
-
-        if !pool_path.exists() {
-            pool_added(pool_name);
-        }
-
-        let mut existing_files = fs::read_dir(pool_path)?
-            .map(|dir_e| dir_e.map(|d| d.file_name().into_string().expect("Unix is utf-8")))
-            .collect::<Result<HashSet<_>, _>>()?;
-
-        for (fs_name, _, fs) in pool.filesystems() {
-            filesystem_added(pool_name, &fs_name, &fs.devnode());
-            existing_files.remove(&fs_name.to_owned());
-        }
-
-        for leftover in existing_files {
-            filesystem_removed(pool_name, &leftover);
-        }
-
-        Ok(())
-    }() {
-        warn!(
-            "setup_pool_devlinks failed for /stratis/{}, reason {:?}",
-            pool_name, err
-        );
-    };
-}
-
 /// Clean up directories and symlinks under /stratis based on current
 /// config. Clear out any directory or file that doesn't correspond to a pool.
 // Don't just remove everything in case there are processes
 // (e.g. user shells) with the current working directory within the tree.
+#[cfg(test)]
 pub fn cleanup_devlinks<'a, I: Iterator<Item = (&'a Name, &'a PoolUuid, &'a StratPool)>>(pools: I) {
     if let Err(err) = || -> StratisResult<()> {
         let mut existing_dirs = fs::read_dir(DEV_PATH)?
@@ -91,6 +115,7 @@ pub fn cleanup_devlinks<'a, I: Iterator<Item = (&'a Name, &'a PoolUuid, &'a Stra
 }
 
 /// Create a directory when a pool is added.
+#[cfg(test)]
 pub fn pool_added(pool: &str) {
     let p = pool_directory(pool);
     if let Err(e) = fs::create_dir(&p) {
@@ -99,6 +124,7 @@ pub fn pool_added(pool: &str) {
 }
 
 /// Remove the directory and its contents when the pool is removed.
+#[cfg(test)]
 pub fn pool_removed(pool: &str) {
     let p = pool_directory(pool);
     if let Err(e) = fs::remove_dir_all(&p) {
@@ -106,66 +132,9 @@ pub fn pool_removed(pool: &str) {
     }
 }
 
-/// Rename the directory to match the pool's new name.
-pub fn pool_renamed(old_name: &str, new_name: &str) {
-    let old = pool_directory(old_name);
-    let new = pool_directory(new_name);
-    if let Err(e) = fs::rename(&old, &new) {
-        warn!(
-            "unable to rename pool directory old {:?}, new {:?}, reason {:?}",
-            old, new, e
-        );
-    }
-}
-
-/// Create a symlink to the new filesystem's block device within its pool's
-/// directory.
-pub fn filesystem_added(pool_name: &str, fs_name: &str, devnode: &Path) {
-    let p = filesystem_mount_path(pool_name, fs_name);
-
-    // Remove existing and recreate to ensure it points to the correct devnode
-    let _ = fs::remove_file(&p);
-    if let Err(e) = symlink(devnode, &p) {
-        warn!(
-            "unable to create symlink for {:?} -> {:?}, reason {:?}",
-            devnode, p, e
-        );
-    }
-}
-
-/// Remove the symlink when the filesystem is destroyed.
-pub fn filesystem_removed(pool_name: &str, fs_name: &str) {
-    let p = filesystem_mount_path(pool_name, fs_name);
-    if let Err(e) = fs::remove_file(&p) {
-        warn!(
-            "unable to remove symlink for filesystem {:?}, reason {:?}",
-            p, e
-        );
-    }
-}
-
-/// Rename the symlink to track the filesystem's new name.
-pub fn filesystem_renamed(pool_name: &str, old_name: &str, new_name: &str) {
-    let old = filesystem_mount_path(pool_name, old_name);
-    let new = filesystem_mount_path(pool_name, new_name);
-    if let Err(e) = fs::rename(&old, &new) {
-        warn!(
-            "unable to rename filesystem symlink for {:?} -> {:?}, reason {:?}",
-            old, new, e
-        );
-    }
-}
-
 /// Given a pool name, synthesize a pool directory name for storing filesystem
 /// mount paths.
+#[cfg(test)]
 fn pool_directory<T: AsRef<str>>(pool_name: T) -> PathBuf {
     vec![DEV_PATH, pool_name.as_ref()].iter().collect()
-}
-
-/// Given a pool name and a filesystem name, return the path it should be
-/// available as a device for mounting.
-pub fn filesystem_mount_path<T: AsRef<str>>(pool_name: T, fs_name: T) -> PathBuf {
-    vec![DEV_PATH, pool_name.as_ref(), fs_name.as_ref()]
-        .iter()
-        .collect()
 }

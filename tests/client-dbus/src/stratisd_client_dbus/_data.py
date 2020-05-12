1aea99555ca58d7772abdcd0941d59b8588d9527
John Baublitz <jbaublitz@redhat.com>
2020-05-12 13:52:25 -0400

    Update KernelKey in udev testing

--- tests/client-dbus/src/stratisd_client_dbus/_data.py
+++ tests/client-dbus/src/stratisd_client_dbus/_data.py
@@ -62,6 +62,20 @@ SPECS = {
 <arg name="return_code" type="q" direction="out"/>
 <arg name="return_string" type="s" direction="out"/>
 </method>
+<method name="SetKey">
+<arg name="key_desc" type="s" direction="in"/>
+<arg name="key_fd" type="h" direction="in"/>
+<arg name="interactive" type="b" direction="in"/>
+<arg name="result" type="(bb)" direction="out"/>
+<arg name="return_code" type="q" direction="out"/>
+<arg name="return_string" type="s" direction="out"/>
+</method>
+<method name="UnsetKey">
+<arg name="key_desc" type="s" direction="in"/>
+<arg name="result" type="b" direction="out"/>
+<arg name="return_code" type="q" direction="out"/>
+<arg name="return_string" type="s" direction="out"/>
+</method>
 <method name="CreatePool">
 <arg name="name" type="s" direction="in"/>
 <arg name="redundancy" type="(bq)" direction="in"/>

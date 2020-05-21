88ea169aea9b83a95cff92b24a57b7729f6ebe52
mulhern <amulhern@redhat.com>
2020-05-20 20:51:54 -0400

    Use lower-case for keys in JSON representation of StratisIdentifiers
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/backstore/metadata/static_header.rs
+++ src/engine/strat_engine/backstore/metadata/static_header.rs
@@ -70,8 +70,8 @@ impl fmt::Display for StratisIdentifiers {
 impl<'a> Into<Value> for &'a StratisIdentifiers {
     fn into(self) -> Value {
         json!({
-            "pool_UUID": Value::from(self.pool_uuid.to_simple_ref().to_string()),
-            "device_UUID": Value::from(self.device_uuid.to_simple_ref().to_string())
+            "pool_uuid": Value::from(self.pool_uuid.to_simple_ref().to_string()),
+            "device_uuid": Value::from(self.device_uuid.to_simple_ref().to_string())
         })
     }
 }

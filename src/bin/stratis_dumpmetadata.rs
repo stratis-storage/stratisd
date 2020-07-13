b9ebdb597b560e446604496431ec7bcd72afb018
Bryan Gurney <bgurney@redhat.com>
2020-07-13 17:48:52 -0400

    stratis_dumpmetadata: add state and state JSON error handling
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/bin/stratis_dumpmetadata.rs
+++ src/bin/stratis_dumpmetadata.rs
@@ -23,10 +23,14 @@ fn run(devpath: String) -> Result<(), String> {
 
     devfile.seek(SeekFrom::Start(0)).unwrap();
 
-    let loaded_state = bda.load_state(&mut devfile).unwrap();
+    let loaded_state = bda
+        .load_state(&mut devfile)
+        .map_err(|stateload_err| format!("Error during load state: {}", stateload_err))?;
     println!("State JSON data:");
-    let state_json: Value = serde_json::from_slice(&loaded_state.unwrap()).unwrap();
-    let state_json_pretty: String = serde_json::to_string_pretty(&state_json).unwrap();
+    let state_json: Value = serde_json::from_slice(&loaded_state.unwrap())
+        .map_err(|extract_err| format!("Error during state JSON extract: {}", extract_err))?;
+    let state_json_pretty: String = serde_json::to_string_pretty(&state_json)
+        .map_err(|parse_err| format!("Error during state JSON parse: {}", parse_err))?;
     println!("{}", state_json_pretty);
     Ok(())
 }

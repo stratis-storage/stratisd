2b4f0465432c4320796c4034bda2c6b68b2b741a
Bryan Gurney <bgurney@redhat.com>
2020-08-13 17:27:22 -0400

    Add stratis_dbusquery_version rust program
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

---
+++ src/bin/stratis_dbusquery_version.rs
@@ -0,0 +1,29 @@
+use std::time::Duration;
+use dbus::{
+    arg::{RefArg, Variant},
+    self,
+    blocking::Connection,
+};
+use lazy_static::lazy_static;
+
+pub const STRATIS_BUS_NAME: &str = "org.storage.stratis2";
+pub const STRATIS_MANAGER_OBJECT: &str = "/org/storage/stratis2";
+pub const STRATIS_MANAGER_IFACE: &str = "org.storage.stratis2.Manager.r1";
+lazy_static! {
+    static ref TIMEOUT: Duration = Duration::new(5,0);
+}
+
+type GetVerRet = Variant<Box<dyn RefArg + 'static>>;
+
+fn get_version() -> Result<GetVerRet, dbus::Error> {
+    let connection = Connection::new_system()?;
+    let proxy = connection.with_proxy(STRATIS_BUS_NAME, STRATIS_MANAGER_OBJECT, *TIMEOUT);
+    Ok(proxy
+        .method_call(STRATIS_MANAGER_IFACE, "Version", ())
+        .map(|r: (GetVerRet,)| r.0)?)
+}
+
+fn main() {
+    let vertest = get_version();
+    println!("{:#?}", vertest);
+}

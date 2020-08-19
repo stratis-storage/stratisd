061f291a4aadcbc1fd7335f07cfb43174518542e
mulhern <amulhern@redhat.com>
2020-08-18 21:51:05 -0400

    Move the info structs to a separate module
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/liminal/mod.rs
+++ src/engine/strat_engine/liminal/mod.rs
@@ -2,10 +2,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+mod device_info;
 #[allow(clippy::module_inception)]
 mod liminal;
 
 pub use self::liminal::LiminalDevices;
 
 #[cfg(test)]
-pub use self::liminal::{get_bdas, get_blockdevs, get_metadata, LStratisInfo};
+pub use self::device_info::LStratisInfo;
+#[cfg(test)]
+pub use self::liminal::{get_bdas, get_blockdevs, get_metadata};

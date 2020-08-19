4da9c3e75cc4e547a33e4940b1874382fa124817
Bryan Gurney <bgurney@redhat.com>
2020-08-19 13:35:30 -0400

    handle get_version result
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/bin/stratis_dbusquery_version.rs
+++ src/bin/stratis_dbusquery_version.rs
@@ -31,9 +31,10 @@ fn get_version() -> Result<GetVerRet, dbus::Error> {
 }
 
 fn run() -> Result<Version, String> {
-    let vertest = get_version();
+    let vertest = get_version()
+        .map_err(|vertest_err| format!("vertest_err: {}", vertest_err))?;
     println!("{:#?}", vertest);
-    let verparse = Version::parse(vertest.unwrap().as_str().unwrap())
+    let verparse = Version::parse(vertest.as_str().unwrap())
         .map_err(|verparse_err| format!("verparse_err: {}", verparse_err))?;
     println!("verparse: {:#?}", verparse);
     println!("STRATIS_VER_UDEV_SYMLINK: {:#?}", *STRATIS_VER_UDEV_SYMLINK);

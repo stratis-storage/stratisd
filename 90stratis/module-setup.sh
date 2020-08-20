2b22de073e1dd9bb367d0502957b3a0011bd63b8
John Baublitz <jbaublitz@redhat.com>
2020-08-20 15:42:13 -0400

    Prototype of stratis dracut module

---
+++ 90stratis/module-setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# called by dracut
+check() {
+    require_binaries stratis-min thin_check thin_repair mkfs.xfs xfs_admin xfs_growfs || return 1
+    return 255
+}
+
+# called by dracut
+depends() {
+    echo dm
+    return 0
+}
+
+# called by dracut
+installkernel() {
+    instmods xfs dm_crypt dm-thin-pool
+}
+
+# called by dracut
+install() {
+    inst_multiple stratis-min thin_check thin_repair mkfs.xfs xfs_admin xfs_growfs
+
+    inst_hook init-queue/settled 25 "$moddir/stratis-encrypted-rootfs.sh"
+}
+

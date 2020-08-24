b39e8f223be4fda3541abfae11f4576e86e0a0e7
John Baublitz <jbaublitz@redhat.com>
2020-08-24 15:36:19 -0400

    Limit retries

--- 90stratis/stratis-encrypted-rootfs.sh
+++ 90stratis/stratis-encrypted-rootfs.sh
@@ -4,6 +4,7 @@ KEY_DESC=test
 
 plymouth ask-for-password \
 	--command="stratis-min key set --capture-key --no-tty $KEY_DESC" \
-	--prompt="Enter passphrase"
+	--prompt="Enter passphrase" \
+	--number-of-tries=3
 
 stratis-min pool setup

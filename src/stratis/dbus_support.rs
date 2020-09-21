ce52afb8ccb952aa14f5e8bbad9324c7a8ee55d4
mulhern <amulhern@redhat.com>
2020-09-21 15:34:44 -0400

    No longer iteratively try to set up the D-Bus connection
    If the D-Bus connection can not be set up, return with an error.
    
    This simple approach is now acceptable since the systemd configuration file
    associated with stratisd makes stratisd of type dbus so that it will not
    be started until the D-Bus is available. This is due to the fact that
    a service of type D-Bus automatically requires dbus.socket.
    
    This simplifies the MaybeDbusSupport implementation, reducing the number
    of method to just three, setup(), process(), and register_pool(). The handle
    field of the MaybeDbusSupport struct no longer needs to be optional, since
    it is set once, or not at all.
    
    The process_poll() method no longer needs to take an explicit timeout;
    it can wait indefinitely for the next event, instead of checking every
    second to see if the D-Bus is up until the D-Bus is up.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/stratis/dbus_support.rs
+++ src/stratis/dbus_support.rs
@@ -11,7 +11,10 @@
 
 use std::{cell::RefCell, rc::Rc};
 
-use crate::engine::{Engine, Name, Pool, PoolUuid};
+use crate::{
+    engine::{Engine, Name, Pool, PoolUuid},
+    stratis::StratisResult,
+};
 
 #[cfg(feature = "dbus_enabled")]
 use crate::{
@@ -21,102 +24,56 @@ use crate::{
 
 pub struct MaybeDbusSupport {
     #[cfg(feature = "dbus_enabled")]
-    handle: Option<DbusConnectionData>,
+    handle: DbusConnectionData,
 }
 
 // If D-Bus compiled out, do very little.
 #[cfg(not(feature = "dbus_enabled"))]
 impl MaybeDbusSupport {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> MaybeDbusSupport {
-        MaybeDbusSupport {}
+    pub fn setup(_engine: &Rc<RefCell<dyn Engine>>) -> StratisResult<MaybeDbusSupport> {
+        Ok(MaybeDbusSupport {})
     }
 
-    pub fn process(
-        &mut self,
-        _engine: &Rc<RefCell<dyn Engine>>,
-        _fds: &mut Vec<libc::pollfd>,
-        _dbus_client_index_start: usize,
-    ) {
-    }
+    pub fn process(&mut self, _fds: &mut Vec<libc::pollfd>, _dbus_client_index_start: usize) {}
 
     pub fn register_pool(&mut self, _pool_name: &Name, _pool_uuid: PoolUuid, _pool: &mut dyn Pool) {
-    }
-
-    pub fn poll_timeout(&self) -> i32 {
-        // Non-DBus timeout is infinite
-        -1
     }
 }
 
 #[cfg(feature = "dbus_enabled")]
 impl MaybeDbusSupport {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> MaybeDbusSupport {
-        MaybeDbusSupport { handle: None }
-    }
-
-    /// Connect to D-Bus and register pools, if not already connected.
-    /// Return the connection, if made or already existing, otherwise, None.
-    fn setup_connection(
-        &mut self,
-        engine: &Rc<RefCell<dyn Engine>>,
-    ) -> Option<&mut DbusConnectionData> {
-        if self.handle.is_none() {
-            match DbusConnectionData::connect(Rc::clone(engine)) {
-                Err(err) => {
-                    warn!("D-Bus API is not available: {}", err);
+    pub fn setup(engine: &Rc<RefCell<dyn Engine>>) -> StratisResult<MaybeDbusSupport> {
+        DbusConnectionData::connect(Rc::clone(engine))
+            .map(|mut handle| {
+                let event_handler = Box::new(EventHandler::new(Rc::clone(&handle.connection)));
+                get_engine_listener_list_mut().register_listener(event_handler);
+                for (pool_name, pool_uuid, pool) in engine.borrow_mut().pools_mut() {
+                    handle.register_pool(&pool_name, pool_uuid, pool)
                 }
-                Ok(mut handle) => {
-                    info!("D-Bus API is available");
-                    let event_handler = Box::new(EventHandler::new(Rc::clone(&handle.connection)));
-                    get_engine_listener_list_mut().register_listener(event_handler);
-                    // Register all the pools with dbus
-                    for (pool_name, pool_uuid, pool) in engine.borrow_mut().pools_mut() {
-                        handle.register_pool(&pool_name, pool_uuid, pool)
-                    }
-                    self.handle = Some(handle);
-                }
-            }
-        };
-        self.handle.as_mut()
+                info!("D-Bus API is available");
+                MaybeDbusSupport { handle }
+            })
+            .map_err(|err| err.into())
     }
 
     /// Handle any client dbus requests.
-    pub fn process(
-        &mut self,
-        engine: &Rc<RefCell<dyn Engine>>,
-        fds: &mut Vec<libc::pollfd>,
-        dbus_client_index_start: usize,
-    ) {
-        if let Some(handle) = self.setup_connection(engine) {
-            handle.handle(&fds[dbus_client_index_start..]);
+    pub fn process(&mut self, fds: &mut Vec<libc::pollfd>, dbus_client_index_start: usize) {
+        self.handle.handle(&fds[dbus_client_index_start..]);
 
-            // Refresh list of dbus fds to poll for. This can change as
-            // D-Bus clients come and go.
-            fds.truncate(dbus_client_index_start);
-            fds.extend(
-                handle
-                    .connection
-                    .borrow()
-                    .watch_fds()
-                    .iter()
-                    .map(|w| w.to_pollfd()),
-            );
-        }
+        // Refresh list of dbus fds to poll for. This can change as
+        // D-Bus clients come and go.
+        fds.truncate(dbus_client_index_start);
+        fds.extend(
+            self.handle
+                .connection
+                .borrow()
+                .watch_fds()
+                .iter()
+                .map(|w| w.to_pollfd()),
+        );
     }
 
     pub fn register_pool(&mut self, pool_name: &Name, pool_uuid: PoolUuid, pool: &mut dyn Pool) {
-        if let Some(h) = self.handle.as_mut() {
-            h.register_pool(pool_name, pool_uuid, pool)
-        }
-    }
-
-    pub fn poll_timeout(&self) -> i32 {
-        // If there is no D-Bus connection set timeout to 1 sec (1000 ms), so
-        // that stratisd can periodically attempt to set up a connection.
-        // If the connection is up, set the timeout to infinite; there is no
-        // need to poll as events will be received.
-        self.handle.as_ref().map_or(1000, |_| -1)
+        self.handle.register_pool(pool_name, pool_uuid, pool)
     }
 }

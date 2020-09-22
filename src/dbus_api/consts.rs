dd3098f088cf4d1924f9edda2a4d22675410d101
John Baublitz <jbaublitz@redhat.com>
2020-09-22 09:23:41 -0400

    Prototype of D-Bus interface for clevis bind and unbind

--- src/dbus_api/consts.rs
+++ src/dbus_api/consts.rs
@@ -23,6 +23,7 @@ pub const LOCKED_POOLS: &str = "LockedPools";
 
 pub const POOL_INTERFACE_NAME: &str = "org.storage.stratis2.pool";
 pub const POOL_INTERFACE_NAME_2_1: &str = "org.storage.stratis2.pool.r1";
+pub const POOL_INTERFACE_NAME_2_2: &str = "org.storage.stratis2.pool.r2";
 pub const POOL_NAME_PROP: &str = "Name";
 pub const POOL_UUID_PROP: &str = "Uuid";
 pub const POOL_HAS_CACHE_PROP: &str = "HasCache";
@@ -67,10 +68,14 @@ pub fn fetch_properties_interfaces() -> Vec<String> {
 /// Get a list of all the standard pool interfaces; i.e., all the revisions of
 /// org.storage.stratis2.pool.
 pub fn standard_pool_interfaces() -> Vec<String> {
-    [POOL_INTERFACE_NAME, POOL_INTERFACE_NAME_2_1]
-        .iter()
-        .map(|s| (*s).to_string())
-        .collect()
+    [
+        POOL_INTERFACE_NAME,
+        POOL_INTERFACE_NAME_2_1,
+        POOL_INTERFACE_NAME_2_2,
+    ]
+    .iter()
+    .map(|s| (*s).to_string())
+    .collect()
 }
 
 /// Get a list of all the standard filesystem interfaces; i.e., all the

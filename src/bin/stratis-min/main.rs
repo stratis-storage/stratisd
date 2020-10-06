a08ae9dbac40277ccb1623e4439033d67b7a4419
John Baublitz <jbaublitz@redhat.com>
2020-10-06 13:25:20 -0400

    Add in logging with the --debug flag and syslog crate

--- src/bin/stratis-min/main.rs
+++ src/bin/stratis-min/main.rs
@@ -5,6 +5,8 @@
 use std::path::Path;
 
 use clap::{App, Arg, ArgGroup, ArgMatches, SubCommand};
+use log::{error, LevelFilter};
+use syslog::{init_unix, Facility};
 
 use libstratis::engine::PoolUuid;
 
@@ -16,72 +18,80 @@ mod udev;
 mod utils;
 
 fn parse_args() -> App<'static, 'static> {
-    App::new("stratis-min").subcommands(vec![
-        SubCommand::with_name("key").subcommands(vec![
-            SubCommand::with_name("set")
-                .group(
-                    ArgGroup::with_name("key_method")
-                        .arg("capture_key")
-                        .arg("keyfile_path")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("capture_key")
-                        .long("--capture-key")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("keyfile_path")
-                        .long("--keyfile-path")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("no_tty")
-                        .long("--no-tty")
-                        .takes_value(false)
-                        .conflicts_with("keyfile_path"),
-                )
-                .arg(Arg::with_name("key_desc").required(true)),
-            SubCommand::with_name("list"),
-            SubCommand::with_name("unset").arg(Arg::with_name("key_desc").required(true)),
-        ]),
-        SubCommand::with_name("pool").subcommands(vec![
-            SubCommand::with_name("setup")
-                .arg(
-                    Arg::with_name("pool_uuid")
-                        .long("--pool-uuid")
-                        .takes_value(true)
-                        .required(false),
-                )
-                .arg(
-                    Arg::with_name("no_tty")
-                        .long("--no-tty")
-                        .takes_value(false)
-                        .requires("pool_uuid"),
-                ),
-            SubCommand::with_name("create")
-                .arg(Arg::with_name("name").required(true))
-                .arg(Arg::with_name("blockdevs").multiple(true).required(true))
-                .arg(
-                    Arg::with_name("key_desc")
-                        .long("--key-desc")
-                        .takes_value(true),
-                ),
-            SubCommand::with_name("init-cache")
-                .arg(Arg::with_name("name").required(true))
-                .arg(Arg::with_name("blockdevs").multiple(true).required(true)),
-            SubCommand::with_name("rename")
-                .arg(Arg::with_name("current_name").required(true))
-                .arg(Arg::with_name("new_name").required(true)),
-            SubCommand::with_name("add-data")
-                .arg(Arg::with_name("name").required(true))
-                .arg(Arg::with_name("blockdevs").multiple(true).required(true)),
-            SubCommand::with_name("destroy").arg(Arg::with_name("name").required(true)),
-            SubCommand::with_name("is-encrypted").arg(Arg::with_name("pool_uuid").required(true)),
-        ]),
-        SubCommand::with_name("report"),
-        SubCommand::with_name("udev").arg(Arg::with_name("dm_name").required(true)),
-    ])
+    App::new("stratis-min")
+        .arg(
+            Arg::with_name("debug")
+                .long("--debug")
+                .takes_value(false)
+                .required(false),
+        )
+        .subcommands(vec![
+            SubCommand::with_name("key").subcommands(vec![
+                SubCommand::with_name("set")
+                    .group(
+                        ArgGroup::with_name("key_method")
+                            .arg("capture_key")
+                            .arg("keyfile_path")
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("capture_key")
+                            .long("--capture-key")
+                            .takes_value(false),
+                    )
+                    .arg(
+                        Arg::with_name("keyfile_path")
+                            .long("--keyfile-path")
+                            .takes_value(true),
+                    )
+                    .arg(
+                        Arg::with_name("no_tty")
+                            .long("--no-tty")
+                            .takes_value(false)
+                            .conflicts_with("keyfile_path"),
+                    )
+                    .arg(Arg::with_name("key_desc").required(true)),
+                SubCommand::with_name("list"),
+                SubCommand::with_name("unset").arg(Arg::with_name("key_desc").required(true)),
+            ]),
+            SubCommand::with_name("pool").subcommands(vec![
+                SubCommand::with_name("setup")
+                    .arg(
+                        Arg::with_name("pool_uuid")
+                            .long("--pool-uuid")
+                            .takes_value(true)
+                            .required(false),
+                    )
+                    .arg(
+                        Arg::with_name("no_tty")
+                            .long("--no-tty")
+                            .takes_value(false)
+                            .requires("pool_uuid"),
+                    ),
+                SubCommand::with_name("create")
+                    .arg(Arg::with_name("name").required(true))
+                    .arg(Arg::with_name("blockdevs").multiple(true).required(true))
+                    .arg(
+                        Arg::with_name("key_desc")
+                            .long("--key-desc")
+                            .takes_value(true),
+                    ),
+                SubCommand::with_name("init-cache")
+                    .arg(Arg::with_name("name").required(true))
+                    .arg(Arg::with_name("blockdevs").multiple(true).required(true)),
+                SubCommand::with_name("rename")
+                    .arg(Arg::with_name("current_name").required(true))
+                    .arg(Arg::with_name("new_name").required(true)),
+                SubCommand::with_name("add-data")
+                    .arg(Arg::with_name("name").required(true))
+                    .arg(Arg::with_name("blockdevs").multiple(true).required(true)),
+                SubCommand::with_name("destroy").arg(Arg::with_name("name").required(true)),
+                SubCommand::with_name("is-encrypted")
+                    .arg(Arg::with_name("pool_uuid").required(true)),
+            ]),
+            SubCommand::with_name("report"),
+            SubCommand::with_name("udev").arg(Arg::with_name("dm_name").required(true)),
+        ])
 }
 
 fn get_paths_from_args<'a>(args: &'a ArgMatches<'a>) -> Vec<&'a Path> {
@@ -91,12 +101,18 @@ fn get_paths_from_args<'a>(args: &'a ArgMatches<'a>) -> Vec<&'a Path> {
         .collect::<Vec<_>>()
 }
 
-fn main() -> Result<(), String> {
+fn main_with_err() -> Result<(), String> {
     let mut app = parse_args();
     let mut help = Vec::new();
     app.write_long_help(&mut help).map_err(|e| e.to_string())?;
     let help = String::from_utf8(help).map_err(|e| e.to_string())?;
     let args = app.get_matches();
+    let level = if args.is_present("debug") {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Warn
+    };
+    init_unix(Facility::LOG_DAEMON, level).map_err(|e| e.to_string())?;
     if let Some(subcommand) = args.subcommand_matches("key") {
         if let Some(args) = subcommand.subcommand_matches("set") {
             key::key_set(
@@ -163,6 +179,15 @@ fn main() -> Result<(), String> {
         udev::udev(args.value_of("dm_name").expect("required"))
     } else {
         println!("{}", help);
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), String> {
+    if let Err(e) = main_with_err() {
+        error!("stratis-min failed with error: {}", e);
+        Err(e)
+    } else {
         Ok(())
     }
 }

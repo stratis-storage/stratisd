933d6ebdd3aa9730eeb7a778b112a06bcd218f2e
mulhern <amulhern@redhat.com>
2020-11-11 12:27:07 -0500

    Fix up a comment
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/backstore/crypt.rs
+++ src/engine/strat_engine/backstore/crypt.rs
@@ -521,7 +521,7 @@ impl CryptHandle {
         }
     }
 
-    /// Bind the given device to a tang server using clevis.
+    /// Bind the given device using clevis.
     pub fn clevis_bind(&mut self, keyfile_path: &Path, pin: &str, json: &Value) -> Result<()> {
         clevis_luks_bind(&self.physical_path, keyfile_path, pin, json, false)
             .map_err(|e| LibcryptErr::Other(e.to_string()))

092d007c4e315c7cc366698a8476256b17909213
mulhern <amulhern@redhat.com>
2020-12-22 19:25:16 -0500

    Make a Segments data structure for operations in RangeAllocator
    Distribute tests between the two.
    
    Remove the TODOs, they've both been taken care of by these changes.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/strat_engine/backstore/range_alloc.rs
+++ src/engine/strat_engine/backstore/range_alloc.rs
@@ -2,24 +2,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::{cmp::min, collections::BTreeMap};
+use std::cmp::min;
 
 use devicemapper::Sectors;
 
 use crate::{
-    engine::strat_engine::metadata::BlockdevSize,
-    stratis::{ErrorEnum, StratisError, StratisResult},
+    engine::strat_engine::{backstore::shared::Segments, metadata::BlockdevSize},
+    stratis::StratisResult,
 };
 
 #[derive(Debug)]
 pub struct RangeAllocator {
-    /// No sectors can be allocated beyond this point.
-    limit: Sectors,
-    /// A map of chunks of data allocated for a single blockdev. The LHS
-    /// is the offset from the start of the device, the RHS is the length.
-    /// Uses a BTReeMap so that an iteration over the elements in the tree
-    /// will be ordered by the value of the LHS.
-    used: BTreeMap<Sectors, Sectors>,
+    segments: Segments,
 }
 
 impl RangeAllocator {
@@ -29,145 +23,24 @@ impl RangeAllocator {
         limit: BlockdevSize,
         initial_used: &[(Sectors, Sectors)],
     ) -> StratisResult<RangeAllocator> {
-        let mut allocator = RangeAllocator {
-            limit: limit.sectors(),
-            used: BTreeMap::new(),
-        };
-        allocator.insert_ranges(initial_used)?;
-        Ok(allocator)
+        let mut segments = Segments::new(limit.sectors());
+        segments.insert_all(initial_used)?;
+        Ok(RangeAllocator { segments })
     }
 
     /// The maximum allocation from this manager
     pub fn size(&self) -> BlockdevSize {
-        BlockdevSize::new(self.limit)
-    }
-
-    fn check_for_overflow(&self, off: Sectors, len: Sectors) -> StratisResult<()> {
-        if let Some(sum) = off.checked_add(len) {
-            if sum > self.limit {
-                let err_msg = format!(
-                    "elements in range ({}, {}) exceed limit {}",
-                    off, len, self.limit
-                );
-                return Err(StratisError::Engine(ErrorEnum::Invalid, err_msg));
-            }
-        } else {
-            let err_msg = format!(
-                "elements in range ({}, {}) inexpressible in this format",
-                off, len
-            );
-            return Err(StratisError::Engine(ErrorEnum::Invalid, err_msg));
-        }
-        Ok(())
-    }
-
-    /// Mark ranges previously marked as unused as now used.
-    /// Return an error if ranges overlap with each other or with previously
-    /// inserted ranges.
-    /// TODO: Make this operation atomic.
-    /// TODO: Consider using a different algorithmic that first sorts ranges
-    /// and then merges used and ranges by traversing them in parallel, for
-    /// efficiency.
-    fn insert_ranges(&mut self, ranges: &[(Sectors, Sectors)]) -> StratisResult<()> {
-        for &(off, len) in ranges {
-            self.check_for_overflow(off, len)?;
-
-            let prev = self.used.range(..off).rev().next().map(|(k, v)| (*k, *v));
-
-            let mut contig_prev = None;
-            if let Some((prev_off, prev_len)) = prev {
-                if prev_off + prev_len > off {
-                    let err_msg = format!(
-                        "range starting at {} overlaps previous range ({}, {})",
-                        off, prev_off, prev_len
-                    );
-                    return Err(StratisError::Engine(ErrorEnum::Invalid, err_msg));
-                }
-                if prev_off + prev_len == off {
-                    contig_prev = Some((prev_off, prev_len))
-                }
-            }
-
-            let next = self.used.range(off..).next().map(|(k, v)| (*k, *v));
-
-            let mut contig_next = None;
-            if let Some((next_off, next_len)) = next {
-                if off + len > next_off {
-                    let err_msg = format!(
-                        "range ({}, {}) overlaps subsequent range starting at {}",
-                        off, len, next_off
-                    );
-                    return Err(StratisError::Engine(ErrorEnum::Invalid, err_msg));
-                }
-                if off + len == next_off {
-                    contig_next = Some((next_off, next_len))
-                }
-            }
-
-            match (contig_prev, contig_next) {
-                (None, None) => {
-                    self.used.insert(off, len);
-                }
-                (None, Some((next_off, next_len))) => {
-                    // Contig with next, make new entry
-                    self.used.insert(off, len + next_len);
-                    self.used
-                        .remove(&next_off)
-                        .expect("matched Some((next_off, ...");
-                }
-                (Some((prev_off, prev_len)), None) => {
-                    // Contig with prev, just extend prev
-                    *self
-                        .used
-                        .get_mut(&prev_off)
-                        .expect("matched Some((prev_off, ...") = prev_len + len;
-                }
-                (Some((prev_off, prev_len)), Some((next_off, next_len))) => {
-                    // Contig with both, remove next and extend prev
-                    self.used.remove(&next_off);
-                    *self
-                        .used
-                        .get_mut(&prev_off)
-                        .expect("matched Some((prev_off, ...") = prev_len + len + next_len;
-                }
-            }
-        }
-        Ok(())
+        BlockdevSize::new(self.segments.limit())
     }
 
     /// Available sectors
     pub fn available(&self) -> Sectors {
-        self.limit - self.used()
+        self.segments.limit() - self.used()
     }
 
     /// Allocated sectors
     pub fn used(&self) -> Sectors {
-        self.used.values().cloned().sum()
-    }
-
-    /// Get a list of (offset, length) segments that are in use.
-    /// The segments are ordered by the first element of the pair because
-    /// the underslying data structure, used, is a BTreeMap.
-    fn used_ranges(&self) -> Vec<(Sectors, Sectors)> {
-        self.used.iter().map(|(k, v)| (*k, *v)).collect()
-    }
-
-    /// Get a list of (offset, length) segments that are not in use
-    fn avail_ranges(&self) -> Vec<(Sectors, Sectors)> {
-        let mut free = Vec::new();
-
-        // Insert an entry to mark the end so the fold works correctly
-        let mut used = self.used_ranges();
-        used.push((self.limit, Sectors(0)));
-
-        used.into_iter().fold(Sectors(0), |prev_end, (start, len)| {
-            if prev_end < start {
-                free.push((prev_end, start - prev_end))
-            }
-            start + len
-        });
-
-        free
+        self.segments.iter().map(|(_, l)| l).cloned().sum()
     }
 
     /// Attempt to allocate. Returns number of sectors allocated (may
@@ -179,21 +52,19 @@ impl RangeAllocator {
         let mut segs = Vec::new();
         let mut needed = amount;
 
-        for (start, len) in self.avail_ranges() {
+        for (&start, &len) in self.segments.complement().iter() {
             if needed == Sectors(0) {
                 break;
             }
-
             let to_use = min(needed, len);
-
             let used_range = (start, to_use);
             segs.push(used_range);
-            self.insert_ranges(&[used_range])
-                .expect("available ranges must be insertable");
-
             needed -= to_use;
         }
 
+        self.segments
+            .insert_all(&segs)
+            .expect("all segments verified to be in available ranges");
         (amount - needed, segs)
     }
 }
@@ -214,14 +85,11 @@ mod tests {
     /// 8. Verify that number of available sectors is 60, used is 68.
     /// 9. Request all available, then verify that nothing is left.
     fn test_allocator_allocations() {
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &[]).unwrap();
-
-        assert_eq!(allocator.used(), Sectors(0));
-        assert_eq!(allocator.available(), Sectors(128));
-
-        allocator
-            .insert_ranges(&[(Sectors(10), Sectors(100))])
-            .unwrap();
+        let mut allocator = RangeAllocator::new(
+            BlockdevSize::new(Sectors(128)),
+            &[(Sectors(10), Sectors(100))],
+        )
+        .unwrap();
 
         assert_eq!(allocator.used(), Sectors(100));
         assert_eq!(allocator.available(), Sectors(28));
@@ -238,65 +106,6 @@ mod tests {
     }
 
     #[test]
-    // Verify some proper functioning when allocator initialized with ranges.
-    fn test_allocator_initialized_with_range() {
-        let ranges = [
-            (Sectors(20), Sectors(10)),
-            (Sectors(10), Sectors(10)),
-            (Sectors(30), Sectors(10)),
-        ];
-        let allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &ranges).unwrap();
-        let used = allocator.used_ranges();
-        assert_eq!(used.len(), 1);
-        assert_eq!(used[0], (Sectors(10), Sectors(30)));
-    }
-
-    #[test]
-    /// Verify insert_ranges properly coalesces adjacent allocations.
-    fn test_allocator_insert_ranges_contig() {
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &[]).unwrap();
-
-        allocator
-            .insert_ranges(&[(Sectors(20), Sectors(10))])
-            .unwrap();
-        allocator
-            .insert_ranges(&[(Sectors(10), Sectors(10))])
-            .unwrap();
-        allocator
-            .insert_ranges(&[(Sectors(30), Sectors(10))])
-            .unwrap();
-
-        let used = allocator.used_ranges();
-        assert_eq!(used.len(), 1);
-        assert_eq!(used[0], (Sectors(10), Sectors(30)));
-    }
-
-    #[test]
-    /// Verify that the largest possible limit may be used for the
-    /// allocator.
-    fn test_max_allocator_range() {
-        use std::u64::MAX;
-
-        RangeAllocator::new(BlockdevSize::new(Sectors(MAX)), &[]).unwrap();
-    }
-
-    #[test]
-    fn test_allocator_insert_prev_overlap() {
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &[]).unwrap();
-
-        let bad_insert_ranges = [(Sectors(21), Sectors(20)), (Sectors(40), Sectors(40))];
-        assert_matches!(allocator.insert_ranges(&bad_insert_ranges), Err(_))
-    }
-
-    #[test]
-    fn test_allocator_insert_next_overlap() {
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &[]).unwrap();
-
-        let bad_insert_ranges = [(Sectors(40), Sectors(1)), (Sectors(39), Sectors(2))];
-        assert_matches!(allocator.insert_ranges(&bad_insert_ranges), Err(_))
-    }
-
-    #[test]
     /// Verify that insert_ranges() errors when all sectors have already been
     /// allocated.
     fn test_allocator_failures_range_overwrite() {
@@ -306,33 +115,10 @@ mod tests {
         assert_eq!(request.0, Sectors(128));
         assert_eq!(request.1, &[(Sectors(0), Sectors(128))]);
 
-        assert_matches!(allocator.insert_ranges(&[(Sectors(1), Sectors(1))]), Err(_));
-    }
+        assert!(allocator.segments.complement().iter().next().is_none());
 
-    #[test]
-    /// Verify that insert_ranges() errors when an element outside the range
-    /// limit is requested.
-    fn test_allocator_failures_overflow_limit() {
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(128)), &[]).unwrap();
-
-        // overflow limit range
         assert_matches!(
-            allocator.insert_ranges(&[(Sectors(1), Sectors(128))]),
-            Err(_)
-        );
-    }
-
-    #[test]
-    /// Verify that insert_ranges() errors when an element in a requested range
-    /// exceeds u64::MAX.
-    fn test_allocator_failures_overflow_max() {
-        use std::u64::MAX;
-
-        let mut allocator = RangeAllocator::new(BlockdevSize::new(Sectors(MAX)), &[]).unwrap();
-
-        // overflow max u64
-        assert_matches!(
-            allocator.insert_ranges(&[(Sectors(MAX), Sectors(1))]),
+            allocator.segments.insert_all(&[(Sectors(1), Sectors(1))]),
             Err(_)
         );
     }
--- src/engine/strat_engine/backstore/shared.rs
+++ src/engine/strat_engine/backstore/shared.rs
@@ -4,9 +4,9 @@
 
 // Methods that are shared by the cache tier and the data tier.
 
-use std::collections::HashMap;
+use std::collections::{btree_map, BTreeMap, BTreeSet, HashMap};
 
-use devicemapper::Device;
+use devicemapper::{Device, Sectors};
 
 use crate::{
     engine::{
@@ -18,6 +18,286 @@ use crate::{
     },
     stratis::{ErrorEnum, StratisError, StratisResult},
 };
+
+#[derive(Debug)]
+pub struct Segments {
+    // the end, no sectors can be allocated beyond this point
+    limit: Sectors,
+    // A map of chunks of data allocated for a single blockdev. The LHS
+    // is the offset from the start of the device, the RHS is the length.
+    // Uses a BTReeMap so that an iteration over the elements in the tree
+    // will be ordered by the value of the LHS.
+    // Invariant: forall (s, l) in used there is no s_i, l_i and
+    // s_(i + 1), l_(i + 1) s.t. s_1 + l_1 >= s_(i+1)
+    used: BTreeMap<Sectors, Sectors>,
+}
+
+impl Segments {
+    /// Create a new Segments struct with the designated limit and no used
+    /// ranges.
+    pub fn new(limit: Sectors) -> Segments {
+        Segments {
+            limit,
+            used: BTreeMap::new(),
+        }
+    }
+
+    /// The boundary past which no allocation is considered.
+    pub fn limit(&self) -> Sectors {
+        self.limit
+    }
+
+    pub fn iter(&self) -> Iter {
+        Iter {
+            items: self.used.iter(),
+        }
+    }
+
+    // Locate two adjacent keys in used. LHS <= value and RHS >= value.
+    // If LHS == RHS then they both equal value. If both are None, then
+    // used is empty. Returns an error if value > limit.
+    // Postcondition: value < limit
+    fn locate_prev_and_next(
+        &self,
+        value: Sectors,
+    ) -> StratisResult<(Option<Sectors>, Option<Sectors>)> {
+        if value >= self.limit {
+            return Err(StratisError::Engine(
+                ErrorEnum::Invalid,
+                format!(
+                    "value specified for start of range, {}, exceeds limit, {}",
+                    value, self.limit
+                ),
+            ));
+        }
+
+        let mut prev = None;
+        let mut next = None;
+        for (&key, _) in self.used.iter() {
+            if value >= key {
+                prev = Some(key);
+            }
+            if value <= key {
+                next = Some(key);
+                break;
+            }
+        }
+
+        Ok((prev, next))
+    }
+
+    // Return the result of what should be obtained on an insertion. A None at
+    // the start or the end of the tuple indicates a contiguous range, hence
+    // a removal from used if it was previously present.
+    // Precondition: prev.is_some() -> prev in self.used
+    // Precondition: next.is_some() -> next in self.used
+    // Precondition: range.1 != 0
+    // Postcondition: prev.is_none() -> result.0.is_none()
+    // Postcondition: next.is_none() -> result.2.is_none()
+    // Postcondition: prev.is_some() && result.0.is_none() -> result.1.0 == prev
+    // Postcondition: range.0 + range.1 <= Sectors max
+    #[allow(clippy::type_complexity)]
+    fn insertion_result(
+        &self,
+        prev: Option<Sectors>,
+        next: Option<Sectors>,
+        range: &(Sectors, Sectors),
+    ) -> StratisResult<(
+        Option<(Sectors, Sectors)>,
+        (Sectors, Sectors),
+        Option<(Sectors, Sectors)>,
+    )> {
+        let (start, len) = range;
+        let (start, len) = (*start, *len);
+
+        if start + len < start {
+            return Err(StratisError::Engine(
+                ErrorEnum::Invalid,
+                format!(
+                    "range ({}, {}) extends beyond maximum possible size",
+                    start, len
+                ),
+            ));
+        }
+
+        if start + len > self.limit {
+            return Err(StratisError::Engine(
+                ErrorEnum::Invalid,
+                format!(
+                    "range ({}, {}) extends beyond limit {}",
+                    start, len, self.limit
+                ),
+            ));
+        };
+
+        let res: (Sectors, Sectors) = (start, len);
+
+        let (lhs, (new_start, new_len)) = if let Some(prev) = prev {
+            let prev_len: Sectors = *self.used.get(&prev).expect("see precondition");
+            if prev + prev_len > start {
+                return Err(StratisError::Engine(
+                    ErrorEnum::Invalid,
+                    format!(
+                        "range to add ({}, {}) overlaps previous range ({}, {})",
+                        start, len, prev, prev_len
+                    ),
+                ));
+            }
+
+            if prev + prev_len == start {
+                (None, (prev, prev_len + len))
+            } else {
+                (Some((prev, prev_len)), res)
+            }
+        } else {
+            (None, res)
+        };
+
+        let (res, rhs) = if let Some(next) = next {
+            if new_start + new_len > next {
+                return Err(StratisError::Engine(
+                    ErrorEnum::Invalid,
+                    format!(
+                        "range to add ({}, {}) overlaps subsequent range starting at {}",
+                        new_start, new_len, next
+                    ),
+                ));
+            }
+
+            let next_len: Sectors = *self.used.get(&next).expect("see precondition");
+
+            if new_start + new_len == next {
+                ((new_start, new_len + next_len), None)
+            } else {
+                ((new_start, new_len), Some((next, next_len)))
+            }
+        } else {
+            ((new_start, new_len), None)
+        };
+
+        Ok((lhs, res, rhs))
+    }
+
+    /// Insert specified range into self. Return an error if there is any
+    /// overlap between the specified range and existing ranges. If the range
+    /// is contiguous with an existing range, combine the two.
+    /// Inserting a 0 length range has no effect.
+    pub fn insert(&mut self, range: &(Sectors, Sectors)) -> StratisResult<()> {
+        let (start, len) = range;
+
+        if *len == Sectors(0) {
+            return Ok(());
+        }
+
+        let (prev, next) = self.locate_prev_and_next(*start)?;
+        let (prev_res, (new_start, new_len), next_res) =
+            self.insertion_result(prev, next, range)?;
+
+        if let Some(prev) = prev {
+            if prev_res.is_none() {
+                self.used.remove(&prev);
+            }
+        }
+
+        if let Some(next) = next {
+            if next_res.is_none() {
+                self.used.remove(&next);
+            }
+        }
+
+        assert!(
+            self.used.insert(new_start, new_len).is_none(),
+            "removed in previous steps if present"
+        );
+
+        Ok(())
+    }
+
+    /// Insert specified ranges into self. Return an error if any of the
+    /// ranges overlaps with existing ranges in self or if the specified ranges
+    /// overlap w/ each other.
+    /// The operation is atomic; either all ranges or none will be inserted.
+    pub fn insert_all(&mut self, ranges: &[(Sectors, Sectors)]) -> StratisResult<()> {
+        let mut temp = Segments::new(self.limit);
+        for range in ranges.iter() {
+            temp.insert(range)?;
+        }
+
+        let union = self.union(&temp)?;
+
+        self.used = union.used;
+
+        Ok(())
+    }
+
+    /// Take the union of two Segments. Require that both Segments objects have
+    /// the same limit, for simplicity.
+    pub fn union(&self, other: &Segments) -> StratisResult<Segments> {
+        if self.limit != other.limit {
+            return Err(StratisError::Engine(
+                ErrorEnum::Invalid,
+                "limits differ between Segments structs, can not do a union".into(),
+            ));
+        }
+
+        let keys = self
+            .used
+            .keys()
+            .chain(other.used.keys())
+            .cloned()
+            .collect::<BTreeSet<Sectors>>();
+
+        let mut union = Segments::new(self.limit);
+
+        for key in keys.iter().rev() {
+            if let Some(val) = self.used.get(key) {
+                union.insert(&(*key, *val))?;
+            }
+            if let Some(val) = other.used.get(key) {
+                union.insert(&(*key, *val))?;
+            }
+        }
+
+        Ok(union)
+    }
+
+    /// A Segments object that is the complement of self, i.e., its used
+    /// ranges are self's free ranges and vice-versa.
+    pub fn complement(&self) -> Segments {
+        let mut free = BTreeMap::new();
+        let mut prev_end = Sectors(0);
+        for (&start, &len) in self.used.iter() {
+            let range = start - prev_end;
+            if range != Sectors(0) {
+                free.insert(prev_end, range);
+            }
+            prev_end = start + len; // always less than self.limit
+        }
+
+        if prev_end < self.limit {
+            free.insert(prev_end, self.limit - prev_end);
+        }
+
+        Segments {
+            limit: self.limit,
+            used: free,
+        }
+    }
+}
+
+/// An iterator for Segments
+pub struct Iter<'a> {
+    items: btree_map::Iter<'a, Sectors, Sectors>,
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = (&'a Sectors, &'a Sectors);
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.items.next()
+    }
+}
 
 /// Given a function that translates a Stratis UUID to a device
 /// number, and some metadata that describes a particular segment within
@@ -91,4 +371,93 @@ pub fn coalesce_blkdevsegs(left: &[BlkDevSegment], right: &[BlkDevSegment]) -> V
         segments.extend_from_slice(right);
     }
     segments
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // Verify some proper functioning when allocator initialized with ranges.
+    fn test_allocator_initialized_with_range() {
+        let mut allocator = Segments::new(Sectors(128));
+
+        let ranges = [
+            (Sectors(20), Sectors(10)),
+            (Sectors(10), Sectors(10)),
+            (Sectors(30), Sectors(10)),
+        ];
+        allocator.insert_all(&ranges).unwrap();
+
+        assert_eq!(allocator.used.len(), 1);
+        assert_eq!(
+            allocator.used.iter().next().unwrap(),
+            (&Sectors(10), &Sectors(30))
+        );
+    }
+
+    #[test]
+    /// Verify that insert() properly coalesces adjacent allocations.
+    fn test_allocator_insert_ranges_contig() {
+        let mut allocator = Segments::new(Sectors(128));
+
+        allocator.insert(&(Sectors(20), Sectors(10))).unwrap();
+        allocator.insert(&(Sectors(10), Sectors(10))).unwrap();
+        allocator.insert(&(Sectors(30), Sectors(10))).unwrap();
+
+        assert_eq!(allocator.used.len(), 1);
+        assert_eq!(
+            allocator.used.iter().next().unwrap(),
+            (&Sectors(10), &Sectors(30))
+        );
+    }
+
+    #[test]
+    /// Verify that the largest possible limit may be used for the
+    /// allocator.
+    fn test_max_allocator_range() {
+        use std::u64::MAX;
+
+        Segments::new(Sectors(MAX));
+    }
+
+    #[test]
+    /// Verify that if two argument ranges overlap there is an error.
+    fn test_allocator_insert_prev_overlap() {
+        let mut allocator = Segments::new(Sectors(128));
+
+        let bad_insert_ranges = [(Sectors(21), Sectors(20)), (Sectors(40), Sectors(40))];
+        assert_matches!(allocator.insert_all(&bad_insert_ranges), Err(_))
+    }
+
+    #[test]
+    /// Verify that if two argument ranges overlap there is an error.
+    fn test_allocator_insert_next_overlap() {
+        let mut allocator = Segments::new(Sectors(128));
+
+        let bad_insert_ranges = [(Sectors(40), Sectors(1)), (Sectors(39), Sectors(2))];
+        assert_matches!(allocator.insert_all(&bad_insert_ranges), Err(_))
+    }
+
+    #[test]
+    /// Verify that insert_ranges() errors when an element outside the range
+    /// limit is requested.
+    fn test_allocator_failures_overflow_limit() {
+        let mut allocator = Segments::new(Sectors(128));
+
+        // overflow limit range
+        assert_matches!(allocator.insert(&(Sectors(1), Sectors(128))), Err(_));
+    }
+
+    #[test]
+    /// Verify that insert_ranges() errors when an element in a requested range
+    /// exceeds u64::MAX.
+    fn test_allocator_failures_overflow_max() {
+        use std::u64::MAX;
+
+        let mut allocator = Segments::new(Sectors(MAX));
+
+        // overflow max u64
+        assert_matches!(allocator.insert(&(Sectors(MAX), Sectors(1))), Err(_));
+    }
 }

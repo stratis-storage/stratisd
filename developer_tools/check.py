dfffd09daf5f2079c01f63d24dcb6c1c1137deb4
mulhern <amulhern@redhat.com>
2021-03-16 20:09:57 -0400

    Flatten developer_tools directory
    Signed-off-by: mulhern <amulhern@redhat.com>

--- developer_tools/check.py
+++ developer_tools/check.py
@@ -15,7 +15,7 @@ ARG_MAP = {
         "--disable=I",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'",
     ],
-    "stratis_id": [
+    "stratis_id.py": [
         "--reports=no",
         "--disable=I",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'",

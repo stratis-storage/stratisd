b4d1037ed2cf587e20f2563ad8b3c08009c7c3b3
mulhern <amulhern@redhat.com>
2021-04-08 13:48:15 -0400

    Use sectors for computation, change to bytes at end
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/bin/stratis-utils.rs
+++ src/bin/stratis-utils.rs
@@ -14,7 +14,7 @@ use clap::{App, Arg};
 use data_encoding::BASE32_NOPAD;
 use serde_json::{json, Value};
 
-use devicemapper::Bytes;
+use devicemapper::{Bytes, Sectors};
 
 use libstratis::{
     engine::{blkdev_size, crypt_metadata_size, BDA},
@@ -58,7 +58,12 @@ fn predict_usage(encrypted: bool, devices: Vec<PathBuf>) -> Result<(), Box<dyn E
         Bytes(0)
     };
 
-    let stratis_metadata_size_bytes = BDA::default().extended_size().sectors().bytes();
+    let crypt_metadata_size_sectors = crypt_metadata_size.sectors();
+
+    // verify that crypt metadata size is divisible by sectors size
+    assert_eq!(crypt_metadata_size_sectors.bytes(), crypt_metadata_size);
+
+    let stratis_metadata_size_sectors = BDA::default().extended_size().sectors();
 
     let sizes = devices
         .iter()
@@ -68,28 +73,30 @@ fn predict_usage(encrypted: bool, devices: Vec<PathBuf>) -> Result<(), Box<dyn E
                 .open(&device)
                 .map_err(|e| e.into())
                 .and_then(|f| blkdev_size(&f))
+                // always truncated to sectors by stratisd
+                .map(|s| s.sectors())
         })
         .collect::<StratisResult<Vec<_>>>()?;
 
-    let total_size: Bytes = sizes.iter().cloned().sum();
+    let total_size: Sectors = sizes.iter().cloned().sum();
 
     let stratis_device_sizes = sizes
         .iter()
-        .map(|&s| s - crypt_metadata_size)
+        .map(|&s| s - crypt_metadata_size_sectors)
         .collect::<Vec<_>>();
 
     let stratis_avail_sizes = stratis_device_sizes
         .iter()
-        .map(|&s| s - stratis_metadata_size_bytes)
+        .map(|&s| s - stratis_metadata_size_sectors)
         .collect::<Vec<_>>();
 
-    let avail_size: Bytes = stratis_avail_sizes.iter().cloned().sum();
+    let avail_size: Sectors = stratis_avail_sizes.iter().cloned().sum();
 
     let used_size = total_size - avail_size;
 
-    let total_size_str = Value::String((*total_size).to_string());
-    let used_size_str = Value::String((*used_size).to_string());
-    let avail_size_str = Value::String((*avail_size).to_string());
+    let total_size_str = Value::String((*(total_size.bytes())).to_string());
+    let used_size_str = Value::String((*(used_size.bytes())).to_string());
+    let avail_size_str = Value::String((*(avail_size.bytes())).to_string());
 
     let json = json!(
         {"total": total_size_str, "used": used_size_str, "free": avail_size_str}

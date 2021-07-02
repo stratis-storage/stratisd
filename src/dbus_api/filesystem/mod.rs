29fd1b307c157db0c3f6ad1b525c86b4cd3dc4ac
mulhern <amulhern@redhat.com>
2021-07-01 21:34:04 -0400

    Change 2.5.0 to 3.0.0
    Use an r0 revision number for the 0th revision, for uniformity.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus_api/filesystem/mod.rs
+++ src/dbus_api/filesystem/mod.rs
@@ -55,7 +55,7 @@ pub fn create_dbus_filesystem<'a>(
                 .add_p(filesystem_2_0::created_property(&f)),
         )
         .add(
-            f.interface(consts::FILESYSTEM_INTERFACE_NAME_2_5, ())
+            f.interface(consts::FILESYSTEM_INTERFACE_NAME_3_0, ())
                 .add_m(filesystem_2_0::rename_method(&f))
                 .add_p(filesystem_2_0::devnode_property(&f))
                 .add_p(filesystem_2_0::name_property(&f))
@@ -89,7 +89,7 @@ pub fn create_dbus_filesystem<'a>(
                 .add_m(fetch_properties_2_0::get_properties_method(&f)),
         )
         .add(
-            f.interface(consts::PROPERTY_FETCH_INTERFACE_NAME_2_5, ())
+            f.interface(consts::PROPERTY_FETCH_INTERFACE_NAME_3_0, ())
                 .add_m(fetch_properties_2_0::get_all_properties_method(&f))
                 .add_m(fetch_properties_2_0::get_properties_method(&f)),
         );
@@ -123,7 +123,7 @@ pub fn get_fs_properties(
             consts::FILESYSTEM_POOL_PROP => parent.clone(),
             consts::FILESYSTEM_CREATED_PROP => shared::fs_created_prop(fs)
         },
-        consts::FILESYSTEM_INTERFACE_NAME_2_5 => {
+        consts::FILESYSTEM_INTERFACE_NAME_3_0 => {
             consts::FILESYSTEM_NAME_PROP => shared::fs_name_prop(fs_name),
             consts::FILESYSTEM_UUID_PROP => uuid_to_string!(fs_uuid),
             consts::FILESYSTEM_DEVNODE_PROP => shared::fs_devnode_prop(fs, pool_name, fs_name),

4ec8ebe94700a6700abe310718f7207c3c4be13a
John Baublitz <jbaublitz@redhat.com>
2021-08-12 16:50:18 -0400

    PR revision

--- src/dbus_api/pool/pool_3_0/methods.rs
+++ src/dbus_api/pool/pool_3_0/methods.rs
@@ -381,7 +381,11 @@ pub fn bind_clevis(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
             return Ok(vec![return_message.append3(default_return, rc, rs)]);
         }
     };
-    let msg = match handle_action!(pool.bind_clevis(pin.as_str(), &json)) {
+    let msg = match handle_action!(
+        pool.bind_clevis(pin.as_str(), &json),
+        dbus_context,
+        pool_path.get_name()
+    ) {
         Ok(CreateAction::Identity) => {
             return_message.append3(false, DbusErrorEnum::OK as u16, OK_STRING.to_string())
         }
@@ -418,7 +422,7 @@ pub fn unbind_clevis(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
     let mut mutex_lock = dbus_context.engine.blocking_lock();
     let (_, pool) = get_mut_pool!(mutex_lock; pool_uuid; default_return; return_message);
 
-    let msg = match handle_action!(pool.unbind_clevis()) {
+    let msg = match handle_action!(pool.unbind_clevis(), dbus_context, pool_path.get_name()) {
         Ok(DeleteAction::Identity) => {
             return_message.append3(false, DbusErrorEnum::OK as u16, OK_STRING.to_string())
         }
@@ -465,7 +469,11 @@ pub fn bind_keyring(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
     let mut mutex_lock = dbus_context.engine.blocking_lock();
     let (_, pool) = get_mut_pool!(mutex_lock; pool_uuid; default_return; return_message);
 
-    let msg = match handle_action!(pool.bind_keyring(&key_desc)) {
+    let msg = match handle_action!(
+        pool.bind_keyring(&key_desc),
+        dbus_context,
+        pool_path.get_name()
+    ) {
         Ok(CreateAction::Identity) => {
             return_message.append3(false, DbusErrorEnum::OK as u16, OK_STRING.to_string())
         }
@@ -502,7 +510,7 @@ pub fn unbind_keyring(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
     let mut mutex_lock = dbus_context.engine.blocking_lock();
     let (_, pool) = get_mut_pool!(mutex_lock; pool_uuid; default_return; return_message);
 
-    let msg = match handle_action!(pool.unbind_keyring()) {
+    let msg = match handle_action!(pool.unbind_keyring(), dbus_context, pool_path.get_name()) {
         Ok(DeleteAction::Identity) => {
             return_message.append3(false, DbusErrorEnum::OK as u16, OK_STRING.to_string())
         }
@@ -549,7 +557,11 @@ pub fn rebind_keyring(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
     let mut mutex_lock = dbus_context.engine.blocking_lock();
     let (_, pool) = get_mut_pool!(mutex_lock; pool_uuid; default_return; return_message);
 
-    let msg = match handle_action!(pool.rebind_keyring(&key_desc)) {
+    let msg = match handle_action!(
+        pool.rebind_keyring(&key_desc),
+        dbus_context,
+        pool_path.get_name()
+    ) {
         Ok(RenameAction::Identity) => {
             return_message.append3(false, DbusErrorEnum::OK as u16, OK_STRING.to_string())
         }
@@ -594,7 +606,7 @@ pub fn rebind_clevis(m: &MethodInfo<MTSync<TData>, TData>) -> MethodResult {
     let mut mutex_lock = dbus_context.engine.blocking_lock();
     let (_, pool) = get_mut_pool!(mutex_lock; pool_uuid; default_return; return_message);
 
-    let msg = match handle_action!(pool.rebind_clevis()) {
+    let msg = match handle_action!(pool.rebind_clevis(), dbus_context, pool_path.get_name()) {
         Ok(_) => return_message.append3(true, DbusErrorEnum::OK as u16, OK_STRING.to_string()),
         Err(e) => {
             let (rc, rs) = engine_to_dbus_err_tuple(&e);

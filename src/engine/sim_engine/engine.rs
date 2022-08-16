673e233563e367abd7dc354c44e32a6b932d9f00
mulhern <amulhern@redhat.com>
2022-08-16 11:29:15 -0400

    Remove lint made obsolete by upgrade to edition 2021
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/engine/sim_engine/engine.rs
+++ src/engine/sim_engine/engine.rs
@@ -170,7 +170,6 @@ impl Engine for SimEngine {
         }
     }
 
-    #[allow(rust_2021_incompatible_closure_captures)]
     async fn handle_events(
         &self,
         _: Vec<UdevEngineEvent>,

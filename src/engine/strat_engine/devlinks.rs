f0d89788a5dfb78ac80cefaddc934acc18c58c1f
Bryan Gurney <bgurney@redhat.com>
2023-08-24 15:12:49 -0400

    Fix useless-vec lint
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/engine/strat_engine/devlinks.rs
+++ src/engine/strat_engine/devlinks.rs
@@ -11,7 +11,7 @@ pub const UEVENT_CHANGE_EVENT: &str = "change";
 /// Given a pool name and a filesystem name, return the path it should be
 /// available as a device for mounting.
 pub fn filesystem_mount_path<T: AsRef<str>>(pool_name: T, fs_name: T) -> PathBuf {
-    vec![DEV_PATH, pool_name.as_ref(), fs_name.as_ref()]
+    [DEV_PATH, pool_name.as_ref(), fs_name.as_ref()]
         .iter()
         .collect()
 }

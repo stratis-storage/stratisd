934c3957da057a4a6ad508b830d7a72496041af1
mulhern <amulhern@redhat.com>
2023-09-26 13:32:05 -0400

    Wait for the pool immediately after creation
    Get the pool's UUID after the pool has appeared.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/client-dbus/tests/udev/test_udev.py
+++ tests/client-dbus/tests/udev/test_udev.py
@@ -254,10 +254,8 @@ class UdevTest3(UdevTest):
             (_, (pool_object_path, device_object_paths)) = create_pool(
                 random_string(5), devnodes, key_description=key_description
             )
-            pool_uuid = Pool.Properties.Uuid.Get(get_object(pool_object_path))
-
             self.wait_for_pools(1)
-
+            pool_uuid = Pool.Properties.Uuid.Get(get_object(pool_object_path))
             wait_for_udev(STRATIS_FS_TYPE, get_devnodes(device_object_paths))
 
         if take_down_dm:
@@ -345,9 +343,8 @@ class UdevTest4(UdevTest):
             (_, (pool_object_path, _)) = create_pool(
                 random_string(5), devnodes, key_description=key_description
             )
-            pool_uuid = Pool.Properties.Uuid.Get(get_object(pool_object_path))
-
             self.wait_for_pools(1)
+            pool_uuid = Pool.Properties.Uuid.Get(get_object(pool_object_path))
 
         remove_stratis_dm_devices()
 

4d89fab217118592368a8078b39a64c99b7618f1
John Baublitz <jbaublitz@redhat.com>
2023-11-20 14:17:41 -0500

    Add infrastructure for allocating from either end of device
    This commit adds the ability to allocate both data and metadata from
    either the front of the back of the device. This will be necessary for
    reserving integrity space at the end of the device.

--- src/engine/strat_engine/liminal/setup.rs
+++ src/engine/strat_engine/liminal/setup.rs
@@ -495,7 +495,14 @@ fn get_blockdev(
     // least the recorded size, so all segments should be
     // available to be allocated. If this fails, the most likely
     // conclusion is metadata corruption.
-    let segments = segment_table.get(&dev_uuid);
+    let segs = segment_table.get(&dev_uuid);
+    let mut segments = vec![];
+    segments.extend(segs.unwrap_or(&vec![]));
+    let meta = data_map.get(&dev_uuid);
+    let raid = meta.map(|base| &base.1.raid_meta_allocs);
+    let integrity = meta.map(|base| &base.1.integrity_meta_allocs);
+    segments.extend(raid.unwrap_or(&vec![]));
+    segments.extend(integrity.unwrap_or(&vec![]));
 
     assert_eq!(info.luks, None);
     Ok((
@@ -503,7 +510,7 @@ fn get_blockdev(
         v2::StratBlockDev::new(
             info.dev_info.device_number,
             bda,
-            segments.unwrap_or(&vec![]),
+            &segments,
             bd_save.user_info.clone(),
             bd_save.hardware_info.clone(),
             devnode,
--- src/engine/strat_engine/serde_structs.rs
+++ src/engine/strat_engine/serde_structs.rs
@@ -116,6 +116,12 @@ pub struct BaseDevSave {
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct BaseBlockDevSave {
     pub uuid: DevUuid,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default)]
+    pub raid_meta_allocs: Vec<(Sectors, Sectors)>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default)]
+    pub integrity_meta_allocs: Vec<(Sectors, Sectors)>,
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(serialize_with = "serialize_option_string")]
     pub user_info: Option<String>,

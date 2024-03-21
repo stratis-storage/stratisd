c2f1532c8da52d98e8f1f4ab4fc570f4d97bbc04
Bryan Gurney <bgurney@redhat.com>
2024-03-21 15:48:57 -0400

    Allow unknown lint for static_mut_refs
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/engine/strat_engine/dm.rs
+++ src/engine/strat_engine/dm.rs
@@ -25,6 +25,7 @@ static mut DM_CONTEXT: Option<DmResult<DM>> = None;
 /// Path to logical devices for encrypted devices
 pub const DEVICEMAPPER_PATH: &str = "/dev/mapper";
 
+#[allow(unknown_lints)]
 #[allow(static_mut_refs)]
 pub fn get_dm_init() -> StratisResult<&'static DM> {
     unsafe {

3cc6f926d4b3c8ad7e3872e231f312572eac07d9
Bryan Gurney <bgurney@redhat.com>
2024-06-13 13:50:35 -0400

    Add test stratis_min_pool_add_data()
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>
    (cherry picked from commit 9c9d16bb0c2b77f03cab8aebb2abbc587f85373d)

--- tests/stratis_min.rs
+++ tests/stratis_min.rs
@@ -436,6 +436,22 @@ fn test_stratis_min_pool_stop_name() {
     test_with_stratisd_min_sim(stratis_min_pool_stop_name);
 }
 
+fn stratis_min_pool_add_data() {
+    stratis_min_create_pool_and_fs();
+    let mut cmd = Command::cargo_bin("stratis-min").unwrap();
+    cmd.arg("pool")
+        .arg("add-data")
+        .arg("pn")
+        .arg("/dev/nonexistentblockdev1");
+    cmd.assert().success();
+}
+
+#[test]
+// Test adding a data device to a pool.
+fn test_stratis_min_pool_add_data() {
+    test_with_stratisd_min_sim(stratis_min_pool_add_data);
+}
+
 fn stratis_min_pool_stop_start_name() {
     stratis_min_create_pool_and_fs();
     let mut cmd = Command::cargo_bin("stratis-min").unwrap();

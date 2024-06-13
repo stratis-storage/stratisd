aaaecbbed1d05724ac92cba342a4114c0c836a15
Bryan Gurney <bgurney@redhat.com>
2024-06-13 14:02:14 -0400

    Add test stratis_min_pool_add_cache()
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>
    (cherry picked from commit 2354e3cad58e4ad3c58b155fa9bbb348fdb8f76f)

--- tests/stratis_min.rs
+++ tests/stratis_min.rs
@@ -452,6 +452,28 @@ fn test_stratis_min_pool_add_data() {
     test_with_stratisd_min_sim(stratis_min_pool_add_data);
 }
 
+fn stratis_min_pool_add_cache() {
+    stratis_min_create_pool_and_fs();
+    let mut cmd = Command::cargo_bin("stratis-min").unwrap();
+    cmd.arg("pool")
+        .arg("init-cache")
+        .arg("pn")
+        .arg("/dev/nonexistentblockdev1");
+    cmd.assert().success();
+    let mut cmd = Command::cargo_bin("stratis-min").unwrap();
+    cmd.arg("pool")
+        .arg("add-cache")
+        .arg("pn")
+        .arg("/dev/nonexistentblockdev2");
+    cmd.assert().success();
+}
+
+#[test]
+// Test adding a cache device to a pool.
+fn test_stratis_min_pool_add_cache() {
+    test_with_stratisd_min_sim(stratis_min_pool_add_cache);
+}
+
 fn stratis_min_pool_stop_start_name() {
     stratis_min_create_pool_and_fs();
     let mut cmd = Command::cargo_bin("stratis-min").unwrap();

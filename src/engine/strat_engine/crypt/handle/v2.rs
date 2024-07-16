9f9dbce328899fb518621646ef184ac7689bb1de
John Baublitz <jbaublitz@redhat.com>
2024-07-16 14:21:11 -0400

    Add Engine interface for encrypting pool

--- src/engine/strat_engine/crypt/handle/v2.rs
+++ src/engine/strat_engine/crypt/handle/v2.rs
@@ -749,7 +749,6 @@ impl CryptHandle {
     }
 
     /// Encrypt an unencrypted pool.
-    #[allow(dead_code)]
     pub fn encrypt(
         pool_uuid: PoolUuid,
         thinpool: &mut ThinPool<v2::Backstore>,

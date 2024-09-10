d75c5c45db99e04ecd0cc5e53f01577c4d25d23b
mulhern <amulhern@redhat.com>
2024-09-10 16:23:48 -0400

    Add additional test code
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/client-dbus/tests/udev/test_revert.py
+++ tests/client-dbus/tests/udev/test_revert.py
@@ -24,6 +24,9 @@ import tempfile
 # isort: THIRDPARTY
 from justbytes import Range
 
+# isort: FIRSTPARTY
+from dbus_python_client_gen import DPClientInvocationError
+
 # isort: LOCAL
 from stratisd_client_dbus import Filesystem
 from stratisd_client_dbus._constants import TOP_OBJECT
@@ -95,6 +98,9 @@ class TestRevert(UdevTest):
                 )
             )
 
+            if return_code != 0:
+                raise RuntimeError(f"Failed to create requested snapshot: {message}")
+
             file2 = "file2.txt"
             with open(os.path.join(mountdir, file2), encoding="utf-8", mode="w") as fd:
                 print(file2, file=fd, end="")
@@ -158,5 +164,127 @@ class TestRevert(UdevTest):
 
             self.assertFalse(os.path.exists(os.path.join(mountdir, file2)))
             self.assertFalse(os.path.exists(f"/dev/stratis/{pool_name}/{snap_name}"))
+
+            subprocess.check_call(["umount", mountdir])
+
+    def test_revert_snapshot_chain(self):  # pylint: disable=too-many-locals
+        """
+        Make a chain of snapshots, schedule excess reverts and verify that
+        those yield an error, and then revert the middle link.
+
+        Verify that the snapshot link now points to the origin.
+        """
+        mountdir = tempfile.mkdtemp("_stratis_mnt")
+
+        with ServiceContextManager():
+            device_tokens = self._lb_mgr.create_devices(2)
+
+            pool_name = random_string(5)
+
+            (_, (pool_object_path, _)) = create_pool(
+                pool_name, self._lb_mgr.device_files(device_tokens)
+            )
+
+            fs_name = "fs1"
+            fs_size = Range(1024**3)
+            ((_, fs_object_paths), return_code, message) = (
+                Pool.Methods.CreateFilesystems(
+                    get_object(pool_object_path),
+                    {"specs": [(fs_name, (True, str(fs_size.magnitude)), (False, ""))]},
+                )
+            )
+
+            if return_code != 0:
+                raise RuntimeError(
+                    f"Failed to create a requested filesystem: {message}"
+                )
+
+            settle()
+
+            filepath = f"/dev/stratis/{pool_name}/{fs_name}"
+            subprocess.check_call(["mount", filepath, mountdir])
+
+            file1 = "file1.txt"
+            with open(os.path.join(mountdir, file1), encoding="utf-8", mode="w") as fd:
+                print(file1, file=fd, end="")
+
+            snap_name_1 = "snap1"
+            ((_, snap_object_path_1), return_code, message) = (
+                Pool.Methods.SnapshotFilesystem(
+                    get_object(pool_object_path),
+                    {"origin": fs_object_paths[0][0], "snapshot_name": snap_name_1},
+                )
+            )
+
+            if return_code != 0:
+                raise RuntimeError(f"Failed to create requested snapshot: {message}")
+
+            file2 = "file2.txt"
+            with open(os.path.join(mountdir, file2), encoding="utf-8", mode="w") as fd:
+                print(file2, file=fd, end="")
+
+            Filesystem.Properties.MergeScheduled.Set(
+                get_object(snap_object_path_1), True
+            )
+            subprocess.check_call(["umount", mountdir])
+
+            self.assertTrue(os.path.exists(f"/dev/stratis/{pool_name}/{snap_name_1}"))
+
+            snap_name_2 = "snap2"
+            ((_, snap_object_path_2), return_code, message) = (
+                Pool.Methods.SnapshotFilesystem(
+                    get_object(pool_object_path),
+                    {"origin": snap_object_path_1, "snapshot_name": snap_name_2},
+                )
+            )
+
+            if return_code != 0:
+                raise RuntimeError(f"Failed to create requested snapshot: {message}")
+
+            settle()
+
+            self.assertTrue(os.path.exists(f"/dev/stratis/{pool_name}/{snap_name_2}"))
+            with self.assertRaises(DPClientInvocationError):
+                Filesystem.Properties.MergeScheduled.Set(
+                    get_object(snap_object_path_2), True
+                )
+
+            # Now stop the pool, which should tear down the devices
+            (_, return_code, message) = Manager.Methods.StopPool(
+                get_object(TOP_OBJECT),
+                {
+                    "id": pool_name,
+                    "id_type": "name",
+                },
+            )
+
+            if return_code != 0:
+                raise RuntimeError(f"Failed to stop the pool {pool_name}: {message}")
+
+            (_, return_code, message) = Manager.Methods.StartPool(
+                get_object(TOP_OBJECT),
+                {
+                    "id": pool_name,
+                    "id_type": "name",
+                    "unlock_method": (False, ""),
+                    "key_fd": (False, 0),
+                },
+            )
+
+            if return_code != 0:
+                raise RuntimeError(f"Failed to start the pool {pool_name}: {message}")
+
+            self.wait_for_pools(1)
+
+            settle()
+
+            subprocess.check_call(["mount", filepath, mountdir])
+
+            with open(os.path.join(mountdir, file1), encoding="utf-8") as fd:
+                self.assertEqual(fd.read(), file1)
+
+            self.assertFalse(os.path.exists(os.path.join(mountdir, file2)))
+            self.assertFalse(os.path.exists(f"/dev/stratis/{pool_name}/{snap_name_1}"))
+            self.assertTrue(os.path.exists(f"/dev/stratis/{pool_name}/{snap_name_2}"))
 
             subprocess.check_call(["umount", mountdir])

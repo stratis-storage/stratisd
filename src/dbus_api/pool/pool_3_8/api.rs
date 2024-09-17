d2c638754a3bdfc8a585ba54ec89a6a315b8699c
John Baublitz <jbaublitz@redhat.com>
2024-09-17 15:32:38 -0400

    Add D-Bus interface for reencrypting pool

--- src/dbus_api/pool/pool_3_8/api.rs
+++ src/dbus_api/pool/pool_3_8/api.rs
@@ -11,7 +11,7 @@ use crate::dbus_api::{
         pool_3_8::{
             methods::{
                 bind_clevis, bind_keyring, encrypt_pool, rebind_clevis, rebind_keyring,
-                unbind_clevis, unbind_keyring,
+                reencrypt_pool, unbind_clevis, unbind_keyring,
             },
             props::{
                 get_pool_clevis_infos, get_pool_free_token_slots, get_pool_key_descs,
@@ -144,6 +144,16 @@ pub fn encrypt_pool_method(f: &Factory<MTSync<TData>, TData>) -> Method<MTSync<T
         // Rust representation: Vec<((bool, u32), String, String)>
         .in_arg(("clevis_infos", "a((bu)ss)"))
         // b: true if pool was newly encrypted
+        //
+        // Rust representation: bool
+        .out_arg(("results", "b"))
+        .out_arg(("return_code", "q"))
+        .out_arg(("return_string", "s"))
+}
+
+pub fn reencrypt_pool_method(f: &Factory<MTSync<TData>, TData>) -> Method<MTSync<TData>, TData> {
+    f.method("ReencryptPool", (), reencrypt_pool)
+        // b: true if successful
         //
         // Rust representation: bool
         .out_arg(("results", "b"))

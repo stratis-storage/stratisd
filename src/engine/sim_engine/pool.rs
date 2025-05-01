b43fc5f3b2c72fccd89850a7a7613e47cf8a5e47
John Baublitz <jbaublitz@redhat.com>
2025-05-01 15:10:06 -0400

    Add reencryption timestamp API to engine

--- src/engine/sim_engine/pool.rs
+++ src/engine/sim_engine/pool.rs
@@ -1068,6 +1068,10 @@ impl Pool for SimPool {
     fn load_volume_key(&mut self, _: PoolUuid) -> StratisResult<bool> {
         Ok(false)
     }
+
+    fn last_reencrypt(&self) -> Option<DateTime<Utc>> {
+        self.last_reencrypt
+    }
 }
 
 #[cfg(test)]

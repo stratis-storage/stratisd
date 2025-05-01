6d781a550fe51666b41eecdfc10b536bd3244732
John Baublitz <jbaublitz@redhat.com>
2025-05-01 10:42:34 -0400

    Update timestamp on successful decryption and reencryption operations

--- src/engine/strat_engine/pool/v2.rs
+++ src/engine/strat_engine/pool/v2.rs
@@ -1244,10 +1244,14 @@ impl Pool for StratPool {
     }
 
     #[pool_mutating_action("NoRequests")]
-    fn reencrypt_pool(&mut self) -> StratisResult<ReencryptedDevice> {
+    fn reencrypt_pool(&mut self, name: &Name) -> StratisResult<ReencryptedDevice> {
         self.thin_pool.suspend()?;
         let encrypt_res = self.backstore.reencrypt();
+        if encrypt_res.is_ok() {
+            self.last_reencrypt = Some(Utc::now());
+        }
         self.thin_pool.resume()?;
+        self.write_metadata(name)?;
         encrypt_res.map(|_| ReencryptedDevice)
     }
 
@@ -1274,6 +1278,9 @@ impl Pool for StratPool {
                         direction,
                     )
                 });
+                if encrypt_res.is_ok() {
+                    self.last_reencrypt = None;
+                }
                 self.thin_pool.resume()?;
                 let metadata_res = self.write_metadata(name);
                 let _ = encrypt_res?;
@@ -2220,7 +2227,8 @@ mod tests {
                     test_async!(engine.get_mut_pool(PoolIdentifier::Uuid(pool_uuid))).unwrap();
                 let (_, _, pool) = handle.as_mut_tuple();
                 assert!(pool.is_encrypted());
-                pool.reencrypt_pool().unwrap();
+                pool.reencrypt_pool(&Name::new("encrypt_with_both".to_string()))
+                    .unwrap();
                 assert!(pool.is_encrypted());
             }
 

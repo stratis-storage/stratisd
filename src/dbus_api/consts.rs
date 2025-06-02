ad44e8e242f72071646978c010bbfd4d19257b95
John Baublitz <jbaublitz@redhat.com>
2025-06-02 14:07:45 -0400

    Add D-Bus interface to report if volume key is loaded

--- src/dbus_api/consts.rs
+++ src/dbus_api/consts.rs
@@ -55,6 +55,7 @@ pub const POOL_OVERPROV_PROP: &str = "Overprovisioning";
 pub const POOL_NO_ALLOCABLE_SPACE_PROP: &str = "NoAllocSpace";
 pub const POOL_METADATA_VERSION_PROP: &str = "MetadataVersion";
 pub const POOL_FREE_TOKEN_SLOTS_PROP: &str = "FreeTokenSlots";
+pub const POOL_VOLUME_KEY_LOADED: &str = "VolumeKeyLoaded";
 
 pub const FILESYSTEM_INTERFACE_NAME_3_0: &str = "org.storage.stratis3.filesystem.r0";
 pub const FILESYSTEM_INTERFACE_NAME_3_1: &str = "org.storage.stratis3.filesystem.r1";

339b61318804b5ff76c29829316740fa29a70453
John Baublitz <jbaublitz@redhat.com>
2025-08-07 11:06:44 -0400

    Add upgrade and downgrade to lock and use in reencryption
    This adds an upgrade and downgrade API that allows atomically converting
    from read to write locks and vice versa. This commit also applies the
    usage of this API in reencryption to allow listing pools during the
    actual reencryption operation.

--- src/engine/strat_engine/pool/v1.rs
+++ src/engine/strat_engine/pool/v1.rs
@@ -1356,6 +1356,7 @@ impl Pool for StratPool {
         self.backstore.prepare_reencrypt()
     }
 
+    #[pool_mutating_action("NoRequests")]
     fn do_reencrypt_pool(
         &self,
         pool_uuid: PoolUuid,

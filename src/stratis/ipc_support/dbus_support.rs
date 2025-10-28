e2b00e77da3f9acd1774c78ab0e3fb6ce976ebb9
John Baublitz <jbaublitz@redhat.com>
2025-10-28 10:43:53 -0400

    Prototype of udev event handling and pool addition to the D-Bus

--- src/stratis/ipc_support/dbus_support.rs
+++ src/stratis/ipc_support/dbus_support.rs
@@ -11,59 +11,38 @@
 
 use std::sync::Arc;
 
-use tokio::{
-    select,
-    sync::mpsc::{UnboundedReceiver, UnboundedSender},
-    task,
-};
+use tokio::{select, task};
 
 use crate::{
-    dbus::{create_dbus_handlers, DbusAction},
-    engine::{Engine, UdevEngineEvent},
+    dbus::UdevHandler,
+    engine::Engine,
     stratis::{StratisError, StratisResult},
 };
 
 /// Set up the cooperating D-Bus threads.
-pub async fn setup(
-    engine: Arc<dyn Engine>,
-    receiver: UnboundedReceiver<UdevEngineEvent>,
-    tree_channel: (UnboundedSender<DbusAction>, UnboundedReceiver<DbusAction>),
-) -> StratisResult<()> {
-    let engine_clone = Arc::clone(&engine);
-    let (mut udev, mut signal) = create_dbus_handlers(engine_clone, receiver, tree_channel).await?;
-
-    let pools = engine.pools().await;
-    for (pool_name, pool_uuid, pool) in pools.iter() {
-        udev.register_pool(pool_name, *pool_uuid, pool).await;
+pub async fn setup(engine: Arc<dyn Engine>, mut udev: UdevHandler) -> StratisResult<()> {
+    {
+        let pools = engine.pools().await;
+        for (_, pool_uuid, _) in pools.iter() {
+            if let Err(e) = udev.register_pool(*pool_uuid).await {
+                warn!("Failed to register existing pools: {e}");
+            }
+        }
     }
     info!("D-Bus API is available");
 
-    let mut signal_handle = task::spawn(async move {
-        if let Err(e) = signal.process_dbus_actions().await {
-            error!(
-                "Failed to process D-Bus object path addition or removal: {e}; \
-                exiting D-Bus thread",
-            );
-        }
-    });
     let mut udev_handle = task::spawn(async move {
         loop {
             trace!("Starting D-Bus udev event handling");
-            if let Err(e) = udev.handle_udev_event().await {
-                error!(
-                    "Failed to process udev event in the D-Bus layer: {e}; exiting D-Bus thread",
-                );
-                return;
+            if let Err(e) = udev.process_udev_events().await {
+                warn!("Failed to handle udev events: {e}");
+                break;
             }
             trace!("Finished D-Bus udev event handling");
         }
     });
 
     select! {
-        res = &mut signal_handle => {
-            error!("The signal handling thread exited...");
-            res.map_err(StratisError::from)
-        }
         res = &mut udev_handle => {
             error!("The udev processing thread exited...");
             res.map_err(StratisError::from)

687acf1a6da7c731a2dcfb4e24a124f047317437
John Baublitz <jbaublitz@redhat.com>
2025-10-28 10:43:53 -0400

    Prototype of udev event handling and pool addition to the D-Bus

--- src/dbus/manager/manager_3_0/props.rs
+++ src/dbus/manager/manager_3_0/props.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub const STRATIS_BASE_PATH: &str = "/org/storage/stratis3";
-pub const STRATIS_BASE_SERVICE: &str = "org.storage.stratis3";
-pub const OK_STRING: &str = "Ok";
+use crate::stratis::VERSION;
+
+pub fn version_prop() -> &'static str {
+    VERSION
+}

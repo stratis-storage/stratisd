84f3bd33d993e566eeaf257c0a53e075f1cf079a
John Baublitz <jbaublitz@redhat.com>
2025-10-30 13:32:46 -0400

    Prototype of adding filesystems

--- src/dbus/pool/pool_3_6/mod.rs
+++ src/dbus/pool/pool_3_6/mod.rs
@@ -2,11 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub type FilesystemSpec<'a> = Vec<(&'a str, (bool, &'a str), (bool, &'a str))>;
+mod methods;
 
-#[derive(Clone, Copy, Debug)]
-#[allow(non_camel_case_types)]
-pub enum DbusErrorEnum {
-    OK = 0,
-    ERROR = 1,
-}
+pub use methods::create_filesystems_method;

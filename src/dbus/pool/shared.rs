3ad82a24b08c02c7d5f6e88c43f1ba5c8467551a
John Baublitz <jbaublitz@redhat.com>
2025-10-30 13:28:24 -0400

    Prototype of signal handling with zbus

---
+++ src/dbus/pool/shared.rs
@@ -0,0 +1,35 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use std::sync::Arc;
+
+use zbus::fdo::Error;
+
+use crate::engine::{Engine, Pool, PoolIdentifier, PoolUuid, SomeLockReadGuard};
+
+pub async fn pool_prop<R>(
+    engine: &Arc<dyn Engine>,
+    uuid: PoolUuid,
+    f: impl Fn(SomeLockReadGuard<PoolUuid, dyn Pool>) -> R,
+) -> Result<R, Error> {
+    let guard = engine
+        .get_pool(PoolIdentifier::Uuid(uuid))
+        .await
+        .ok_or_else(|| Error::Failed(format!("No pool associated with UUID {uuid}")))?;
+
+    Ok(f(guard))
+}
+
+pub async fn try_pool_prop<R>(
+    engine: &Arc<dyn Engine>,
+    uuid: PoolUuid,
+    f: impl Fn(SomeLockReadGuard<PoolUuid, dyn Pool>) -> R,
+) -> Result<R, Error> {
+    let guard = engine
+        .get_pool(PoolIdentifier::Uuid(uuid))
+        .await
+        .ok_or_else(|| Error::Failed(format!("No pool associated with UUID {uuid}")))?;
+
+    Ok(f(guard))
+}

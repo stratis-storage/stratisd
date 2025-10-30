573d14d47e0687dc97fa5c4efb883cedfa564416
John Baublitz <jbaublitz@redhat.com>
2025-10-30 13:32:46 -0400

    Prototype of adding filesystems

--- src/engine/sim_engine/pool.rs
+++ src/engine/sim_engine/pool.rs
@@ -218,12 +218,12 @@ impl Pool for SimPool {
         }
     }
 
-    fn create_filesystems<'b>(
+    fn create_filesystems(
         &mut self,
         _pool_name: &str,
         _pool_uuid: PoolUuid,
-        specs: &[(&'b str, Option<Bytes>, Option<Bytes>)],
-    ) -> StratisResult<SetCreateAction<(&'b str, FilesystemUuid, Sectors)>> {
+        specs: &[(&str, Option<Bytes>, Option<Bytes>)],
+    ) -> StratisResult<SetCreateAction<(Name, FilesystemUuid, Sectors)>> {
         self.check_fs_limit(specs.len())?;
 
         let spec_map = validate_filesystem_size_specs(specs)?;
@@ -255,7 +255,7 @@ impl Pool for SimPool {
                 let new_filesystem = SimFilesystem::new(size, size_limit, None)?;
                 self.filesystems
                     .insert(Name::new((name).to_owned()), uuid, new_filesystem);
-                result.push((name, uuid, size));
+                result.push((Name::new(name.to_owned()), uuid, size));
             }
         }
 
@@ -1134,7 +1134,7 @@ mod tests {
             .unwrap()
             .changed()
             .unwrap();
-        let old_uuid = results.iter().find(|x| x.0 == old_name).unwrap().1;
+        let old_uuid = results.iter().find(|x| x.0.as_ref() == old_name).unwrap().1;
         assert_matches!(
             pool.rename_filesystem(pool_name, old_uuid, new_name),
             Err(_)
@@ -1273,7 +1273,7 @@ mod tests {
             .ok()
             .and_then(|fs| fs.changed())
         {
-            Some(names) => (names.len() == 1) & (names[0].0 == "name"),
+            Some(names) => (names.len() == 1) & (names[0].0.as_ref() == "name"),
             _ => false,
         });
     }
@@ -1327,7 +1327,7 @@ mod tests {
             .ok()
             .and_then(|fs| fs.changed())
         {
-            Some(names) => (names.len() == 1) & (names[0].0 == fs_name),
+            Some(names) => (names.len() == 1) & (names[0].0.as_ref() == fs_name),
             _ => false,
         });
     }

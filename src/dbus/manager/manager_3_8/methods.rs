539321ced80cbd81a498eba0e6a1c8d87fddd4e1
John Baublitz <jbaublitz@redhat.com>
2025-11-20 12:15:27 -0500

    Add ManagerR8 interface

--- src/dbus/manager/manager_3_8/methods.rs
+++ src/dbus/manager/manager_3_8/methods.rs
@@ -4,12 +4,16 @@
 
 use std::{
     path::PathBuf,
+    os::fd::AsRawFd,
     sync::{atomic::AtomicU64, Arc},
 };
 
 use serde_json::{from_str, Value};
 use tokio::sync::RwLock;
-use zbus::{zvariant::ObjectPath, Connection};
+use zbus::{
+    zvariant::{Fd, ObjectPath},
+    Connection,
+};
 
 use devicemapper::Bytes;
 
@@ -17,13 +21,14 @@ use crate::{
     dbus::{
         consts::OK_STRING,
         manager::Manager,
+        filesystem::register_filesystem,
         pool::register_pool,
         types::DbusErrorEnum,
-        util::{engine_to_dbus_err_tuple, tuple_to_option},
+        util::{engine_to_dbus_err_tuple, tuple_to_option, send_stopped_pools_signals, send_locked_pools_signals},
     },
     engine::{
         CreateAction, Engine, InputEncryptionInfo, IntegritySpec, IntegrityTagSpec, KeyDescription,
-        Lockable,
+        Lockable, Name, PoolIdentifier, PoolUuid, StartAction, TokenUnlockMethod,
     },
     stratis::{StratisError, StratisResult},
 };
@@ -118,6 +123,131 @@ pub async fn create_pool_method<'a>(
             }
         }
         Ok(CreateAction::Identity) => (
+            default_return,
+            DbusErrorEnum::OK as u16,
+            OK_STRING.to_string(),
+        ),
+        Err(e) => {
+            let (rc, rs) = engine_to_dbus_err_tuple(&e);
+            (default_return, rc, rs)
+        }
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+pub async fn start_pool_method<'a>(
+    engine: &Arc<dyn Engine>,
+    connection: &Arc<Connection>,
+    manager: &Lockable<Arc<RwLock<Manager>>>,
+    counter: &Arc<AtomicU64>,
+    id: &str,
+    id_type: &str,
+    unlock_method_tuple: (bool, (bool, u32)),
+    key_fd: (bool, Fd<'_>),
+) -> (
+    (
+        bool,
+        (ObjectPath<'a>, Vec<ObjectPath<'a>>, Vec<ObjectPath<'a>>),
+    ),
+    u16,
+    String,
+) {
+    let default_return = (
+        false,
+        (ObjectPath::default(), Vec::default(), Vec::default()),
+    );
+
+    let id = match id_type {
+        "uuid" => match PoolUuid::parse_str(id) {
+            Ok(u) => PoolIdentifier::Uuid(u),
+            Err(e) => {
+                let (rc, rs) = engine_to_dbus_err_tuple(&e);
+                return (default_return, rc, rs);
+            }
+        },
+        "name" => PoolIdentifier::Name(Name::new(id.to_string())),
+        _ => {
+            return (
+                default_return,
+                DbusErrorEnum::ERROR as u16,
+                format!("ID type {id_type} not recognized"),
+            );
+        }
+    };
+    let key_fd_option = tuple_to_option(key_fd);
+
+    match handle_action!(
+        engine
+            .start_pool(
+                id,
+                TokenUnlockMethod::from_options(
+                    tuple_to_option(unlock_method_tuple).map(tuple_to_option)
+                ),
+                key_fd_option.as_ref().map(|fd| fd.as_raw_fd()),
+                false
+            )
+            .await
+    ) {
+        Ok(StartAction::Started(pool_uuid)) => {
+            let guard = match engine.get_pool(PoolIdentifier::Uuid(pool_uuid)).await {
+                Some(g) => g,
+                None => {
+                    return (
+                        default_return,
+                        DbusErrorEnum::ERROR as u16,
+                        format!("No pool found for newly started pool with UUID {pool_uuid}"),
+                    );
+                }
+            };
+            let mut fs_paths = Vec::default();
+            for fs_uuid in guard
+                .filesystems()
+                .into_iter()
+                .map(|(_, fs_uuid, _)| fs_uuid)
+                .collect::<Vec<_>>()
+            {
+                let fs_path = match register_filesystem(
+                    engine, connection, manager, counter, pool_uuid, fs_uuid,
+                )
+                .await
+                {
+                    Ok(fp) => fp,
+                    Err(e) => {
+                        let (rc, rs) = engine_to_dbus_err_tuple(&e);
+                        return (default_return, rc, rs);
+                    }
+                };
+                fs_paths.push(fs_path);
+            }
+            let (pool_path, dev_paths) =
+                match register_pool(engine, connection, manager, counter, pool_uuid).await {
+                    Ok(pp) => pp,
+                    Err(e) => {
+                        let (rc, rs) = engine_to_dbus_err_tuple(&e);
+                        return (default_return, rc, rs);
+                    }
+                };
+
+            if guard.is_encrypted() {
+                if let Err(e) = send_locked_pools_signals(connection).await {
+                    warn!(
+                        "Failed to send signals for changed properties for the Manager interfaces: {e}"
+                    );
+                }
+            }
+            if let Err(e) = send_stopped_pools_signals(connection).await {
+                warn!(
+                    "Failed to send signals for changed properties for the Manager interfaces: {e}"
+                );
+            }
+
+            (
+                (true, (pool_path, dev_paths, fs_paths)),
+                DbusErrorEnum::OK as u16,
+                OK_STRING.to_string(),
+            )
+        }
+        Ok(StartAction::Identity) => (
             default_return,
             DbusErrorEnum::OK as u16,
             OK_STRING.to_string(),

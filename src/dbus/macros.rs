55ac43009a972e26c3017126118f596155956656
John Baublitz <jbaublitz@redhat.com>
2025-11-21 14:25:12 -0500

    Add PoolR0 interface

--- src/dbus/macros.rs
+++ src/dbus/macros.rs
@@ -10,4 +10,22 @@ macro_rules! handle_action {
         }
         action
     }};
+    ($action:expr, $connection:expr, $manager:expr, $pool_uuid:expr) => {{
+        let action = $action;
+        if let Ok(ref a) = action {
+            log::info!("{}", a);
+        } else if let Err(ref e) = action {
+            if e.error_to_available_actions().is_some() {
+                match futures::executor::block_on($manager.read()).pool_get_path(&$pool_uuid) {
+                    Some(p) => {
+                        futures::executor::block_on($crate::dbus::util::send_action_availability_signal(&$connection, p));
+                    }
+                    None => {
+                        warn!("Could not find path associated with pool with UUID {}; could not send action availability change signal", $pool_uuid);
+                    }
+                }
+            }
+        }
+        action
+    }};
 }

0f15d64bd3652b23267563f1f8ae7814481eae23
mulhern <amulhern@redhat.com>
2026-01-09 21:30:46 -0500

    Fix hang on filesystem signal emit
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus/filesystem/filesystem_3_0/methods.rs
+++ src/dbus/filesystem/filesystem_3_0/methods.rs
@@ -23,42 +23,46 @@ pub async fn set_name_method(
 ) -> ((bool, String), u16, String) {
     let default_return = (false, FilesystemUuid::nil().simple().to_string());
 
-    match engine
-        .get_mut_pool(PoolIdentifier::Uuid(uuid))
-        .await
-        .ok_or_else(|| StratisError::Msg(format!("No pool associated with uuid {uuid}")))
-    {
-        Err(err) => (default_return, DbusErrorEnum::ERROR as u16, err.to_string()),
-        Ok(mut guard) => {
-            let (pool_name, _, pool) = guard.as_mut_tuple();
-            match handle_action!(pool.rename_filesystem(&pool_name, fs_uuid, name)) {
-                Ok(RenameAction::NoSource) => (
-                    default_return,
-                    DbusErrorEnum::ERROR as u16,
-                    format!("pool doesn't know about filesystem {fs_uuid}"),
-                ),
-                Ok(RenameAction::Renamed(_)) => {
-                    match manager.read().await.filesystem_get_path(&fs_uuid) {
-                        Some(p) => {
-                            send_fs_name_signal(connection, &p.as_ref()).await;
-                        }
-                        None => {
-                            warn!("No object path associated with pool UUID {uuid}; failed to send pool name change signals");
-                        }
-                    };
-                    (
-                        (true, fs_uuid.simple().to_string()),
-                        DbusErrorEnum::OK as u16,
-                        OK_STRING.to_string(),
-                    )
+    let result = {
+        let mut guard = match engine
+            .get_mut_pool(PoolIdentifier::Uuid(uuid))
+            .await
+            .ok_or_else(|| StratisError::Msg(format!("No pool associated with uuid {uuid}")))
+        {
+            Err(err) => return (default_return, DbusErrorEnum::ERROR as u16, err.to_string()),
+            Ok(guard) => guard,
+        };
+
+        let (pool_name, _, pool) = guard.as_mut_tuple();
+        handle_action!(pool.rename_filesystem(&pool_name, fs_uuid, name))
+    };
+
+    match result {
+        Ok(RenameAction::NoSource) => (
+            default_return,
+            DbusErrorEnum::ERROR as u16,
+            format!("pool doesn't know about filesystem {fs_uuid}"),
+        ),
+        Ok(RenameAction::Renamed(_)) => {
+            match manager.read().await.filesystem_get_path(&fs_uuid) {
+                Some(p) => {
+                    send_fs_name_signal(connection, &p.as_ref()).await;
                 }
-                Ok(RenameAction::Identity) => (
-                    default_return,
-                    DbusErrorEnum::OK as u16,
-                    OK_STRING.to_string(),
-                ),
-                Err(err) => (default_return, DbusErrorEnum::ERROR as u16, err.to_string()),
-            }
+                None => {
+                    warn!("No object path associated with pool UUID {uuid}; failed to send pool name change signals");
+                }
+            };
+            (
+                (true, fs_uuid.simple().to_string()),
+                DbusErrorEnum::OK as u16,
+                OK_STRING.to_string(),
+            )
         }
+        Ok(RenameAction::Identity) => (
+            default_return,
+            DbusErrorEnum::OK as u16,
+            OK_STRING.to_string(),
+        ),
+        Err(err) => (default_return, DbusErrorEnum::ERROR as u16, err.to_string()),
     }
 }

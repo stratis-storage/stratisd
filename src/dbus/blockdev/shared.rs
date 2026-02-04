ee9d33f65526928561dd8345e1f3d218f66d407b
John Baublitz <jbaublitz@redhat.com>
2026-02-04 15:32:20 -0500

    Send signals on all interfaces for settable blockdev properties

--- src/dbus/blockdev/shared.rs
+++ src/dbus/blockdev/shared.rs
@@ -2,27 +2,49 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::sync::Arc;
+use std::{future::Future, sync::Arc};
 
-use zbus::{self, fdo::Error};
+use tokio::sync::RwLock;
+use zbus::{self, fdo::Error, Connection};
 
-use crate::engine::{
-    BlockDev, BlockDevTier, DevUuid, Engine, Pool, PoolIdentifier, PoolUuid, SomeLockWriteGuard,
+use crate::{
+    dbus::manager::Manager,
+    engine::{
+        BlockDev, BlockDevTier, DevUuid, Engine, Lockable, Pool, PoolIdentifier, PoolUuid,
+        SomeLockWriteGuard,
+    },
 };
 
-pub async fn set_blockdev_prop<V>(
+#[allow(clippy::too_many_arguments)]
+pub async fn set_blockdev_prop<'a, 'b, V, F, Fut, S, SFut>(
     engine: &Arc<dyn Engine>,
+    connection: &'a Arc<Connection>,
+    manager: &'b Lockable<Arc<RwLock<Manager>>>,
     uuid: PoolUuid,
     bd_uuid: DevUuid,
     value: V,
-    f: impl Fn(&mut SomeLockWriteGuard<PoolUuid, dyn Pool>, DevUuid, V) -> Result<(), zbus::Error>,
-) -> Result<(), zbus::Error> {
-    let mut guard = engine
+    f: F,
+    send_signal: S,
+) -> Result<(), zbus::Error>
+where
+    F: FnOnce(SomeLockWriteGuard<PoolUuid, dyn Pool>, DevUuid, V) -> Fut,
+    Fut: Future<Output = Result<bool, zbus::Error>>,
+    S: FnOnce(&'a Arc<Connection>, &'b Lockable<Arc<RwLock<Manager>>>, DevUuid) -> SFut,
+    SFut: Future<Output = ()>,
+{
+    let guard = engine
         .get_mut_pool(PoolIdentifier::Uuid(uuid))
         .await
         .ok_or_else(|| zbus::Error::Failure(format!("No pool associated with UUID {uuid}")))?;
 
-    f(&mut guard, bd_uuid, value)
+    let changed = f(guard, bd_uuid, value).await?;
+    // Guard is dropped here, lock is released
+
+    if changed {
+        send_signal(connection, manager, bd_uuid).await;
+    }
+
+    Ok(())
 }
 
 pub async fn blockdev_prop<R>(

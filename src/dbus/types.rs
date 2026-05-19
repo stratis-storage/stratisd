5ee2c7176b2c6047ef8b90e37b2ebdaa1ac9d1bb
mulhern <amulhern@redhat.com>
2026-05-19 16:38:41 -0400

    Remove unnecessary trait implementations
    The trait implementations for DevUuid and Filesystem Uuid were made
    unnecessary by the previous commit.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus/types.rs
+++ src/dbus/types.rs
@@ -8,9 +8,7 @@ use zbus::zvariant::{signature::Child, Basic, Signature, Type, Value};
 
 use crate::{
     dbus::util::result_option_to_tuple,
-    engine::{
-        ActionAvailability, DevUuid, FilesystemUuid, LockedPoolsInfo, PoolUuid, StoppedPoolsInfo,
-    },
+    engine::{ActionAvailability, LockedPoolsInfo, PoolUuid, StoppedPoolsInfo},
 };
 
 pub type FilesystemSpec<'a> = Vec<(&'a str, (bool, &'a str), (bool, &'a str))>;
@@ -245,36 +243,6 @@ impl Type for PoolUuid {
 
 impl<'a> From<PoolUuid> for Value<'a> {
     fn from(u: PoolUuid) -> Value<'a> {
-        Value::from(u.simple().to_string())
-    }
-}
-
-impl Basic for FilesystemUuid {
-    const SIGNATURE_CHAR: char = 's';
-    const SIGNATURE_STR: &str = "s";
-}
-
-impl Type for FilesystemUuid {
-    const SIGNATURE: &Signature = &Signature::Str;
-}
-
-impl<'a> From<FilesystemUuid> for Value<'a> {
-    fn from(u: FilesystemUuid) -> Value<'a> {
-        Value::from(u.simple().to_string())
-    }
-}
-
-impl Basic for DevUuid {
-    const SIGNATURE_CHAR: char = 's';
-    const SIGNATURE_STR: &str = "s";
-}
-
-impl Type for DevUuid {
-    const SIGNATURE: &Signature = &Signature::Str;
-}
-
-impl<'a> From<DevUuid> for Value<'a> {
-    fn from(u: DevUuid) -> Value<'a> {
         Value::from(u.simple().to_string())
     }
 }
